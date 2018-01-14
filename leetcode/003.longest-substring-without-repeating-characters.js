6f5e1296b9cae2ad8670879701805e04464d80c0
jiangshanmeta <540118044@qq.com>
2018-01-14 16:07:06 +0800

    update leetcode 003

--- leetcode/003.longest-substring-without-repeating-characters.js
+++ leetcode/003.longest-substring-without-repeating-characters.js
@@ -4,29 +4,33 @@
  */
 var lengthOfLongestSubstring = function(s) {
     var maxLen = 0;
-    var sLen = s.length;
-    var set;
-    var curLength;
+    // startIndex 是左界
+    var startIndex = 0;
+    var hash = {};
     var item;
-    for(var i=0;i<sLen;i++){
-        set = Object.create(null);
-        curLength = 0;
-        for(var j=i;j<sLen;j++){
-            item = s[j];
-            if(!set[item]){
-                set[item] = item;
-                curLength++;
-            }else{
-                break;
+    // i是右界
+    for(var i=0;i<s.length;i++){
+        item = s[i];
+        if(!hash[item]){
+            hash[item] = true;
+        }else{
+            // 从startIndex到i只重复i对应元素
+            // 但是要满足i在子串内，需要移动左边界
+            // 要满足 i对应元素在内，移动左边界
+
+            while(true){
+                // startIndex对应元素和i对应元素一致
+                // 删除startIndex后就是一个不重复的子串了
+                if(s[startIndex] === item){
+                    startIndex++;
+                    break;
+                }
+                hash[s[startIndex]] = false;
+                startIndex++;
             }
-            
+
         }
-        
-        if(curLength>maxLen){
-            maxLen = curLength
-        }
-        
-        continue;
+        maxLen = Math.max(maxLen,i- startIndex +1);
     }
     return maxLen;
 };
