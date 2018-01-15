b0045449efd8148e1b6e9208832c41c2f4c8475c
jiangshanmeta <540118044@qq.com>
2018-01-15 19:10:11 +0800

    leetcode 011

---
+++ leetcode/011.container-with-most-water.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    if(height.length<2){
+        return 0;
+    }
+    var area = 0;
+    var leftIndex = 0;
+    var rightIndex = height.length - 1;
+
+    
+    while(leftIndex<rightIndex){
+        area = Math.max(area,Math.min(height[leftIndex],height[rightIndex])*(rightIndex-leftIndex)   );
+        
+        if(height[leftIndex]<height[rightIndex]){
+            leftIndex++
+        }else{
+            rightIndex--;
+        }
+        
+        
+    }
+    return area;
+    
+    
+};
