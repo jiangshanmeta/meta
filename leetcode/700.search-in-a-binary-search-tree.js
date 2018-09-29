1b5171d4cd527dbab9ed890b4decf0db78e0923d
jiangshanmeta <540118044@qq.com>
2018-09-29 12:50:01 +0800

    search in a binary tree

---
+++ leetcode/700.search-in-a-binary-search-tree.js
@@ -0,0 +1,27 @@
+// 二叉搜索树的基本操作
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    if(root === null){
+        return [];
+    }
+    
+    if(root.val === val){
+        return root;
+    }
+    
+    if(root.val<val){
+        return searchBST(root.right,val);
+    }
+    return searchBST(root.left,val);
+};
