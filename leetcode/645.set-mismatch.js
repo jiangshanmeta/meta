b354da62de1c92b2b4e9acb54760af91e1f2434b
jiangshanmeta <540118044@qq.com>
2018-10-20 17:49:44 +0800

    leetcode 645

---
+++ leetcode/645.set-mismatch.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function(nums) {
+    let sum = 0;
+    const map = {};
+    let duplicateNum;
+    for(let i=0;i<nums.length;i++){
+        const val = nums[i];
+        sum += val;
+        // 利用map找重复元素
+        if(map[val] === undefined){
+            map[val] = 1;
+        }else{
+            duplicateNum = val;
+        }
+    }
+    
+    let L = nums.length;
+    // 等差数列求和
+    const targetSum = (L+1)*L/2;
+    
+    return [duplicateNum,targetSum-sum+duplicateNum]
+};
