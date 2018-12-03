e0110af0f03486675b8714fb8408cd2abc9c04b3
jiangshanmeta <540118044@qq.com>
2018-12-04 00:37:48 +0800

    update leetcode 003

--- leetcode/003.longest-substring-without-repeating-characters.js
+++ leetcode/003.longest-substring-without-repeating-characters.js
@@ -3,34 +3,21 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    var maxLen = 0;
-    // startIndex 是左界
-    var startIndex = 0;
-    var hash = {};
-    var item;
-    // i是右界
-    for(var i=0;i<s.length;i++){
-        item = s[i];
-        if(!hash[item]){
-            hash[item] = true;
+    // map记录每个字母出现的最后位置
+    const map = {};
+    let max = 0;
+    let cur = 0;
+    for(let i=0;i<s.length;i++){
+        // i-cur是当前子串的起始位置
+        if(map[s[i]] !== undefined && map[s[i]]>i-cur-1 ){
+            cur = i-map[s[i]];
         }else{
-            // 第i个元素与第k(startIndex=<k<i)个元素重复,
-            // 则以startIndex到k为开头的满足条件子串长度都不大于以startIndex为开头的子串
-            // 新的开头为k+1
-
-            while(true){
-                // 第k个元素
-                if(s[startIndex] === item){
-                    startIndex++;
-                    break;
-                }
-                // 第k个元素之前的元素
-                hash[s[startIndex]] = false;
-                startIndex++;
+            cur++;
+            if(cur>max){
+                max = cur;
             }
-
         }
-        maxLen = Math.max(maxLen,i- startIndex +1);
+        map[s[i]] = i;
     }
-    return maxLen;
+    return max;
 };
