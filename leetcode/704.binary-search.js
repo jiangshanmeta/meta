5b2f4ba3eccf2f4dbc11791a82fa42e3d7ffd78b
jiangshanmeta <540118044@qq.com>
2019-02-08 19:42:10 +0800

    leetcode 704

---
+++ leetcode/704.binary-search.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+// 标准二分查找
+var search = function(nums, target) {
+    let low = 0;
+    let high = nums.length-1;
+    while(low<=high){
+        const mid = (low+high) >> 1;
+        if(nums[mid] > target){
+            high = mid-1;
+        }else if(nums[mid] < target){
+            low = mid+1;
+        }else{
+            return mid;
+        }
+    }
+    return -1;
+};
