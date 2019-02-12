a2f7aaaccd24afdefc7140d4b389e18d43b29d3d
jiangshanmeta <540118044@qq.com>
2019-02-13 01:06:19 +0800

    leetcode 141

---
+++ leetcode/141.linked-list-cycle.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+// https://leetcode.com/articles/linked-list-cycle/
+// 快慢指针
+var hasCycle = function(head) {
+    if(head === null || head.next === null){
+        return false;
+    }
+    
+    let slow = head;
+    let fast = head.next;
+    
+    while(slow !== fast){
+        if(fast===null || fast.next===null){
+            return false;
+        }
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return true;
+};
