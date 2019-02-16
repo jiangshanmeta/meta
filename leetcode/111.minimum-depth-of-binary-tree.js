172221d3ca0b5b2c37278495beffd668bed70fe2
jiangshanmeta <540118044@qq.com>
2019-02-17 00:06:16 +0800

    leetcode 111

---
+++ leetcode/111.minimum-depth-of-binary-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if(root === null){
+        return 0;
+    }
+    // 如果只有一侧有子树 只考虑这一侧
+    if(root.left === null){
+        return minDepth(root.right)+1;
+    }
+    if(root.right === null){
+        return minDepth(root.left)+1;
+    }
+    return Math.min(minDepth(root.left),minDepth(root.right))+1;
+};
