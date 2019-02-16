287c509edde275b3727b856af9055b0e333cddf7
jiangshanmeta <540118044@qq.com>
2019-02-16 14:17:56 +0800

    leetcode 098

---
+++ leetcode/098.validate-binary-search-tree.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+function inOrderTravel(node,sequence){
+    node.left && inOrderTravel(node.left,sequence);
+    sequence.push(node.val);
+    node.right && inOrderTravel(node.right,sequence)
+}
+
+
+var isValidBST = function(root) {
+    if(!root){
+        return true;
+    }
+    
+    const sequence = [];
+    // 先中序遍历
+    inOrderTravel(root,sequence);
+    
+    // 看是否是单调递增
+    for(let i=1;i<sequence.length;i++){
+        if(sequence[i]<=sequence[i-1]){
+            return false;
+        }
+    }
+    return true;
+};
