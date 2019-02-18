1a50cdc745bb1b7e0e6fae14f3f1750c0a6054d5
jiangshanmeta <540118044@qq.com>
2019-02-18 14:17:46 +0800

    leetcode 114

---
+++ leetcode/114.flatten-binary-tree-to-linked-list.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+    if(!root){
+        return;
+    }
+    function helper(node){
+        if(!node){
+            return node;
+        }
+        
+        node.right && helper(node.right);
+        
+        if(node.left){
+            const leftStart = helper(node.left);
+            let leftEnd = leftStart;
+            
+            while(leftEnd.right){
+                leftEnd = leftEnd.right;
+            }
+            
+            leftEnd.right = node.right;
+            node.right = leftStart;
+
+            node.left = null;
+        }
+    
+        return node;
+    }
+    
+    helper(root);
+};
