545baf758189ae328b535e4f78149d47d166ae19
jiangshanmeta <540118044@qq.com>
2019-02-19 00:02:45 +0800

    leetcode 129

---
+++ leetcode/129.sum-root-to-leaf-numbers.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function(root) {
+    if(!root){
+        return 0;
+    }
+    const sequences = [];
+    function helper(node,num){
+        if(!node.left && !node.right){
+            return sequences.push(num*10+node.val);
+        }
+        if(!node.left){
+            return helper(node.right,num*10+node.val);
+        }
+        if(!node.right){
+            return helper(node.left,num*10+node.val);
+        }
+        helper(node.left,num*10+node.val);
+        helper(node.right,num*10+node.val);
+    }
+    helper(root,0);
+    
+    return sequences.reduce((sum,num)=>{
+        sum += num;
+        return sum;
+    },0);
+};
