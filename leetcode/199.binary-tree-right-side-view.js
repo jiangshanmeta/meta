c6bf2cdb762ce803215081b08da7e65a980ff6aa
jiangshanmeta <540118044@qq.com>
2019-02-19 00:02:54 +0800

    leetcode 199

---
+++ leetcode/199.binary-tree-right-side-view.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// 层序遍历
+var rightSideView = function(root) {
+    if(!root){
+        return [];
+    }
+    
+    const rst = [];
+    
+    let stack = [root];
+    while(stack.length){
+        const newStack = [];
+        rst.push(stack[stack.length-1].val);
+        for(let i =0;i<stack.length;i++){
+            const node = stack[i];
+            node.left && newStack.push(node.left);
+            node.right && newStack.push(node.right);
+        }
+        
+        stack = newStack;
+    }
+    return rst;
+};
