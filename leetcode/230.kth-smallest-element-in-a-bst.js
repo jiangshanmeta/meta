7e263546e479306fd80409e29d15c5ecd71dd76f
jiangshanmeta <540118044@qq.com>
2019-02-20 02:59:43 +0800

    leetcode 230

---
+++ leetcode/230.kth-smallest-element-in-a-bst.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+// BST 中遍历的结果就是从小到大的
+var kthSmallest = function(root, k) {
+    let counter = 0;
+    let rst;
+    
+    function inorderTravel(node){
+        node.left && inorderTravel(node.left);
+        
+        counter++;
+        if(counter === k){
+            rst = node.val;
+            return;
+        }
+        
+        node.right && inorderTravel(node.right);
+    }
+    inorderTravel(root)
+    return rst;
+};
