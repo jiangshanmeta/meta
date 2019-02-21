8ad2a8eb49c6bfc113899e6cb2892e4a798f664b
jiangshanmeta <540118044@qq.com>
2019-02-21 20:48:55 +0800

    leetcode 236

---
+++ leetcode/236.lowest-common-ancestor-of-a-binary-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+// 时间复杂度O(n)
+var lowestCommonAncestor = function(root, p, q) {
+    // 碰到 p或q不需要继续向下探查了
+    if(root === null || p === root || q === root){
+        return root;
+    }
+    let left = lowestCommonAncestor(root.left,p,q);
+    let right = lowestCommonAncestor(root.right,p,q);
+    // 如果左右两子树都有节点 则当前节点为最近祖先借点
+    if(left && right){
+        return root;
+    }
+    return left?left:right;
+};
