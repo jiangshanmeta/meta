4902a58755effb16bcd04b0c7264d61269c7bafa
jiangshanmeta <540118044@qq.com>
2019-02-22 16:00:35 +0800

    leetcode 95

--- leetcode/095.unique-binary-search-trees-ii.js
+++ leetcode/095.unique-binary-search-trees-ii.js
@@ -9,27 +9,30 @@
  * @param {number} n
  * @return {TreeNode[]}
  */
+// 分治思想
 var generateTrees = function(n) {
     const arr = Array.apply(null,{length:n}).map((item,index)=>index+1);
     
-    function dfs(arr,startIndex,endIndex){
+    function divideConquer(arr,startIndex,endIndex){
         if(startIndex>endIndex){
             return [];
         }
         
         let rst = [];
         for(let i=startIndex;i<=endIndex;i++){
+            // arr[i] 作为根节点的值
             if(startIndex === endIndex){
                 rst.push(new TreeNode(arr[i]));
                 continue;
             }
             
-            
-            const leftNodes = dfs(arr,startIndex,i-1);
-            const rightNodes = dfs(arr,i+1,endIndex);
+            // 划分左右
+            const leftNodes = divideConquer(arr,startIndex,i-1);
+            const rightNodes = divideConquer(arr,i+1,endIndex);
             const leftLength = leftNodes.length;
             const rightLength = rightNodes.length;
             
+            // 左右都有节点 有 leftLength*rightLength种情况
             if(leftLength && rightLength){
                 for(let j=0;j<leftNodes.length;j++){
                     for(let k=0;k<rightNodes.length;k++){
@@ -43,6 +46,7 @@ var generateTrees = function(n) {
                 continue;
             }
             
+            // 最多只有一侧有子节点
             if(!leftLength){
                 for(let j=0;j<rightNodes.length;j++){
                     let node = new TreeNode(arr[i]);
@@ -66,5 +70,5 @@ var generateTrees = function(n) {
         return rst;
     }
     
-    return dfs(arr,0,n-1);
+    return divideConquer(arr,0,n-1);
 };
