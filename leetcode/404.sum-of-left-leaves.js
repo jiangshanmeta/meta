d66989d36e559ecb1df9dac25911566671f819bd
jiangshanmeta <540118044@qq.com>
2019-02-24 11:04:43 +0800

    leetcode 404

---
+++ leetcode/404.sum-of-left-leaves.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+// DFS
+var sumOfLeftLeaves = function(root) {
+    if(!root){
+        return 0;
+    }
+    
+    let sum = 0;
+    
+    function inner(node,flag){
+        if(!node.left && !node.right && flag){
+            sum += node.val;
+            return;
+        }
+        node.left && inner(node.left,true);
+        node.right && inner(node.right,false);
+    }
+    
+    inner(root,false);
+    return sum;
+};
