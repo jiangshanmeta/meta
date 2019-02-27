0b10045342e488b087ff0f602b18ac2b86ad1387
jiangshanmeta <540118044@qq.com>
2019-02-27 14:09:27 +0800

    leetcode 501

---
+++ leetcode/501.find-mode-in-binary-search-tree.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var findMode = function(root) {
+    if(!root){
+        return [];
+    }
+    let max = 0;
+    let lastVal;
+    let modes = [];
+    let counter = 0;
+    
+    function inOrderTravel(node){
+        node.left && inOrderTravel(node.left);
+        
+        if(node.val !== lastVal){
+            lastVal = node.val;
+            counter = 1;
+        }else{
+            counter++;
+        }
+        
+        if(counter>max){
+            max = counter;
+            modes = [node.val];
+        }else if(counter === max){
+            modes.push(node.val);
+        }
+        
+        node.right && inOrderTravel(node.right);
+    }
+    
+    inOrderTravel(root);
+    return modes;
+};
