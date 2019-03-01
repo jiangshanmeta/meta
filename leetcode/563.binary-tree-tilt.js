03ac93f8238a26d3b3ddd1c7a112a3f54ee61ab7
jiangshanmeta <540118044@qq.com>
2019-03-01 14:37:31 +0800

    leetcode 563

---
+++ leetcode/563.binary-tree-tilt.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+// 后序遍历
+
+var findTilt = function(root) {
+    if(!root){
+        return 0;
+    }
+    
+    let counter = 0;
+    
+    function dfs(node){
+        if(!node){
+            return 0;
+        }
+
+        const leftSubTreeSum = dfs(node.left);
+        const rightSubTreeSum = dfs(node.right);
+        counter += Math.abs(leftSubTreeSum-rightSubTreeSum);
+        
+        return leftSubTreeSum + rightSubTreeSum + node.val;
+    }
+
+    
+    
+    dfs(root);
+    
+    return counter;
+};
