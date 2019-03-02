b829a7acbfdde487ac21cd4a1a4180d57a4cadf0
jiangshanmeta <540118044@qq.com>
2019-03-02 10:58:39 +0800

    leetcode 617

---
+++ leetcode/617.merge-two-binary-trees.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {TreeNode}
+ */
+var mergeTrees = function(t1, t2) {
+    // 为什么要返回数组呢  问leetcode去
+    if(!t1 && !t2){
+        return [];
+    }
+    
+    function helper(t1,t2){
+        if(!t1){
+            return t2 || null;
+        }
+        if(!t2){
+            return t1;
+        }
+
+        t1.val = t1.val + t2.val;
+        t1.left = helper(t1.left,t2.left);
+        t1.right = helper(t1.right,t2.right);
+
+        return t1;
+    }
+    
+
+    return helper(t1,t2);
+};
