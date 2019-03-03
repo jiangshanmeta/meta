fde3bfc651f4c06dce7dcc86f2a140a565a8f92a
jiangshanmeta <540118044@qq.com>
2019-03-04 00:49:39 +0800

    leetcode 654

---
+++ leetcode/654.maximum-binary-tree.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var constructMaximumBinaryTree = function(nums) {
+    function helper(nums,startIndex,endIndex){
+        if(endIndex<startIndex){
+            return null;
+        }
+        // 在范围内找到最大值 构建根节点
+        let maxIndex = startIndex;
+        let maxVal = nums[startIndex];
+        for(let i=startIndex+1;i<=endIndex;i++){
+            if(nums[i]>maxVal){
+                maxIndex = i;
+                maxVal = nums[i];
+            }
+        }
+        
+        const root = new TreeNode(nums[maxIndex]);
+        // 构建左右子树
+        root.left = helper(nums,startIndex,maxIndex-1);
+        root.right = helper(nums,maxIndex+1,endIndex);
+        
+        return root;
+    }
+    
+    return helper(nums,0,nums.length-1);
+};
