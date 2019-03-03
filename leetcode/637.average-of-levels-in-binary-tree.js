16dea2f9f0e5c0852b0a149bbcbfd2c24ed36602
jiangshanmeta <540118044@qq.com>
2019-03-03 16:00:43 +0800

    leetcode 637

---
+++ leetcode/637.average-of-levels-in-binary-tree.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// 找到每一层的平均值
+// 最直接的实现是层序遍历(BFS)
+// 其实DFS也行
+function getLevelSequence(nodes,sequences){
+    const values = [];
+    const nextLevelNodes = nodes.reduce((arr,node)=>{
+        values.push(node.val);
+        node.left && arr.push(node.left);
+        node.right && arr.push(node.right);
+        return arr;
+    },[]);
+    
+    sequences.push(values.reduce((sum,val)=>{
+        sum+=val;
+        return sum;
+    },0)/nodes.length);
+    
+    if(nextLevelNodes.length){
+        return getLevelSequence(nextLevelNodes,sequences);
+    }
+    
+}
+
+
+var averageOfLevels = function(root) {
+    const firstLevelNodes = [root];
+    const sequences = [];
+    getLevelSequence(firstLevelNodes,sequences);
+    return sequences;
+};
