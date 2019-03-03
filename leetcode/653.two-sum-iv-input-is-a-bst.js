ed376ccb5fa5d9c439afcacd2c3e0d7c1274653b
jiangshanmeta <540118044@qq.com>
2019-03-04 00:49:30 +0800

    leetcode 653

---
+++ leetcode/653.two-sum-iv-input-is-a-bst.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+// 第一题two sum总该会做吧 O(n)时间复杂度的没难度吧
+var findTarget = function(root, k) {
+    const set = new Set();
+    let flag = false;
+    function inorder(node){
+        node.left && inorder(node.left);
+        
+        if(set.has(k-node.val)){
+            return flag = true;
+        }
+        set.add(node.val);
+        
+        node.right && inorder(node.right);
+    }
+    inorder(root);
+    return flag;
+};
