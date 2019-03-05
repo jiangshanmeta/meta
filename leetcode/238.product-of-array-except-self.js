25bf4182c44662a8c4ec4cd4455853ae05945bce
jiangshanmeta <540118044@qq.com>
2019-03-05 14:16:20 +0800

    leetcode 238

---
+++ leetcode/238.product-of-array-except-self.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    // 这题的难点在于不用除法
+    // 对于第i个节点 对应结果是 前i-1个节点的积 * 后面节点的积
+    const result = new Array(nums.length).fill(1);
+    // 这个循环找到前i-1个节点的积 
+    for(let i=1;i<nums.length;i++){
+        result[i] = result[i-1]*nums[i-1];
+    }
+    
+    // right对应后面节点的积
+    let right = 1;
+    for(let i=nums.length-1;i>-1;i--){
+        result[i] *= right;
+        right *= nums[i];
+    }
+    
+    return result;
+};
