818b79b478679c6f4b9a23ebc1cc9dcacbc2d6c4
jiangshanmeta <540118044@qq.com>
2019-03-21 05:00:22 +0800

    leetcode 709

---
+++ leetcode/709.to-lower-case.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function(str) {
+    const result = new Array(str.length);
+    for(let i=0;i<str.length;i++){
+        const code = str.charCodeAt(i);
+        // 大写字母
+        if(code>64 && code<91){
+            result[i] = String.fromCharCode(code+32);   
+        }else{
+            result[i] = str[i];
+        }
+    }
+    return result.join("");
+};
