a6c647f814593e9ed9ced2f62bbb822e5c7329bb
jiangshanmeta <540118044@qq.com>
2019-03-23 15:43:40 +0800

    leetcode 053

---
+++ leetcode/053.maximum-subarray.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// 最大子列和
+var maxSubArray = function(nums) {
+    let max = -Infinity;
+    let cur = -Infinity;
+    
+    for(let i=0;i<nums.length;i++){
+        if(cur === -Infinity){
+            cur = nums[i];
+        }else{
+            cur += nums[i];
+        }
+        
+        if(cur>max){
+            max = cur;
+        }
+        if(cur<0){
+            cur = -Infinity;
+        }
+    }
+    return max;
+};
