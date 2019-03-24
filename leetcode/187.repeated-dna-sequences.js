28b5175726d2fd972a2c9a8ffa6111c74f9081f6
jiangshanmeta <540118044@qq.com>
2019-03-24 16:30:37 +0800

    leetcode 187

---
+++ leetcode/187.repeated-dna-sequences.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var findRepeatedDnaSequences = function(s) {
+    const result = [];
+    const map = {};
+    for(let i=9;i<s.length;i++){
+        // 取长度为10的子串
+        const sub = s.substring(i-9,i+1);
+        if(!map[sub]){
+            map[sub] = 1;
+        }else{
+            map[sub] === 1 && result.push(sub);
+            map[sub]++;
+        }
+    }
+    return result;
+};
