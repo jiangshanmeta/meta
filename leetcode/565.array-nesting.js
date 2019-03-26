3480ddd47d9a8822fff2e129f006b94a7046a473
jiangshanmeta <540118044@qq.com>
2019-03-27 07:22:46 +0800

    leetcode 565

---
+++ leetcode/565.array-nesting.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// 类似于leetcode 41
+var arrayNesting = function(nums) {
+    let result = 0;
+    for(let i=0;i<nums.length;i++){
+        let count = 1;
+        // 一组nest元素 调整到应有的位置
+        while(nums[i] !== i){
+            const tmp = nums[nums[i]];
+            nums[nums[i]] = nums[i];
+            nums[i] = tmp;
+            count++;
+        }
+        result = Math.max(result,count)
+    }
+    return result;
+};
