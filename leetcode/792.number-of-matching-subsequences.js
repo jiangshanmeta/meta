a06d3707ce2ce29aa3e49186d9f11c66745f1916
jiangshanmeta <540118044@qq.com>
2019-03-30 18:19:49 +0800

    leetcode 792

---
+++ leetcode/792.number-of-matching-subsequences.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} S
+ * @param {string[]} words
+ * @return {number}
+ */
+// 判断是否是子序列(不一定要连续的)
+function isSubsequence(S,sub){
+    let index = -1;
+    for(let i=0;i<sub.length;i++){
+        index = S.indexOf(sub[i],index+1);
+        if(index === -1){
+            return false;
+        }
+    }
+    return true;
+}
+
+var numMatchingSubseq = function(S, words) {
+    let count = 0;
+    for(let i=0;i<words.length;i++){
+        if(words[i].length>S.length){
+            continue;
+        }
+        isSubsequence(S,words[i]) && count++;
+    }
+    return count;
+};
