b2c332f07a7d4be36e529de72cdd4449e4b726bc
jiangshanmeta <540118044@qq.com>
2019-04-04 13:26:05 +0800

    leetcode 398

---
+++ leetcode/398.random-pick-index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ */
+var Solution = function(nums) {
+    this.map = nums.reduce((map,item,index)=>{
+        (map[item] || (map[item] = [])).push(index);
+        return map;
+    },{});
+};
+
+/** 
+ * @param {number} target
+ * @return {number}
+ */
+Solution.prototype.pick = function(target) {
+    const indexs = this.map[target];
+
+    return indexs[Math.floor(Math.random()*indexs.length)];
+};
+
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.pick(target)
+ */
