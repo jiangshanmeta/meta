1c11894daa87c232262f7d49e323a0773f748c58
jiangshanmeta <540118044@qq.com>
2019-04-11 15:00:08 +0800

    leetcode 518

---
+++ leetcode/518.coin-change-2.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function(amount, coins) {
+    // dp[j] 表示要组成j有多少种方式
+    const dp = new Array(amount+1).fill(0);
+    dp[0] = 1;
+    for(let i=0;i<coins.length;i++){
+        const coin = coins[i];
+        // dp[j] 加上以coins[i]为结尾的所有可能性
+        for(let j=coin;j<dp.length;j++){
+            dp[j] += dp[j-coin];
+        }
+    }
+    return dp[amount];
+};
