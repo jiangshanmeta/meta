afb521fdbdee90b6f7abd7dc711f9d8c164892ae
jiangshanmeta <540118044@qq.com>
2019-04-24 00:01:59 +0800

    leetcode 485

---
+++ leetcode/485.max-consecutive-ones.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let max = 0;
+    let count = 0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] === 1){
+            count++;
+            if(count>max){
+                max = count;
+            }
+        }else{
+            count = 0;
+        }
+    }
+    return max;
+};
