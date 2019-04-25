c7c4a82f165a5b2a6892ed83cb0c9ae656d05eb6
jiangshanmeta <540118044@qq.com>
2019-04-25 23:31:59 +0800

    leetcode 118

---
+++ leetcode/118.pascals-triangle.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    if(numRows === 0){
+        return [];
+    }
+    
+    const dp = [
+        [1],  
+    ];
+    for(let i=1;i<numRows;i++){
+        const newRow = [1];
+        const lastRow = dp[dp.length-1];
+        for(let j=0;j<i-1;j++){
+            newRow.push(lastRow[j]+lastRow[j+1]);
+        }
+        newRow.push(1);
+        dp.push(newRow);
+    }
+    
+    return dp;
+};
