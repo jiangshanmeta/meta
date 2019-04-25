cf0d4f2a2ea65f8ecd2e4e88901f936367e2a986
jiangshanmeta <540118044@qq.com>
2019-04-26 01:28:46 +0800

    leetcode 771

---
+++ leetcode/771.jewels-and-stones.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+    const jMap = {};
+    for(let i=0;i<J.length;i++){
+        jMap[J[i]] = true;
+    }
+    let count = 0;
+    for(let i=0;i<S.length;i++){
+        if(jMap[S[i]]){
+            count++;
+        }
+    }
+    return count;
+};
