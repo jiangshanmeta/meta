a8a72caa4319ad37170e6e19b81995a840faf0a5
jiangshanmeta <540118044@qq.com>
2019-05-01 13:28:20 +0800

    letcode 1009

---
+++ leetcode/1009.complement-of-base-10-integer.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} N
+ * @return {number}
+ */
+var bitwiseComplement = function(N) {
+    if(N === 0){
+        return 1;
+    }
+    let base = 1;
+    let result = 0;
+    while(N>0){
+        // 二进制最后一位为0 
+        if(!(N&1)){
+            result += base;
+        }
+        N >>= 1;
+        base <<= 1;
+    }
+    return result;
+};
