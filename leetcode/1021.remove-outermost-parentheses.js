dbc356ed9c448613ac9af7d01b9c819f23cf098d
jiangshanmeta <540118044@qq.com>
2019-05-02 16:58:42 +0800

    leetcode 1021

---
+++ leetcode/1021.remove-outermost-parentheses.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var removeOuterParentheses = function(S) {
+    let result = '';
+    // 左括号个数
+    let leftCount = 0;
+    let startIndex = 0;
+    for(let i=0;i<S.length;i++){
+        if(S[i] === '('){
+            leftCount++;
+        }else{
+            // 遇到最外层对应的右括号
+            if(leftCount === 1){
+                result = `${result}${S.substring(startIndex+1,i)}`;
+                startIndex = i+1;
+            }
+            leftCount--;
+        }
+    }
+    return result;
+};
