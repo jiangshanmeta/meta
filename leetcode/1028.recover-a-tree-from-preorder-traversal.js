f662c9066e82c218626ac52a8137ab11b590274e
jiangshanmeta <540118044@qq.com>
2019-05-05 00:18:20 +0800

    leetcode 1028

---
+++ leetcode/1028.recover-a-tree-from-preorder-traversal.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {string} S
+ * @return {TreeNode}
+ */
+var recoverFromPreorder = function(S) {
+    const stack = [];
+    let index = 0;
+    while(index<S.length){
+        // 解析出节点所在的深度
+        let depth = 0;
+        while(index<S.length && S[index] === '-'){
+            depth++;
+            index++;
+        }
+        // 解析出节点值
+        const start = index;
+        while(index<S.length && S[index] !== '-'){
+            index++;
+        }
+
+        // 把栈中深度>=该节点的都pop出去
+        // 使得栈顶元素为当前元素的父元素
+        while(stack.length>depth){
+            stack.pop();
+        }
+        
+        const node = new TreeNode(S.substring(start,index));
+        if(stack.length){
+            const last = stack[stack.length-1];
+            if(!last.left){
+                last.left = node;
+            }else{
+                last.right = node;
+            }
+        }
+        stack.push(node)
+    }
+
+    return stack[0];
+};
