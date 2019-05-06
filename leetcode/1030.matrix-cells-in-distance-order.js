d14f3a223ef92039a0bec0fd78cd90493395c8ae
jiangshanmeta <540118044@qq.com>
2019-05-06 10:43:45 +0800

    leetcode 1030

---
+++ leetcode/1030.matrix-cells-in-distance-order.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} R
+ * @param {number} C
+ * @param {number} r0
+ * @param {number} c0
+ * @return {number[][]}
+ */
+var allCellsDistOrder = function(R, C, r0, c0) {
+    const matrix = new Array(R);
+    for(let i=0;i<R;i++){
+        matrix[i] = new Array(C).fill(false);
+    }
+    matrix[r0][c0] = true;
+    const result = [];
+    // BFS
+    const queue = [[r0,c0]];
+    while(queue.length){
+        const node = queue.shift();
+        const [x,y] = node;
+        result.push(node);
+        if(x>0 && !matrix[x-1][y]){
+            queue.push([x-1,y]);
+            matrix[x-1][y] = true;
+        }
+        if(y>0 && !matrix[x][y-1]){
+            queue.push([x,y-1]);
+            matrix[x][y-1] = true;
+        }
+        if(x<R-1 && !matrix[x+1][y]){
+            queue.push([x+1,y]);
+            matrix[x+1][y] = true;
+        }
+        if(y<C-1 && !matrix[x][y+1]){
+            queue.push([x,y+1]);
+            matrix[x][y+1] = true;
+        }
+        
+    }
+    
+    return result;
+};
