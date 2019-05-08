fcae5698bc741f61818d934001af77febc812132
jiangshanmeta <540118044@qq.com>
2019-05-09 00:01:38 +0800

    leetcode 747

---
+++ leetcode/0747.largest-number-at-least-twice-of-others.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var dominantIndex = function(nums) {
+    // 找到最大值和次最大值
+    let max = -Infinity;
+    let lessMax = -Infinity;
+    let maxIndex = -1;
+    
+    for(let i=0;i<nums.length;i++){
+        const val = nums[i];
+        if(val>max){
+            lessMax = max;
+            max = val;
+            maxIndex = i;
+        }
+        
+        if(val>lessMax && val<max){
+            lessMax = val;
+        }
+        
+    }
+    
+    if(max>=2*lessMax){
+        return maxIndex;
+    }
+    return -1;
+};
