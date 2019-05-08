9615afdcbe45e1816801aab32453d051362f320b
jiangshanmeta <540118044@qq.com>
2019-05-09 00:01:31 +0800

    leetcode 746

---
+++ leetcode/0746.min-cost-climbing-stairs.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    if(cost.length === 0 || cost.length === 1){
+        return 0;
+    }
+    
+    let a = 0;
+    let b = 0;
+    
+    for(let i=2;i<cost.length+1;i++){
+        let tmp1 = a + cost[i-2];
+        a = b;
+        b = Math.min(tmp1,b + cost[i-1]);
+    }
+    
+    return b;
+};
