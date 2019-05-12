bc342a0b5b58f72755533fc859bf83ce394fcb14
jiangshanmeta <540118044@qq.com>
2019-05-12 10:42:54 +0800

    leetcode 796

---
+++ leetcode/0796.rotate-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {boolean}
+ */
+var rotateString = function(A, B) {
+    if(A.length !== B.length){
+        return false;
+    }
+    return (A+A).includes(B);
+};
