9d7d0ade84ff89b5511cc20232cee927e9e819cc
jiangshanmeta <540118044@qq.com>
2019-05-19 10:16:56 +0800

    leetcode 841

---
+++ leetcode/841.keys-and-rooms.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+var canVisitAllRooms = function(rooms) {
+    const canEnter = new Array(rooms.length).fill(false);
+    canEnter[0] = true;
+    // BFS DFS也一样
+    const queue = [0];
+    
+    while(queue.length){
+        const room = rooms[queue.shift()];
+        for(let i=0;i<room.length;i++){
+           const key = room[i];
+           if(!canEnter[key]){
+               canEnter[key] = true;
+               queue.push(key);
+           }
+        }
+        
+    }
+
+    return canEnter.every((item)=>item);
+};
