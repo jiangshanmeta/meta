1f0064555a892451819b227ef3d6e46412fca17a
jiangshanmeta <540118044@qq.com>
2019-05-21 10:41:24 +0800

    leetcode 848

---
+++ leetcode/848.shifting-letters.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} S
+ * @param {number[]} shifts
+ * @return {string}
+ */
+var shiftingLetters = function(S, shifts) {
+    // 获得第i个字符总共shift的次数
+    for(let i=shifts.length-2;i>-1;i--){
+        shifts[i] += shifts[i+1];
+    }
+    const newCharCode = [];
+    for(let i=0;i<S.length;i++){
+        newCharCode.push((S.charCodeAt(i)-97+shifts[i])%26+97);
+    }
+    
+    return String.fromCharCode(...newCharCode);
+};
