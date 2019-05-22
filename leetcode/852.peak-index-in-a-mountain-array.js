66dce0dacc0de813c47020b9a0d04a87a7a70cf0
jiangshanmeta <540118044@qq.com>
2019-05-22 10:43:35 +0800

    leetcode 852

---
+++ leetcode/852.peak-index-in-a-mountain-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+var peakIndexInMountainArray = function(A) {
+    let low = 0;
+    let high = A.length-1;
+    while(true){
+        const mid = (low+high) >> 1;
+        // 找到山峰了
+        if(A[mid]>A[mid-1] && A[mid]>A[mid+1]){
+            return mid;
+        }
+
+        if(A[mid]<A[mid-1]){
+            // 处于下坡
+            high = mid-1;
+        }else{
+            // 处于上坡
+            low = mid+1;
+        }
+    }
+};
