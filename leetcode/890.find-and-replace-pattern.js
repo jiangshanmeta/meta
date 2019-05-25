608043dc13c895e10613493bde8f2635d6cc98a5
jiangshanmeta <540118044@qq.com>
2019-05-25 11:13:15 +0800

    leetcode  890

---
+++ leetcode/890.find-and-replace-pattern.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string[]} words
+ * @param {string} pattern
+ * @return {string[]}
+ */
+var findAndReplacePattern = function(words, pattern) {
+    const result = [];
+    const Length = pattern.length;
+    for(let i=0;i<words.length;i++){
+        const word = words[i];
+        const word2pattern = {};
+        const pattern2word = {};
+        let flag = true;
+        for(let j=0;j<Length;j++){
+            const wordLetter = word[j];
+            const patternLettern = pattern[j];
+            if(word2pattern[wordLetter] === undefined){
+                if(pattern2word[patternLettern] !== undefined ){
+                    flag = false;
+                    break;
+                }
+                word2pattern[wordLetter] = patternLettern;
+                pattern2word[patternLettern] = wordLetter;
+            }else if(word2pattern[wordLetter] !== patternLettern){
+                flag = false;
+                break;
+            }
+        }
+        flag && result.push(word);
+    }
+    return result;
+};
