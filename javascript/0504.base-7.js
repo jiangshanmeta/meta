e996d22ad6ad7479582f18f553f2160e09eab2a6
jiangshanmeta <540118044@qq.com>
2019-05-30 10:48:13 +0800

    leetcode 504

---
+++ javascript/0504.base-7.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+// 10进制转7进制 除k取余法
+var convertToBase7 = function(num) {
+    if(num === 0){
+        return '0';
+    }
+    const prefix = num>0?'':'-'
+    num = Math.abs(num);
+    let str = '';
+    while(num>0){
+        str = `${num%7}${str}`;
+        num = num/7 | 0;
+    }
+    return prefix+str;
+};
