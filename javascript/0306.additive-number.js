7d403911965266e14ebf193dcd3e7b8e508752b0
jiangshanmeta <540118044@qq.com>
2019-06-01 19:16:11 +0800

    leetcode 306

---
+++ javascript/0306.additive-number.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+
+function check(val1,val2,rest){
+    if(rest === ""){
+        return true;
+    }
+    const val3 = (+val1)+(+val2)+"";
+
+    if(rest.startsWith(val3)){
+        return check(val2,val3,rest.slice(val3.length));
+    }
+    return false;
+}
+
+var isAdditiveNumber = function(num) {
+    // index1是第一个数结束位置(不含)
+    // index2是第二个数结束位置(不含)
+    for(let index1=1;index1<num.length-1;index1++){
+        let val1 = num.slice(0,index1);
+        if(val1.length>1 && val1[0] === "0"){
+            continue;
+        }
+        for(let index2=index1+1;index2<num.length;index2++){
+            let val2 = num.slice(index1,index2);
+            if(val2.length>1 && val2[0] === "0"){
+                continue;
+            }
+            
+            if(check(val1,val2,num.slice(index2))){
+                return true;
+            }
+        }
+    }
+    return false;
+};
