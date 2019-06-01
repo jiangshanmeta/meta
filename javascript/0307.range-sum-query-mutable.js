0f9a32984a925266f5916d6b3ba5e26e46973b33
jiangshanmeta <540118044@qq.com>
2019-06-01 19:16:20 +0800

    leetcode 307

---
+++ javascript/0307.range-sum-query-mutable.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ */
+// 用303的方法暂且解了
+var NumArray = function(nums) {
+    this.nums = nums;
+    const sum = new Array(nums.length);
+    sum[0] = 0;
+    let curSum = 0;
+    for(let i=0;i<nums.length;i++){
+        curSum += nums[i];
+        sum[i+1] = curSum;
+    }
+    this.sum = sum;
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} val
+ * @return {void}
+ */
+NumArray.prototype.update = function(i, val) {
+    const delta = val-this.nums[i];
+    this.nums[i] = val;
+    if(delta){
+        while(i<this.nums.length){
+            this.sum[++i] += delta;
+        }
+    }
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+    return this.sum[j+1]-this.sum[i];
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * obj.update(i,val)
+ * var param_2 = obj.sumRange(i,j)
+ */
