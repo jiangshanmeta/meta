d9a4d3ca08c1abbf2cb14119e2a4e77cfe6fbe94
jiangshanmeta <540118044@qq.com>
2019-06-01 19:16:40 +0800

    leetcode 326

---
+++ javascript/0326.power-of-three.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+// 暴力算的
+var isPowerOfThree = function(n) {
+    if(n<1){
+        return false;
+    }
+    
+    while(n%3===0){
+        n /= 3;
+    }
+    
+    return n === 1;
+    
+};
