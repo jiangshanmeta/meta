a6957408fc00d80b14f57c78997dbb7c7603bd5a
jiangshanmeta <540118044@qq.com>
2019-06-02 10:18:14 +0800

    leetcode 342

---
+++ javascript/0342.power-of-four.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPowerOfFour = function(num) {
+    if(num<=0){
+        return false;
+    }
+    // 判断是否是2的幂
+    if( (num & (num-1)) === 0 ){
+        // 每次右移两位 直到为1或者0
+        // 为1即是4的幂
+        while(num>1){
+            num = num >> 2;
+        }
+        return num === 1
+    }
+    return false;
+};
