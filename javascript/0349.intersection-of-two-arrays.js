70471ca6bdd5b14da521ced80b24e9289e6b263f
jiangshanmeta <540118044@qq.com>
2019-06-02 10:18:22 +0800

    leetcode 349

---
+++ javascript/0349.intersection-of-two-arrays.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let set1 = {};
+    nums1.forEach((num)=>{
+        set1[num] = true;
+    });
+    
+    return nums2.reduce((arr,num)=>{
+        if(set1[num]){
+            arr.push(num);
+            delete set1[num];
+        }
+        return arr;
+    },[]);
+};
