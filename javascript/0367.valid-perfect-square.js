4f8ace73198580c1a9063091aa51de3a24b192aa
jiangshanmeta <540118044@qq.com>
2019-06-03 11:24:40 +0800

    leetcode 367

---
+++ javascript/0367.valid-perfect-square.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    let low = 1;
+    let high = num;
+    // 二分
+    while(low<=high){
+        const mid = Math.floor((low+high)/2);
+        const pingfang = mid*mid;
+        if(pingfang === num){
+            return true;
+        }
+        if(pingfang>num){
+            high = mid-1;
+        }else{
+            low = mid + 1;
+        }
+        
+    }
+    return false;
+};
