4cbe52dea07cdaeb27b2e78d7e98c9e6363082bd
jiangshanmeta <540118044@qq.com>
2019-06-04 10:52:39 +0800

    leetcode 382

---
+++ javascript/0382.linked-list-random-node.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node.
+ * @param {ListNode} head
+ */
+var Solution = function(head) {
+    this.queue = [];
+    let node = head;
+    while (node) {
+        this.queue.push(node);
+        node = node.next;
+    }
+};
+
+/**
+ * Returns a random node's value.
+ * @return {number}
+ */
+Solution.prototype.getRandom = function() {
+    const randomIdx = Math.floor(Math.random() * this.queue.length);
+    return this.queue[randomIdx].val;
+};
+
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = Object.create(Solution).createNew(head)
+ * var param_1 = obj.getRandom()
+ */
