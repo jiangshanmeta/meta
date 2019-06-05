fb2c0c986311adb49440c6bd0317787593e79345
jiangshanmeta <540118044@qq.com>
2019-06-05 10:45:45 +0800

    leetcode 397

---
+++ javascript/0397.integer-replacement.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerReplacement = function(n) {
+    let count = 0;
+    while(n>1){
+        if( (n&1) === 0){
+            count++;
+            n >>>= 1;
+        }else{
+            const m = n >>> 1;
+            if(m>1 && (m&1) === 1){
+                count++;
+                n++;
+            }else{
+                n = m;
+                count += 2;
+            }
+        }
+    }
+    return count;
+};
