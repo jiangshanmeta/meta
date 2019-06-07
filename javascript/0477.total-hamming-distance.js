86e1a14306dc781e9232b655282b12ce1c4f96fe
jiangshanmeta <540118044@qq.com>
2019-06-07 13:30:01 +0800

    leetcode 477

---
+++ javascript/0477.total-hamming-distance.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var totalHammingDistance = function(nums) {
+    let count = 0;
+    const L = nums.length;
+    for(let j=0;j<32;j++){
+        let bitCount = 0;
+        for(let i=0;i<L;i++){
+            bitCount += (nums[i]&1);
+            nums[i] = nums[i] >> 1;
+        }
+        count += bitCount*(L-bitCount);
+    }
+    
+    
+    return count;
+};
