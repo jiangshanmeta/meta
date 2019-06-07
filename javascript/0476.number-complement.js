d009b44bafae4abcc1be76d8c48136c10765e376
jiangshanmeta <540118044@qq.com>
2019-06-07 12:30:01 +0800

    leetcode 476

---
+++ javascript/0476.number-complement.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function(num) {
+    const arr = [];
+    while(num>0){
+        arr.unshift(num&1);
+        num = num >> 1;
+    }
+    const newArr = arr.map((bit)=>{
+        return bit?0:1;
+    });
+    return parseInt(newArr.join(""),2);
+};
