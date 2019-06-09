6adb5ed1d1ac8df6ba7ce8848846ffa014c18546
jiangshanmeta <540118044@qq.com>
2019-06-09 10:58:15 +0800

    leetcode 482

---
+++ javascript/0482.license-key-formatting.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} S
+ * @param {number} K
+ * @return {string}
+ */
+var licenseKeyFormatting = function(S, K) {
+    S = S.replace(/-/g,"").toUpperCase();
+    let arr = [];
+    let mod = S.length%K;
+    let index = mod;
+    if(mod>0){
+        let str = '';
+        for(let i=0;i<mod;i++){
+            str += S[i];
+        }
+        arr.push(str);
+    }
+    
+    while(index<S.length){
+        let str = '';
+        for(let i=0;i<K;i++){
+            str += S[index+i];   
+        }
+        arr.push(str);
+        index += K;
+    }
+    
+    return arr.join("-");
+};
