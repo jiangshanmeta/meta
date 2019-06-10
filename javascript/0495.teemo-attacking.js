384b37b5e0fd53cad60d3936404b3cb1620e9d8d
jiangshanmeta <540118044@qq.com>
2019-06-10 10:59:07 +0800

    leetcode 495

---
+++ javascript/0495.teemo-attacking.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} timeSeries
+ * @param {number} duration
+ * @return {number}
+ */
+var findPoisonedDuration = function(timeSeries, duration) {
+    if(timeSeries.length === 0){
+        return 0;
+    }
+    let lastTime = timeSeries[0];
+    let total = 0;
+    for(let i=1;i<timeSeries.length;i++){
+        total += Math.min(duration,timeSeries[i]-lastTime);
+        lastTime = timeSeries[i];
+    }
+    return total+duration;
+};
