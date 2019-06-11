30270e3b837810314d09b1f93e59c92ad8fac83a
jiangshanmeta <540118044@qq.com>
2019-06-11 10:57:12 +0800

    leetcode 216

---
+++ javascript/0216.combination-sum-iii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number[][]}
+ */
+var combinationSum3 = function(k, n) {
+    function dfs(list,index,sum,sequence){
+        if(list.length === k && sum === n){
+            return sequence.push(list.slice());
+        }
+        
+        if(list.length>k || sum>n || index>9){
+            return;
+        }
+        
+        for(let i=index;i<10;i++){
+            list.push(i);
+            dfs(list,i+1,sum+i,sequence);
+            list.pop();
+        }
+        
+    }
+    const sequence = [];
+    dfs([],1,0,sequence);
+    return sequence;
+};
