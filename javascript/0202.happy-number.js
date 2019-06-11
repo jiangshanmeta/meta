0dd2fbd501740b63c64abfb52dca63aba87a3ea7
jiangshanmeta <540118044@qq.com>
2019-06-11 10:57:05 +0800

    leetcode 202

---
+++ javascript/0202.happy-number.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    if(n === 1){
+        return true;
+    }
+    function helper(n,hash){
+        let sum = 0;
+        while(n>0){
+            sum += (n%10) ** 2;
+            n = n/10 | 0;
+        }
+        if(sum === 1){
+            return true;
+        }
+        
+        if(hash[sum]){
+            return false;
+        }
+        hash[sum] = true;
+        return helper(sum,hash);
+    }
+
+    return helper(n,{[n]:true})
+};
