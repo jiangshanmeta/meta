68bd83ae5762f1ad0d7af9ee256cc980f117f3e8
jiangshanmeta <540118044@qq.com>
2019-06-13 10:50:25 +0800

    leetcode 217

---
+++ javascript/0217.contains-duplicate.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    // return nums.length !== new Set(nums).size
+    const set = new Set();
+    return nums.some((num)=>{
+        if(set.has(num)){
+            return true;
+        }else{
+            set.add(num);
+        }
+        
+    });
+    
+};
