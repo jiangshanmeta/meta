4a814cb727798dff5bd6c33a2d9490c607e73ef8
jiangshanmeta <540118044@qq.com>
2019-06-13 10:50:35 +0800

    leetcode 219

---
+++ javascript/0219.contains-duplicate-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    k++;
+    const map = new Map();
+    return nums.some((num,index)=>{
+        if(!map.has(num)){
+            map.set(num,index);
+            return false;
+        }
+        const lastIndex = map.get(num);
+        if(index-lastIndex<k){
+            return true;
+        }
+        map.set(num,index);
+        
+    });
+};
