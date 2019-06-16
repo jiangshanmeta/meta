462394245a6583f82d3d59b1e55dea38e2cddca9
jiangshanmeta <540118044@qq.com>
2019-06-16 12:37:31 +0800

    leetcode 260

---
+++ javascript/0260.single-number-iii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var singleNumber = function(nums) {
+    let bitSum = 0;
+    for(let i=0;i<nums.length;i++){
+        bitSum ^= nums[i];
+    }
+    
+    let ruler = 1;
+    while((bitSum & ruler) === 0){
+        ruler <<= 1;
+    }
+    
+    let a = 0;
+    for(let i=0;i<nums.length;i++){
+        if((nums[i]&ruler) !== 0){
+            a ^= nums[i]
+        }
+    }
+    
+    return [a,bitSum^a];
+};
