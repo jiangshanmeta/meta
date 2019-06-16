3fc9875bceccf16d26d63c60517ce3be2d1f574a
jiangshanmeta <540118044@qq.com>
2019-06-16 12:37:39 +0800

    leetcode 263

---
+++ javascript/0263.ugly-number.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isUgly = function(num) {
+    if(num < 1){
+        return false; 
+    }
+    
+    while((num&1) === 0){
+        num >>= 1;
+    }
+    
+    while(num%3===0){
+        num /= 3;
+    }
+    
+    while(num%5 === 0){
+        num /= 5;
+    }
+
+    if(num == 1){
+        return true;
+    }
+    return false;
+};
