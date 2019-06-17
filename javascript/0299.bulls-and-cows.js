1929c3d8317360c09853f756c78ca351a3c111f1
jiangshanmeta <540118044@qq.com>
2019-06-17 09:52:12 +0800

    leetcode 299

---
+++ javascript/0299.bulls-and-cows.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} secret
+ * @param {string} guess
+ * @return {string}
+ */
+var getHint = function(secret, guess) {
+    const secretMap = {};
+    const guessMap = {};
+    let bull = 0;
+    
+    for(let i=0;i<secret.length;i++){
+        if(secret[i] !== guess[i]){
+            if(secretMap[secret[i]] === undefined){
+                secretMap[secret[i]] = 1;
+            }else{
+                secretMap[secret[i]]++;
+            }
+            if(guessMap[guess[i]] === undefined){
+                guessMap[guess[i]] = 1;
+            }else{
+                guessMap[guess[i]]++;
+            }
+        }else{
+            bull++;
+        }
+    }
+    let cow = 0;
+    Object.keys(guessMap).forEach((num)=>{
+        cow += Math.min(guessMap[num],secretMap[num] || 0);
+    });
+    
+    return `${bull}A${cow}B`;
+};
