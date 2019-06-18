2df107929dee6e4ae454e764ae77d41738d3cc9b
jiangshanmeta <540118044@qq.com>
2019-06-18 10:51:29 +0800

    leetcode 532

---
+++ javascript/0532.k-diff-pairs-in-an-array.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findPairs = function(nums, k) {
+    if(k<0){
+        return 0;
+    }
+    
+    const map = {};
+    let count = 0;
+    
+    if(k === 0){
+        for(let i=0;i<nums.length;i++){
+            const num = nums[i];
+            if(map[num] === undefined){
+                map[num] = 1;
+            }else{
+                map[num]++;
+                map[num] === 2 && count++;
+            }
+        }
+        return count;
+    }
+    
+    
+    for(let i=0;i<nums.length;i++){
+        const num = nums[i];
+        if(map[num] === undefined){
+            map[num] = 1;
+        }else{
+            map[num]++;
+        }
+    }
+    
+    nums = [...new Set(nums)];
+    let index = 0;
+    for(let i=0;i<nums.length;i++){
+        if(map[nums[i]+k] !== undefined){
+            count++
+        }
+    }
+    
+    return count;   
+};
