5a66ee96fb84431a9097ef7cc30d137f67b15d9b
jiangshanmeta <540118044@qq.com>
2019-06-22 11:48:13 +0800

    leetcode 594

---
+++ javascript/0594.longest-harmonious-subsequence.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLHS = function(nums) {
+    const map = {};
+    const numTypes = [];
+    for(let i=0;i<nums.length;i++){
+        const num = nums[i];
+        if(map[num] === undefined){
+            map[num] = 1;
+            numTypes.push(num);
+        }else{
+            map[num]++;
+        }
+    }
+
+    let max = 0;
+    for(let i=0;i<numTypes.length;i++){
+        const curNum = numTypes[i];
+        const curCount = map[curNum]; 
+        if(map[curNum+1] !== undefined){
+            const nextCount = map[curNum+1];
+            if(curCount+nextCount>max){
+                max = curCount+nextCount;
+            }
+            
+        }
+        
+    }
+    
+    return max;
+};
