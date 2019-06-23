03770fc094657ed92f2c010056a3e4ffa832ae3c
jiangshanmeta <540118044@qq.com>
2019-06-23 12:10:19 +0800

    leetcode 598

---
+++ javascript/0598.range-addition-ii.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} ops
+ * @return {number}
+ */
+// 即求每个op都碰到的区域
+// 即x，y的最小值
+var maxCount = function(m, n, ops) {
+    for(let i=0;i<ops.length;i++){
+        const x = ops[i][0];
+        if(x<m){
+            m = x;
+            if(x === 1){
+                break;
+            }
+        }
+    }
+    
+    for(let i=0;i<ops.length;i++){
+        const y = ops[i][1];
+        if(y<n){
+            n = y;
+            if(y === 1){
+                break;
+            }
+        }
+    }
+    
+    
+    
+    return m*n;
+};
