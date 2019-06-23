33e60bd7c248e2ec1e3d621719b897339efac1df
jiangshanmeta <540118044@qq.com>
2019-06-23 12:10:34 +0800

    leetcode 657

---
+++ javascript/0657.robot-return-to-origin.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+// 无脑算就好了
+var judgeCircle = function(moves) {
+    let x = 0 ,y = 0;
+    for(let i=0;i<moves.length;i++){
+        switch(moves[i]){
+            case 'R':
+                x++;
+                break;
+            case 'L':
+                x--;
+                break;
+            case 'U':
+                y++;
+                break;
+            case 'D':
+                y--;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    return x === 0 && y === 0;
+};
