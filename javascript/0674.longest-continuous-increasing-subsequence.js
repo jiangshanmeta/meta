d2a4a9e926e366c628906a2e7150a384e7935ad4
jiangshanmeta <540118044@qq.com>
2019-06-24 10:35:11 +0800

    leetcode 674

---
+++ javascript/0674.longest-continuous-increasing-subsequence.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLengthOfLCIS = function(nums) {
+    let startIndex = 0;
+    let maxCount = 0;
+    while(startIndex<nums.length){
+        let lastVal = nums[startIndex];
+        let endIndex = startIndex+1;
+        while(endIndex<nums.length && nums[endIndex]>lastVal){
+            lastVal = nums[endIndex];
+            endIndex++;
+        }
+        let count = endIndex-startIndex;
+        if(count>maxCount){
+            maxCount = count;
+        }
+        
+        startIndex = endIndex;
+    }
+    
+    return maxCount;
+};
