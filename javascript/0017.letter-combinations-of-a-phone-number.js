f69b607b1f738d38d58a6f2e75af6684d37c95d7
jiangshanmeta <540118044@qq.com>
2019-06-26 10:46:08 +0800

    leetcode 17

---
+++ javascript/0017.letter-combinations-of-a-phone-number.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    var map = {
+        // 0:[""],
+        // 1:[""],
+        2:["a","b","c"],
+        3:["d","e","f"],
+        4:["g","h","i"],
+        5:["j","k","l"],
+        6:["m","n","o"],
+        7:["p","q","r","s"],
+        8:["t","u","v"],
+        9:["w","x","y","z"],
+    }
+    var digitsArr = digits.split("").filter(function(item){
+        return item>1;
+    })
+    var rst = [];
+    var len = digitsArr.length;
+    if(!len){
+        return rst;
+    }
+    inner("",0);
+    console.log(digitsArr)
+    function inner(prefix,index){
+        if(index<len){
+            for(var i=0;i<map[digitsArr[index]].length;i++){
+                inner(prefix+map[digitsArr[index]][i],index+1);
+            }
+        }else{
+            rst.push(prefix);
+        }
+    }
+    return rst;
+};
