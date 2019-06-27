5e7bf333031ccefe6fed27c3a0c066310fe70946
jiangshanmeta <540118044@qq.com>
2019-06-27 10:50:34 +0800

    leetcode 27

---
+++ javascript/0027.remove-element.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let index = nums.length-1;
+    while(index>-1){
+        if(nums[index] === val){
+            nums.splice(index,1);
+        }
+        index--;
+    }
+    return nums.length;
+};
