164c5b5598c4bc13db7d9444d098395901d265d9
jiangshanmeta <540118044@qq.com>
2019-07-01 00:10:50 +0800

    leetcode 49

---
+++ javascript/0049.group-anagrams.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const hashMap = strs.reduce((obj,item)=>{
+        const key = item.split('').sort().join('');
+        (obj[key] || (obj[key] = [])).push(item)
+        return obj;
+    },{});
+    return Object.values(hashMap);
+};
