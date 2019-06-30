8122d85915fd79eafb9e97ba425104c87f1a9e4a
jiangshanmeta <540118044@qq.com>
2019-06-30 11:01:38 +0800

    leetcode 35

---
+++ javascript/0035.search-insert-position.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    // 二分
+    let low = 0;
+    let high = nums.length-1;
+    let index = nums.length;
+    while(low<=high){
+        const mid = (low+high) >> 1;
+        if(nums[mid]>=target){
+            index = Math.min(index,mid);
+            high = mid-1;
+        }else{
+            low = mid+1;
+        }
+    }
+    return index;
+};
