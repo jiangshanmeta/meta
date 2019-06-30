42cc786f836ddce3df6d9ed4e46a5d7634d817e6
jiangshanmeta <540118044@qq.com>
2019-06-30 11:02:06 +0800

    leetcode 38

---
+++ javascript/0038.count-and-say.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+    var rst = "1";
+    var arr;
+    var last;
+    var counter;
+    for(var i = 1;i<n;i++){
+        last = rst[0];
+        arr = [];
+        counter = 1;
+        for(var j = 1;j<rst.length;j++){
+            if(rst[j] === last){
+                counter++;
+            }else{
+                arr.push(counter,last);
+                last = rst[j];
+                counter = 1;
+            }
+        }
+        arr.push(counter,last);
+        rst = arr.join("");
+    }
+    return rst;
+};
