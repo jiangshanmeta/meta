8a74037a6fc896590b92e81eddd9521d35f14e60
jiangshanmeta <540118044@qq.com>
2019-07-02 10:29:08 +0800

    leetcode 60

---
+++ javascript/0060.permutation-sequence.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+
+const cache = {
+    0:1,
+    1:1,
+};
+
+function getFactorial(n){
+    if(!cache[n]){
+        cache[n] = getFactorial(n-1)*n;
+    }
+    return cache[n];
+}
+
+
+var getPermutation = function(n, k) {
+    const arr = Array.apply(null,{length:n}).map((item,index)=>index+1);
+    
+    k--;
+    n--;
+    
+    let rst = "";
+    
+    while(k>0){
+        let factorial = getFactorial(n);
+        let index = Math.floor(k/factorial);
+        k -= factorial*index;
+        rst += arr[index];
+        arr.splice(index,1);
+        n--;
+    }
+    
+    return rst + arr.join("")
+    
+};
