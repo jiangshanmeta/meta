114614c68ba3180e8d108587dbb388652959ee88
jiangshanmeta <540118044@qq.com>
2019-07-02 10:27:45 +0800

    leetcode 58

---
+++ javascript/0058.length-of-last-word.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    const arr = s.split(" ");
+    let index = arr.length;
+    while(index--){
+        const item = arr[index];
+        if(item){
+            return item.length;
+        }
+    }
+    
+    return 0;
+};
