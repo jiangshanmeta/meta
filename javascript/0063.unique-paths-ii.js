94ed485ff06d60dbb5d5f3766ab9ebe884fafe47
jiangshanmeta <540118044@qq.com>
2019-07-03 11:36:21 +0800

    leetcode 63

---
+++ javascript/0063.unique-paths-ii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    // 62 unique path的变形
+    const row = obstacleGrid.length;
+    const column = obstacleGrid[0].length;
+    let dp = new Array(column).fill(1);
+    let index = obstacleGrid[0].indexOf(1);
+    if(index>-1){
+        for(let i=index;i<column;i++){
+            dp[i] = 0;
+        }
+    }
+    
+    for(let i=1;i<row;i++){
+        if(obstacleGrid[i][0] === 1){
+            dp[0] = 0;
+        }
+        for(let j=1;j<column;j++){
+            if(obstacleGrid[i][j] === 1){
+                dp[j] = 0;
+            }else{
+                dp[j] += dp[j-1];
+            }
+        }
+    }
+    return dp[column-1];
+};
