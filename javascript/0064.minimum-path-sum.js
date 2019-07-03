459696a3ce2cbf32bdc2a97381088926526adb18
jiangshanmeta <540118044@qq.com>
2019-07-03 11:36:28 +0800

    leetcode 64

---
+++ javascript/0064.minimum-path-sum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+    // 很像 62 unique path
+    let dp = grid[0];
+    const column = dp.length;
+    for(let i=1;i<column;i++){
+        dp[i] += dp[i-1];
+    }
+    
+    for(let i=1;i<grid.length;i++){
+        dp[0] += grid[i][0];
+        for(let j=1;j<column;j++){
+            dp[j] = Math.min(dp[j-1],dp[j])+grid[i][j];
+        }
+    }
+    
+    return dp[column-1];
+};
