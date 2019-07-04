117cd48ab09f68d483efe9aa1526be9cfb6e0f60
jiangshanmeta <540118044@qq.com>
2019-07-04 11:01:59 +0800

    leetcode 1095 js

---
+++ javascript/1095.find-in-mountain-array.js
@@ -0,0 +1,60 @@
+/**
+ * @param {number} target
+ * @param {MountainArray} mountainArr
+ * @return {number}
+ */
+var findInMountainArray = function(target, mountainArr) {
+    const L = mountainArr.length();
+    // 找到峰顶
+    let peakIndex;
+    let low = 0;
+    let high = L-1;
+    while(low<=high){
+        const mid = (low+high) >> 1;
+        const midVal = mountainArr.get(mid);
+        const rightVal = mountainArr.get(mid+1);
+        if(midVal<rightVal){
+            low = mid+1;
+        }else {
+            const leftVal = mountainArr.get(mid-1);
+            if(midVal>leftVal){
+                peakIndex = mid;
+                break;
+            }else{
+                high = mid-1;
+            }
+        }
+    }
+    
+    // 在左边找
+    low = 0;
+    high = peakIndex;
+    while(low<=high){
+        const mid = (low+high) >> 1;
+        const midVal = mountainArr.get(mid);
+        if(midVal === target){
+            return mid;
+        }else if(midVal<target){
+            low = mid+1
+        }else{
+            high = mid-1
+        }
+    }
+
+    // 在右边找
+    low = peakIndex;
+    high = L-1;
+    while(low<=high){
+        const mid = (low+high) >> 1;
+        const midVal = mountainArr.get(mid);
+        if(midVal === target){
+            return mid;
+        }else if(midVal<target){
+            high = mid-1;
+        }else{
+            low = mid+1;
+        }
+    }
+    // 都没有
+    return -1;
+};
