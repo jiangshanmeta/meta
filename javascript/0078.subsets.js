04bba08b3369e0bb5a6d6c0b1747f097581b90fb
jiangshanmeta <540118044@qq.com>
2019-07-08 10:55:56 +0800

    leetcode 78

--- javascript/0078.subsets.js
+++ javascript/0078.subsets.js
@@ -1,22 +1,18 @@
-// 不解释
-
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
 var subsets = function(nums) {
-    var subsets = function(nums) {
-        function backTracking(list,nums,sequence,index){
-            sequence.push(list.slice());
-            for(let i=index;i<nums.length;i++){
-                list.push(nums[i]);
-                backTracking(list,nums,sequence,i+1);
-                list.pop();
-            }
-            
+    function backTracking(list,nums,sequence,index){
+        sequence.push(list.slice());
+        for(let i=index;i<nums.length;i++){
+            list.push(nums[i]);
+            backTracking(list,nums,sequence,i+1);
+            list.pop();
         }
-        const sequence = [];
-        backTracking([],nums,sequence,0);
-        return sequence;
-    };
+        
+    }
+    const sequence = [];
+    backTracking([],nums,sequence,0);
+    return sequence;
 };
