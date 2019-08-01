185274147622d702be56e409991a929b3a27efbb
jiangshanmeta <540118044@qq.com>
2019-08-01 12:57:54 +0800

    leetcode 1134

---
+++ javascript/1134.armstrong-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} N
+ * @return {boolean}
+ */
+// 按照题目
+var isArmstrong = function(N) {
+    const k = (''+N).length;
+    let sum = 0;
+    let copyN = N;
+    while(copyN>0){
+        const digit = copyN%10;
+        sum += digit**k;
+        if(sum>N){
+            return false;
+        }
+        copyN = (copyN-digit)/10;
+    }
+    return sum === N;
+};
