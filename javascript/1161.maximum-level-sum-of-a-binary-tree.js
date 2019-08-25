dc47512bc4fcf217bcdf8ccea3fe8bd26b77c32f
jiangshanmeta <540118044@qq.com>
2019-08-25 15:12:56 +0800

    leetcode 1161

---
+++ javascript/1161.maximum-level-sum-of-a-binary-tree.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxLevelSum = function(root) {
+    // 层序遍历
+    let maxLevel = 0;
+    let maxSum = -Infinity;
+    let level = 1;
+    let nodes = [root];
+    while(nodes.length){
+        let sum = 0;
+        const nextLevel = [];
+        for(let i=0;i<nodes.length;i++){
+            nodes[i].left && nextLevel.push(nodes[i].left);
+            nodes[i].right && nextLevel.push(nodes[i].right);
+            sum += nodes[i].val;
+        }
+        if(sum>maxSum){
+            maxLevel = level;
+            maxSum = sum;
+        }
+        level++;
+        nodes = nextLevel;
+    }
+    return maxLevel
+};
