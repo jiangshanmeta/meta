e3db6b3a30f756943575ffa4886cd4dfea22b0b6
jiangshanmeta <540118044@qq.com>
2019-09-01 15:15:23 +0800

    leetcode 204

---
+++ javascript/0204.count-primes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    const primes = new Array(n).fill(true);
+    let count = 0;
+    for(let i=2;i<n;i++){
+        if(!primes[i]){
+            continue;
+        }
+        count++;
+        // 如果一个数是质数，那么它的倍数j不是质数
+        for(let j=i*2;j<n;j+=i){
+            primes[j] = false;
+        }
+    }
+    return count;
+};
