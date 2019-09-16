f8ea0586f3fbb4985fb5f956aaa4e9e323aad570
jiangshanmeta <540118044@qq.com>
2019-09-16 11:11:15 +0800

    leetcode 1190

---
+++ javascript/1190.reverse-substrings-between-each-pair-of-parentheses.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseParentheses = function(s) {
+    let index = 0;
+    function parse(){
+        const result = [];
+        while(index<s.length && s[index] !== ')'){
+            if(s[index] === '('){
+                index++;
+                // 处理子括号
+                result.push(...parse().reverse().join(''))
+            }else{
+                result.push(s[index++])
+            }
+        }
+        index++;
+        return result;
+    }
+    return parse().join('');
+};
