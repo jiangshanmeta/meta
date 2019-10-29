9655dc445f1120520000e5a5d6c9869096ec28fc
jiangshanmeta <540118044@qq.com>
2019-10-30 00:05:41 +0800

    leetcode 50

--- javascript/0050.powx-n.js
+++ javascript/0050.powx-n.js
@@ -4,21 +4,23 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-    if(n === 0){
+    if (n === 0 || x === 1){
         return 1;
     }
-    
-    if(n === 1){
-        return x;
+    if(x === 0){
+        return 0;
     }
-
-
     if(n<0){
-        return 1/myPow(x,-n);
+        x = 1/x;
+        n = -n;
     }
-    
-    if(n%2 === 0){
-        return myPow(x*x,n/2);
+    let result = 1;
+    while(n){
+        if(n&1){
+            result = result*x;
+        }
+        n >>>= 1;
+        x *= x;
     }
-    return myPow(x*x,(n-1)/2)*x;
+    return result;
 }
