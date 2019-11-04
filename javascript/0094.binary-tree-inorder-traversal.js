9589ef9d5c9e13ac80a0a39c37ea6935aa60b7a4
jiangshanmeta <540118044@qq.com>
2019-11-05 00:20:36 +0800

    leetcode 94

--- javascript/0094.binary-tree-inorder-traversal.js
+++ javascript/0094.binary-tree-inorder-traversal.js
@@ -1,5 +1,4 @@
 // 中序遍历不解释
-
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -11,6 +10,7 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+// 递归实现
 var inorderTraversal = function(root) {
     if(!root){
         return [];
@@ -24,4 +24,24 @@ var inorderTraversal = function(root) {
     const sequence = [];
     inorder(root,sequence);
     return sequence;
+};
+
+// 非递归实现
+var inorderTraversal = function(root) {
+    const result = [];
+    const stack = [];
+    while(root){
+        stack.push(root);
+        root = root.left;
+    }
+    while(stack.length){
+        let node = stack.pop();
+        result.push(node.val);
+        node = node.right;
+        while(node){
+            stack.push(node);
+            node = node.left;
+        }
+    }
+    return result;
 };
