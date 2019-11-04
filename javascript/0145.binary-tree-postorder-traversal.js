95dc7eb202ece80b2b0d107d89449f4f50065bc5
jiangshanmeta <540118044@qq.com>
2019-11-05 00:20:50 +0800

    leetcode 145

--- javascript/0145.binary-tree-postorder-traversal.js
+++ javascript/0145.binary-tree-postorder-traversal.js
@@ -24,3 +24,32 @@ var postorderTraversal = function(root) {
     root && postorderTraversalSequence(root,sequence);
     return sequence;
 };
+
+// 非递归实现
+var postorderTraversal = function(root) {
+    const stack1 = [];
+    const stack2 = [];
+    while(root){
+        stack1.push(root);
+        root = root.left;
+    }
+    const result = [];
+    while(stack1.length){
+        root = stack1.pop();
+        // 说明此时是第二次遇到
+        if(stack2.length && stack2[stack2.length-1] === root){
+            result.push(root.val);
+            stack2.pop();
+        }else{
+            // 第一次遇到该节点
+            stack1.push(root);
+            stack2.push(root);
+            root = root.right;
+            while(root){
+                stack1.push(root);
+                root = root.left;
+            }
+        }
+    }
+    return result;
+};
