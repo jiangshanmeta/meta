6e1a15329c6b54a2914da851b1dae5dd866dad5e
jiangshanmeta <540118044@qq.com>
2019-11-13 13:50:07 +0800

    leetcode 1233

---
+++ javascript/1233.remove-sub-folders-from-the-filesystem.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} folder
+ * @return {string[]}
+ */
+var removeSubfolders = function(folder) {
+    // 先排序 这样子目录会紧挨着父目录
+    folder.sort();
+    const result = [];
+    for(let i=0;i<folder.length;i++){
+        // 判断前一个是否是父目录 注意这个/ 因为有 /a /ab 这种情况  
+        if(result.length === 0 || !folder[i].startsWith(result[result.length-1]+'/' ) ){
+            result.push(folder[i])
+        }
+    }
+    return result;
+};
