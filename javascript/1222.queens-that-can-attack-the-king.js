ea567fe9748a2b02a5b4319f23fc908a7ab4a7c2
jiangshanmeta <540118044@qq.com>
2019-11-15 12:14:38 +0800

    leetcode 1222

---
+++ javascript/1222.queens-that-can-attack-the-king.js
@@ -0,0 +1,50 @@
+/**
+ * @param {number[][]} queens
+ * @param {number[]} king
+ * @return {number[][]}
+ */
+var queensAttacktheKing = function(queens, king) {
+    const queensMap = {};
+    const deltas = [
+        [0,1],
+        [0,-1],
+        [1,0],
+        [-1,0],
+        [1,1],
+        [-1,-1],
+        [1,-1],
+        [-1,1],
+    ];
+    for(let i=0;i<queens.length;i++){
+        const [x,y] = queens[i];
+        queensMap[`${x},${y}`] = true;
+    }
+
+    const result = [];
+    const usedQueenMap = {};
+
+    const [x,y] = king;
+    // 在各个方向寻找最近的Queen
+    for(let j=0;j<deltas.length;j++){
+        let curX = x;
+        let curY = y;
+        const [deltax,deltay] = deltas[j];
+        while(curX+deltax>-1 && curX+deltax<8 && curY+deltay>-1 && curY+deltay<8){
+            curX += deltax;
+            curY += deltay;
+            if(usedQueenMap[`${curX},${curY}`]){
+                break;
+            }
+            if(queensMap[`${curX},${curY}`]){
+                usedQueenMap[`${curX},${curY}`] = true;
+                result.push([curX,curY]);
+                break;
+            }
+            
+        }
+        
+    }
+        
+    
+    return result;
+};
