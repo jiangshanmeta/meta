cd438a9b81b79e2f0f6445a086222335c472b460
jiangshanmeta <540118044@qq.com>
2019-11-16 12:49:07 +0800

    leetcode 1247

---
+++ javascript/1247.minimum-swaps-to-make-strings-equal.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {number}
+ */
+var minimumSwap = function(s1, s2) {
+    let count = 0;
+    let xy = 0;
+    let yx = 0;
+    for(let i=0;i<s1.length;i++){
+        if(s1[i] === s2[i]){
+            continue;
+        }
+        if(s1[i] === 'x'){
+            xy++;
+        }else{
+            yx++;
+        }
+        count++;
+    }
+    if(count&1){
+        return -1;
+    }
+    let result = 0;
+    result += (xy>>1);
+    xy %= 2;
+    result += (yx>>1);
+    yx %= 2;
+    if(xy && yx){
+        result += 2;
+    }
+    return result;
+};
