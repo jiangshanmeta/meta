f57d21b8a2b2b5304a4db79c41b88a146aa0d243
jiangshanmeta <540118044@qq.com>
2019-11-16 12:48:05 +0800

    leetcode 1248

---
+++ javascript/1248.count-number-of-nice-subarrays.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfSubarrays = function(nums, k) {
+    // 把所有的奇数对应的索引放这
+    const indexs = [-1];
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]&1){
+            indexs.push(i);
+        }
+    }
+    indexs.push(nums.length);
+    // 有点类似滑动窗口了
+    let result = 0;
+    for(let i=k;i<indexs.length-1;i++){
+        const start = i+1-k;
+        result += (indexs[start]-indexs[start-1])*(indexs[i+1]-indexs[i]);
+    }
+    return result;
+};
