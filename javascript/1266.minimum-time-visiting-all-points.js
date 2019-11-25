96d17047313266bcdad039ea3808d76297d0da06
jiangshanmeta <540118044@qq.com>
2019-11-25 13:21:17 +0800

    leetcode 1266

---
+++ javascript/1266.minimum-time-visiting-all-points.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minTimeToVisitAllPoints = function(points) {
+    let result = 0;
+    for(let i=1;i<points.length;i++){
+        const deltax = Math.abs(points[i][0]-points[i-1][0]);
+        const deltay = Math.abs(points[i][1]-points[i-1][1]);
+        // 优先走斜路，再尝试水平垂直走
+        result += Math.max(deltax,deltay);
+    }
+    return result;
+};
