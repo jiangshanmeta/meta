0facbb881f6b377b3412cb45591cb68bf5375290
jiangshanmeta <540118044@qq.com>
2019-12-08 15:21:15 +0800

    leetcode 1281

---
+++ javascript/1281.subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+    let sum = 0;
+    let product = 1;
+    while(n){
+        const digit = n%10;
+        sum += digit;
+        product *= digit;
+        n = (n-digit)/10;
+    }
+    return product-sum;
+};
