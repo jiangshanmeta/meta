976bc423e55c28f00dcdf370835d1f97871bea36
jiangshanmeta <540118044@qq.com>
2019-12-08 15:21:34 +0800

    leetcode 1283

---
+++ javascript/1283.find-the-smallest-divisor-given-a-threshold.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} threshold
+ * @return {number}
+ */
+var smallestDivisor = function(nums, threshold) {
+    let low = 1;
+    let high = Math.max(...nums);
+    let result = high;
+    while(low<=high){
+        const mid = (low+high)>>1;
+        let sum = 0;
+        for(let i=0;i<nums.length;i++){
+            sum += Math.ceil(nums[i]/mid);
+        }
+        if(sum>threshold){
+            low = mid+1;
+        }else{
+            result = Math.min(result,mid);
+            high = mid-1;
+        }
+        
+    }
+    
+    return result;
+};
