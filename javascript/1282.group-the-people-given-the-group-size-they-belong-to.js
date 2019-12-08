e0fe6ea2a0a4b962c9883e8835a35da64bed9ada
jiangshanmeta <540118044@qq.com>
2019-12-08 15:21:25 +0800

    leetcode 1282

---
+++ javascript/1282.group-the-people-given-the-group-size-they-belong-to.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} groupSizes
+ * @return {number[][]}
+ */
+var groupThePeople = function(groupSizes) {
+    const result = [];
+    const groupBySize = {};
+    for(let i=0;i<groupSizes.length;i++){
+        const size = groupSizes[i];
+        (groupBySize[size] || (groupBySize[size] = [])).push(i);
+        if(groupBySize[size].length === size){
+            result.push(groupBySize[size]);
+            groupBySize[size] = null;
+        }
+    }
+    
+    return result;
+};
