91962192b2f4a3a4e24ab4eb087adf830738a2e9
jiangshanmeta <540118044@qq.com>
2019-12-17 00:07:44 +0800

    leetcode 974

---
+++ javascript/0974.subarray-sums-divisible-by-k.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
+var subarraysDivByK = function(A, K) {
+    // 参考leetcode 560
+    const modCount = new Array(K).fill(0);
+    modCount[0] = 1;
+    let sum = 0;
+    let result = 0;
+    for(let i=0;i<A.length;i++){
+        sum += A[i];
+        let mod = sum%K;
+        if(mod<0){
+            mod += K;
+        }
+        result += modCount[mod];
+        modCount[mod]++;
+    }
+    return result;
+};
