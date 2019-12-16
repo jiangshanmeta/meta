8555f91778aabffb427249fdf0f4a33fbc8d022f
jiangshanmeta <540118044@qq.com>
2019-12-17 00:07:35 +0800

    leetcode 714

---
+++ javascript/0714.best-time-to-buy-and-sell-stock-with-transaction-fee.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} prices
+ * @param {number} fee
+ * @return {number}
+ */
+var maxProfit = function(prices, fee) {
+    // dp
+    let result = 0;
+    let buy = -prices[0];
+    let sell = 0;
+    let colldown = 0;
+    for(let i=1;i<prices.length;i++){
+        const newBuy = Math.max(buy,sell-prices[i],colldown-prices[i]);
+        const newSell = buy+prices[i]-fee;
+        const newColldown = Math.max(colldown,sell);
+        result = Math.max(result,newBuy,newSell,newColldown);
+        buy = newBuy;
+        sell = newSell;
+        colldown = newColldown;
+    }
+    return result;
+};
