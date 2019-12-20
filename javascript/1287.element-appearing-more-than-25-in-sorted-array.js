ba926ffea2b2526c13568e3558b6925e457568df
jiangshanmeta <540118044@qq.com>
2019-12-21 03:37:01 +0800

    leetcode 1287

---
+++ javascript/1287.element-appearing-more-than-25-in-sorted-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var findSpecialInteger = function(arr) {
+    // 其实可以用二分 偷懒了
+    let oneFourth = arr.length/4;
+    let element;
+    let count = 0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] === element){
+            count++;
+        }else{
+            count = 1;
+            element = arr[i];
+        }
+        if(count>oneFourth){
+            return element;
+        }
+    }
+};
