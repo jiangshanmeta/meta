9bd6150aad9bde5f907e076ea0887c853b756f01
jiangshanmeta <540118044@qq.com>
2019-12-21 03:37:10 +0800

    leetcode 1288

---
+++ javascript/1288.remove-covered-intervals.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var removeCoveredIntervals = function(intervals) {
+    intervals.sort((a,b)=>a[0]-b[0]);
+    let index = 1;
+    while(index<intervals.length){
+        let index2 = index-1;
+        let found = false;
+        while(index2>-1 && intervals[index2][1]>=intervals[index][1]){
+            if(intervals[index2][0]<=intervals[index][0]){
+                found = true;
+                break;
+            }
+        }
+        if(found){
+            intervals.splice(index,1);
+        }else{
+            index++;
+        }
+        
+    }
+    
+    return intervals.length;
+};
