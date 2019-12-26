4455815133fe64a9118231609fbefd097d16fd79
jiangshanmeta <540118044@qq.com>
2019-12-26 14:17:35 +0800

    leetcode 154

---
+++ javascript/0154.find-minimum-in-rotated-sorted-array-ii.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    // left 左侧增长序列最大值
+    // right 右侧增长序列最小值
+    let left = 0;
+    let right = nums.length-1;
+    while(nums[left]>=nums[right]){
+        if(left+1 === right){
+            return nums[right];
+        }
+        const mid = (left+right) >> 1;
+        // 三个相等 只能遍历找了
+        if(nums[left] === nums[right] && nums[left] === nums[mid]){
+            let minVal = nums[left];
+            for(let i=left+1;i<right+1;i++){
+                if(nums[i] < minVal){
+                    minVal = nums[i];
+                }
+            }
+            return minVal;
+        }
+        
+        if(nums[mid]>=nums[left]){
+            left = mid;
+        }else{
+            right = mid;
+        }
+    }
+    // 旋转后是原序列的情况
+    return nums[0];
+};
