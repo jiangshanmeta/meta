68d27ee158fc071721a5df167b269c9268fc8344
jiangshanmeta <540118044@qq.com>
2019-12-28 14:32:00 +0800

    leetcode 1079

---
+++ javascript/1079.letter-tile-possibilities.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} tiles
+ * @return {number}
+ */
+var numTilePossibilities = function(tiles) {
+    // 有重复的所以要排个序
+    tiles = tiles.split('').sort();
+    const visited = new Array(tiles.length).fill(false);
+    return backTracking(tiles,visited);
+};
+
+function backTracking(tiles,visited){
+    let result = 0;
+    for(let i=0;i<tiles.length;i++){
+        if(visited[i]){
+            continue;    
+        }
+        // 处理重复出现的字符
+        if(i>0 && tiles[i] === tiles[i-1] && !visited[i-1]){
+            continue;
+        }
+        result++;
+        visited[i] = true;
+        result += backTracking(tiles,visited);
+        visited[i] = false;
+    }
+    return result;
+}
