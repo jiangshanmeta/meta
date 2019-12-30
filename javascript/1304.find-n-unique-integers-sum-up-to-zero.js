b1113660f182d1c569cb4d0e63f3662ef654891d
jiangshanmeta <540118044@qq.com>
2019-12-30 13:39:02 +0800

    leetcode 1304

---
+++ javascript/1304.find-n-unique-integers-sum-up-to-zero.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var sumZero = function(n) {
+    const result = [];
+    // 奇数的多加个0就好了
+    if(n&1){
+        result.push(0);
+        n--;
+    }
+    // 剩下的就是正负对应着来
+    n /= 2;
+    for(let i=1;i<=n;i++){
+        result.push(i);
+        result.push(-i);
+    }
+    return result;
+};
