4214c95cb0c520e0465da798a28bb402f3286f50
jiangshanmeta <540118044@qq.com>
2019-12-31 13:50:46 +0800

    leetcode 1302

---
+++ javascript/1302.deepest-leaves-sum.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+// 层序遍历找最后一层
+var deepestLeavesSum = function(root) {
+    return levelOrder([root])
+};
+
+function levelOrder(lastLevel){
+    const nextLevel = [];
+    for(let i=0;i<lastLevel.length;i++){
+        lastLevel[i].left && nextLevel.push(lastLevel[i].left);
+        lastLevel[i].right && nextLevel.push(lastLevel[i].right);
+    }
+    if(nextLevel.length === 0){
+        return lastLevel.reduce((sum,node)=>{
+            return sum+node.val;
+        },0);
+    }else{
+        return levelOrder(nextLevel);
+    }
+    
+}
