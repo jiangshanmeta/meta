93ade70dfd4fe2b35872a836d2d30014a72e5051
jiangshanmeta <540118044@qq.com>
2020-01-06 09:24:55 +0800

    leetcode 1309

---
+++ javascript/1309.decrypt-string-from-alphabet-to-integer-mapping.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var freqAlphabets = function(s) {
+    const result = [];
+    let index = 0;
+    while(index<s.length){
+        let code;
+        // 通过#判断是否当成两位数字处理
+        if(index+2<s.length && s[index+2] === '#'){
+            code = +s.slice(index,index+2);
+            index += 3;
+        }else{
+            code = +s.slice(index,index+1);
+            index++;
+        }
+        result.push(String.fromCharCode(code+96))
+    }
+    return result.join('');
+};
