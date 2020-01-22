c7e35b6431969bff9b5f49267eb9b90e55fb8a14
jiangshanmeta <540118044@qq.com>
2020-01-22 15:29:09 +0800

    leetcode 1323

---
+++ javascript/1323.maximum-69-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number  = function(num) {
+    // 尝试把最左侧的6转换为9
+    const str = num.toString();
+    for(let i=0;i<str.length;i++){
+        if(str[i] === '6'){
+            return +(str.slice(0,i)+'9'+str.slice(i+1));
+        }
+    }
+    // 无法转换 全是9了
+    return num;
+};
