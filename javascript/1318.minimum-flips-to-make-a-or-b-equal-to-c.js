6030f95ae1b3554c82f40bae5b2f4da5df5ac3e0
jiangshanmeta <540118044@qq.com>
2020-01-22 15:29:02 +0800

    leetcode 1318

---
+++ javascript/1318.minimum-flips-to-make-a-or-b-equal-to-c.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var minFlips = function(a, b, c) {
+    let result = 0;
+    while(a || b || c){
+        const cDigit = c&1;
+        const aDigit = a&1;
+        const bDigit = b&1;
+        if(cDigit === 0){
+            if(aDigit){
+                result++;
+            }
+            if(bDigit){
+                result++;
+            }
+        }else{
+            if( (!aDigit) && (!bDigit)){
+                result++;
+            }
+            
+        }
+        a >>>= 1;
+        b >>>= 1;
+        c >>>= 1;
+    }
+    return result;
+};
