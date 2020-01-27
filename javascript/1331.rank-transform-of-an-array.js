2f4a8b116c0e6a935315f020538f6e644c5ce8c5
jiangshanmeta <540118044@qq.com>
2020-01-27 18:26:23 +0800

    leetcode 1331

---
+++ javascript/1331.rank-transform-of-an-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var arrayRankTransform = function(arr) {
+    // 去重 排序
+    const uniqueArr = [...new Set(arr)].sort((a,b)=>a-b);
+    // hash化
+    const elementRankMap = {};
+    for(let i=0;i<uniqueArr.length;i++){
+        elementRankMap[uniqueArr[i]] = i+1;
+    }
+    
+    
+    return arr.map(node=>elementRankMap[node]);
+};
