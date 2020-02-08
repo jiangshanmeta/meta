d838901ccdd5137f97de7d751c9fbcdac804049d
jiangshanmeta <540118044@qq.com>
2020-02-09 00:21:22 +0800

    1344

---
+++ javascript/1344.angle-between-hands-of-a-clock.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} hour
+ * @param {number} minutes
+ * @return {number}
+ */
+var angleClock = function(hour, minutes) {
+    // 小学数学题
+    const degreeMinute = minutes*6;
+    const degreeHour = (hour+minutes/60)*30;
+    const result = Math.abs(degreeHour-degreeMinute);
+    
+    return result>180?360-result:result;
+};
