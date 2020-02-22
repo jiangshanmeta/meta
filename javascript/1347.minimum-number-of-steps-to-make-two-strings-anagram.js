a62a3695a037856b2441789668604598129ce750
jiangshanmeta <540118044@qq.com>
2020-02-22 14:01:18 +0800

    1347

---
+++ javascript/1347.minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var minSteps = function(s, t) {
+    const sCharCounts = new Array(26).fill(0);
+    for(let i=0;i<s.length;i++){
+        sCharCounts[s.charCodeAt(i)-97]++;
+    }
+    const tCharCounts = new Array(26).fill(0);
+    for(let i=0;i<t.length;i++){
+        tCharCounts[t.charCodeAt(i)-97]++;
+    }
+    let result = 0;
+    for(let i=0;i<26;i++){
+        if(tCharCounts[i]>sCharCounts[i]){
+            result += tCharCounts[i]-sCharCounts[i];
+        }
+
+    }
+    return result;
+};
