615d3443f7e60cd1865e0146bb10982931848d27
jiangshanmeta <540118044@qq.com>
2020-03-01 15:17:51 +0800

    1360

---
+++ javascript/1360.number-of-days-between-two-dates.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} date1
+ * @param {string} date2
+ * @return {number}
+ */
+var daysBetweenDates = function(date1, date2) {
+    // 偷懒用API了
+    let delta = new Date(date2).getTime()-new Date(date1).getTime();
+    if(delta<0){
+        delta = -delta;
+    }
+    
+    return delta/(1000*86400);
+};
