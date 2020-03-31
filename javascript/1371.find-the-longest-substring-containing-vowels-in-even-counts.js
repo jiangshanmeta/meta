a42f7f72f16b9446fd45e8ddc68b6fb5621f5f24
jiangshanmeta <540118044@qq.com>
2020-03-31 22:49:40 +0800

    1371

---
+++ javascript/1371.find-the-longest-substring-containing-vowels-in-even-counts.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var findTheLongestSubstring = function(s) {
+    let result = 0;
+    let bitStartIndex = new Array(32).fill(s.length);
+    bitStartIndex[0] = -1;
+    // bitmask 五位表示aeiou是否是奇数
+    let bitmask = 0;
+    for(let i=0;i<s.length;i++){
+        if(s[i] === 'a'){
+            bitmask ^= 16;
+        }else if(s[i] === 'e'){
+            bitmask ^= 8;
+        }else if(s[i] === 'i'){
+            bitmask ^= 4;
+        }else if(s[i] === 'o'){
+            bitmask ^= 2;
+        }else if(s[i] === 'u'){
+            bitmask ^= 1;
+        }
+        if(bitStartIndex[bitmask] === s.length){
+            bitStartIndex[bitmask] = i;
+        }else{
+            // 相同的bitmask 中间的元音一定是偶数
+            result = Math.max(result,i-bitStartIndex[bitmask]);
+        }
+    }
+    return result;
+};
