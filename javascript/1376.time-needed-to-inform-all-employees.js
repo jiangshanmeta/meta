1d2a688371e5a73af50d355a7d9ed3138429d082
jiangshanmeta <540118044@qq.com>
2020-04-18 13:48:36 +0800

    1376

---
+++ javascript/1376.time-needed-to-inform-all-employees.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @param {number} headID
+ * @param {number[]} manager
+ * @param {number[]} informTime
+ * @return {number}
+ */
+var numOfMinutes = function(n, headID, manager, informTime) {
+    const tree = {};
+    for(let i=0;i<manager.length;i++){
+        const managerId = manager[i];
+        if(!tree[managerId]){
+            tree[managerId] = [];
+        }
+        tree[managerId].push(i);
+    }
+    
+    return levelOrder(headID,tree,informTime)
+};
+
+function levelOrder(rootIndex,tree,informTime){
+    if(!tree[rootIndex]){
+        return 0;
+    }
+    
+    return Math.max(...tree[rootIndex].map(childIndex=>levelOrder(childIndex,tree,informTime)  ))+informTime[rootIndex];
+}
