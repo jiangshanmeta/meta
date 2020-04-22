77e30837cfe6e249f7702dc18b68d26955f0dea5
jiangshanmeta <540118044@qq.com>
2020-04-22 20:14:48 +0800

    1385

---
+++ javascript/1385.find-the-distance-value-between-two-arrays.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @param {number} d
+ * @return {number}
+ */
+var findTheDistanceValue = function(arr1, arr2, d) {
+    let result = 0;
+    for(let i=0;i<arr1.length;i++){
+        let flag = true;
+        for(let j=0;j<arr2.length;j++){
+            if(Math.abs(arr2[j]-arr1[i])<=d){
+                flag = false;
+                break;
+            }
+        }
+        flag && result++;
+    }
+    return result;
+};
