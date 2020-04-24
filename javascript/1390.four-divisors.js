117fc2e5e3badb35c512f0c9d567c3155a98eb21
jiangshanmeta <540118044@qq.com>
2020-04-24 20:07:27 +0800

    1390

---
+++ javascript/1390.four-divisors.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumFourDivisors = function(nums) {
+    return nums.reduce((sum,item)=>{
+        let hasAnother = false;
+        let anotherVal;
+        const sqrt = Math.sqrt(item);
+        for(let i=2;i<=sqrt;i++){
+            if(item%i === 0){
+                if(hasAnother){
+                    return sum;
+                }else{
+                    // 注意等于sqrt的特殊情况
+                    if(i === sqrt){
+                        return sum;
+                    }
+
+                    hasAnother = true;
+                    anotherVal = i;
+                }
+            }
+        }
+        if(hasAnother){
+            return sum+1+item+anotherVal+item/anotherVal;
+        }else{
+            return sum;
+        }
+    },0);
+};
