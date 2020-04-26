6a418c16573e75bdd5a5e7cef824ac42e172aeb4
jiangshanmeta <540118044@qq.com>
2020-04-26 22:09:28 +0800

    1395

---
+++ javascript/1395.count-number-of-teams.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} rating
+ * @return {number}
+ */
+var numTeams = function(rating) {
+    const smaller = new Array(rating.length).fill(0);
+    const larger = new Array(rating.length).fill(0);
+    for(let i=1;i<rating.length;i++){
+        for(let j=0;j<i;j++){
+            rating[j]<rating[i] && smaller[i]++;
+            rating[j]>rating[i] && larger[i]++;
+        }
+    }
+    let result = 0;
+    for(let i=2;i<rating.length;i++){
+        for(let j=1;j<i;j++){
+            if(rating[j]<rating[i]){
+                result += smaller[j];
+            }else if(rating[j]>rating[i]){
+                result += larger[j];
+            }
+        }
+    }
+    return result;
+};
