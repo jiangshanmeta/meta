cb4b65a951ac6635e8f8e16bcfbab9e1db21cbe9
jiangshanmeta <540118044@qq.com>
2020-04-30 20:51:07 +0800

    1403

---
+++ javascript/1403.minimum-subsequence-in-non-increasing-order.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var minSubsequence = function(nums) {
+    nums.sort((a,b)=>b-a);
+    let restSum = nums.reduce((sum,item)=>{
+        return sum+item;
+    },0);
+    const result = [];
+    let sum = 0;
+    let index = 0;
+    while(sum<=restSum){
+        const item = nums[index++];
+        result.push(item);
+        sum += item;
+        restSum -= item;
+    }
+    return result;
+};
