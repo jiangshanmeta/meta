f7778f647c0cda7999aa6f5eaa40a9a9a6717765
jiangshanmeta <540118044@qq.com>
2020-04-30 20:51:12 +0800

    1404

---
+++ javascript/1404.number-of-steps-to-reduce-a-number-in-binary-representation-to-one.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numSteps = function(s) {
+    const stack = s.split('');
+    let result = 0;
+    while(stack.length>1){
+        // 偶数的除二
+        if(stack[stack.length-1] === '0'){
+            stack.pop();
+            result++;
+        }else{
+            // 奇数的+1
+            result++;
+            // 顺便把除2做了
+            while(stack.length && stack[stack.length-1] === '1'){
+                stack.pop();
+                result++;
+            }
+            stack.pop();
+            stack.push('1');
+        }
+    }
+    return result;
+};
