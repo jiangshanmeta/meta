7ba9303bb15dadd7777bbece5bb4e2d67e9001ac
jiangshanmeta <540118044@qq.com>
2020-05-01 11:32:51 +0800

    1409

---
+++ javascript/1409.queries-on-a-permutation-with-key.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} queries
+ * @param {number} m
+ * @return {number[]}
+ */
+var processQueries = function(queries, m) {
+    const permutation = [];
+    for(let i=0;i<m;i++){
+        permutation.push(i+1);
+    }
+    const result = [];
+    for(let i=0;i<queries.length;i++){
+        const index = permutation.indexOf(queries[i]);
+        result.push(index);
+        permutation.splice(index,1);
+        permutation.unshift(queries[i]);
+    }
+    return result;
+};
