74f0dbe259ddd9614fe00e7a0b146e16219f7548
jiangshanmeta <540118044@qq.com>
2020-05-03 11:32:50 +0800

    1415

---
+++ javascript/1415.the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+var getHappyString = function(n, k) {
+    const result = [];
+    backTracking([],result,n,k);
+    return k>result.length?'':result[k-1];
+};
+const candidate = ['a','b','c'];
+function backTracking(sequence,result,n,k){
+    if(result.length === k){
+        return;
+    }
+    if(sequence.length === n){
+        result.push(sequence.join(''));
+        return;
+    }
+    for(let i=0;i<3;i++){
+        if(sequence.length && sequence[sequence.length-1] === candidate[i]){
+            continue;
+        }
+        sequence.push(candidate[i]);
+        backTracking(sequence,result,n,k);
+        sequence.pop();
+    }
+}
