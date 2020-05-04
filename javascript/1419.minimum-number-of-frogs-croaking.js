abca4b9171a91f6dc3257be203823034fca53677
jiangshanmeta <540118044@qq.com>
2020-05-04 20:48:52 +0800

    1419

---
+++ javascript/1419.minimum-number-of-frogs-croaking.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string} croakOfFrogs
+ * @return {number}
+ */
+const charIndexMap = {
+    'r':0,
+    'o':1,
+    'a':2,
+    'k':3
+};
+
+const nextIndexMap = {
+    'c':0,
+    'r':1,
+    'o':2,
+    'a':3,
+}
+
+var minNumberOfFrogs = function(croakOfFrogs) {
+    const counts = [0,0,0,0];
+    let frogs = 0;
+    let inUsingFrogs = 0;
+    for(let i=0;i<croakOfFrogs.length;i++){
+        if(croakOfFrogs[i] === 'c'){
+            if(inUsingFrogs === frogs){
+                frogs++;
+            }
+            inUsingFrogs++;
+        }else{
+            if(counts[charIndexMap[croakOfFrogs[i]]] === 0){
+                return -1;
+            }
+            counts[charIndexMap[croakOfFrogs[i]]]++;
+        }
+        if(croakOfFrogs[i] !== 'k'){
+            counts[nextIndexMap[croakOfFrogs[i]]]--;
+        }else{
+            inUsingFrogs--;
+        }
+    }
+    if(!counts.every(item=>item === 0)){
+        return -1;
+    }
+    
+    return frogs;
+};
