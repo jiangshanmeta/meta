bfba3450a4e578d3442fa7b107578cfa266babe1
jiangshanmeta <540118044@qq.com>
2020-05-05 03:17:19 +0800

    1422

---
+++ javascript/1422.maximum-score-after-splitting-a-string.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxScore = function(s) {
+    let result = 0;
+    let oneCount = 0;
+    for(let i=0;i<s.length;i++){
+        s[i] === '1' && oneCount++;
+    }
+    let zeroCount = 0;
+    for(let i=0;i<s.length-1;i++){
+        if(s[i] === '0'){
+            zeroCount++;
+        }else{
+            oneCount--;
+        }
+        result = Math.max(result,zeroCount+oneCount);
+    }
+    
+    return result;
+};
