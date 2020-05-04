82b1e5fcc25add0f4fab20c221cfefdebbaf4404
jiangshanmeta <540118044@qq.com>
2020-05-05 03:17:28 +0800

    1424

---
+++ javascript/1424.diagonal-traverse-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} nums
+ * @return {number[]}
+ */
+var findDiagonalOrder = function(nums) {
+    const cache = {};
+    let maxIndex = 0;
+    for(let i=0;i<nums.length;i++){
+        for(let j=0;j<nums[i].length;j++){
+            (cache[i+j] || (cache[i+j] = [])).push(nums[i][j]);
+            maxIndex = Math.max(maxIndex,i+j);
+        }
+    }
+    const result = [];
+    for(let i=0;i<=maxIndex;i++){
+        if(!cache[i]){
+            continue;
+        }
+        for(let j=cache[i].length-1;j>-1;j--){
+            result.push(cache[i][j]);
+        }
+    }
+    return result;
+};
