35ed22ab02492c7dcf43671f5459afe02def2ee3
jiangshanmeta <540118044@qq.com>
2020-05-05 03:17:24 +0800

    1423

---
+++ javascript/1423.maximum-points-you-can-obtain-from-cards.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} cardPoints
+ * @param {number} k
+ * @return {number}
+ */
+var maxScore = function(cardPoints, k) {
+    const sum = cardPoints.reduce((sum,item)=>{
+        return sum+item;
+    },0);
+    const windowLength = cardPoints.length-k;
+    if(windowLength === 0){
+        return sum;
+    }
+    let result = 0;
+    let slidingWindowSum = 0;
+    for(let i=0;i<windowLength-1;i++){
+        slidingWindowSum += cardPoints[i];
+    }
+    for(let i=windowLength-1;i<cardPoints.length;i++){
+        slidingWindowSum += cardPoints[i];
+        result = Math.max(result,sum-slidingWindowSum);
+        slidingWindowSum -= cardPoints[i+1-windowLength];
+    }
+    return result;
+};
