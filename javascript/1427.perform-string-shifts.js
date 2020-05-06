5fd44e6b5b11c72ca4eb1c0bc6a00202174c3b7f
jiangshanmeta <540118044@qq.com>
2020-05-06 20:19:27 +0800

    1427

---
+++ javascript/1427.perform-string-shifts.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @param {number[][]} shift
+ * @return {string}
+ */
+var stringShift = function(s, shift) {
+    let count = 0;
+    for(let i=0;i<shift.length;i++){
+        const [direction,amount] = shift[i];
+        if(direction === 0){
+            count += amount;
+        }else{
+            count -= amount;
+        }
+    }
+    const list = s.split('');
+
+    if(count>0){
+        count %= s.length;
+        for(let i=0;i<count;i++){
+            list.push(list.shift());
+        }
+    }else if(count<0){
+        count = -count;
+        count %= s.length;
+        for(let i=0;i<count;i++){
+            list.unshift(list.pop())
+        }
+    }
+    return list.join('');
+};
