6dbd3529d2292f1df7f15bf361e108c283ed6e57
jiangshanmeta <540118044@qq.com>
2020-05-06 20:19:31 +0800

    1428

---
+++ javascript/1428.leftmost-column-with-at-least-a-one.js
@@ -0,0 +1,52 @@
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * function BinaryMatrix() {
+ *     @param {integer} x, y
+ *     @return {integer}
+ *     this.get = function(x, y) {
+ *         ...
+ *     };
+ *
+ *     @return {[integer, integer]}
+ *     this.dimensions = function() {
+ *         ...
+ *     };
+ * };
+ */
+
+/**
+ * @param {BinaryMatrix} binaryMatrix
+ * @return {number}
+ */
+var leftMostColumnWithOne = function(binaryMatrix) {
+    const [N,M] = binaryMatrix.dimensions();
+    const oneIndexs = [];
+    for(let i=0;i<N;i++){
+        oneIndexs[i] = findFirstOneIndex(binaryMatrix,i,M);
+    }
+    const index = Math.min(...oneIndexs);
+    if(index === M){
+        return -1;
+    }else{
+        return index;
+    }
+    
+};
+
+function findFirstOneIndex(binaryMatrix,x,M){
+    let result = M;
+    let low = 0;
+    let high = M-1;
+    while(low<=high){
+        const mid = (low+high)>>>1;
+        const value = binaryMatrix.get(x,mid);
+        if(value === 1){
+            result = Math.min(result,mid);
+            high = mid-1;
+        }else{
+            low = mid+1;
+        }
+    }
+    return result;
+}
