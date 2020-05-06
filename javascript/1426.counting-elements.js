b5957dd6ad40f6d528011a316e2074dee6857283
jiangshanmeta <540118044@qq.com>
2020-05-06 20:19:22 +0800

    1426

---
+++ javascript/1426.counting-elements.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var countElements = function(arr) {
+    const hash = {};
+    for(let i=0;i<arr.length;i++){
+        hash[arr[i]] = true;
+    }
+    let result = 0;
+    for(let i=0;i<arr.length;i++){
+        if(hash[arr[i]+1]){
+            result++;
+        }
+    }
+    return result;
+};
