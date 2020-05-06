00845dea0d3e266da487efa1a2b02d4c6499eeb0
jiangshanmeta <540118044@qq.com>
2020-05-07 00:18:01 +0800

    1430

---
+++ javascript/1430.check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var isValidSequence = function(root, arr) {
+    if(!root){
+        return false;
+    }
+    let levelNodes = [root];
+    let index = 0;
+    while(index<arr.length && levelNodes.length){
+        if(index === arr.length-1){
+            return levelNodes.some((node)=>{
+                return !node.left && !node.right && node.val === arr[index];
+            });
+        }
+        levelNodes = levelNodes.reduce((list,node)=>{
+            if(node.val === arr[index]){
+                node.left && list.push(node.left);
+                node.right && list.push(node.right);
+            }
+            return list;
+        },[]);
+        index++;
+    }
+    return false;
+};
