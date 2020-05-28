2c43c68ab8d3f470608c85ba3d9e0ef1765bd2c2
jiangshanmeta <540118044@qq.com>
2020-05-28 20:13:29 +0800

    1436

---
+++ javascript/1436.destination-city.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[][]} paths
+ * @return {string}
+ */
+var destCity = function(paths) {
+    const map = Object.create(null);
+    for(let i=0;i<paths.length;i++){
+        map[paths[i][0]] = paths[i][1];
+    }
+    let city = paths[0][0];
+    while(map[city]){
+        city = map[city];
+    }
+    return city;
+};
