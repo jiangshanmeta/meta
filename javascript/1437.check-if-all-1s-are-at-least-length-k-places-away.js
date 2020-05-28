07fddfb96e7897e4ea548c16d22c0b855b04e990
jiangshanmeta <540118044@qq.com>
2020-05-28 20:13:34 +0800

    1437

---
+++ javascript/1437.check-if-all-1s-are-at-least-length-k-places-away.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var kLengthApart = function(nums, k) {
+    let index = nums.indexOf(1);
+    if(index === -1){
+        return true;
+    }
+    let nextIndex = index+1;
+    while(nextIndex<nums.length){
+        while(nextIndex<nums.length && nums[nextIndex] === 0){
+            nextIndex++;
+        }
+        if(nextIndex === nums.length){
+            return true;
+        }
+        if(nextIndex-index<k+1){
+            return false;
+        }
+        index = nextIndex;
+        nextIndex = index+1;
+    }
+    return true;
+};
