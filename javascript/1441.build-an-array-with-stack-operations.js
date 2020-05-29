d60d134224815ae9de86cec644a153666ca5c0bc
jiangshanmeta <540118044@qq.com>
2020-05-29 21:08:55 +0800

    1441

---
+++ javascript/1441.build-an-array-with-stack-operations.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} target
+ * @param {number} n
+ * @return {string[]}
+ */
+var buildArray = function(target, n) {
+    const result = [];
+    let index = 1;
+    for(let i=0;i<target.length;i++){
+        const num = target[i];
+        while(index<num){
+            result.push('Push');
+            result.push('Pop');
+            index++;
+        }
+        result.push('Push');
+        index++;
+    }
+    return result;
+};
