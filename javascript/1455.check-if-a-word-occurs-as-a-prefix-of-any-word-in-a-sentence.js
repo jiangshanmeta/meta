9240e4f194dc0fa6404cc6379546698487f151fd
jiangshanmeta <540118044@qq.com>
2020-05-31 23:02:21 +0800

    1455

---
+++ javascript/1455.check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} sentence
+ * @param {string} searchWord
+ * @return {number}
+ */
+var isPrefixOfWord = function(sentence, searchWord) {
+    const sequence = sentence.split(' ');
+    for(let i=0;i<sequence.length;i++){
+        if(sequence[i].startsWith(searchWord)){
+            return i+1;
+        }
+    }
+    return -1;
+};
