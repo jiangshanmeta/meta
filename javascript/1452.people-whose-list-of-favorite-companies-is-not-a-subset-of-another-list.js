98f3a5fbda019d6b4523cceccc9f73818354fbfb
jiangshanmeta <540118044@qq.com>
2020-05-31 23:02:14 +0800

    1452

---
+++ javascript/1452.people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[][]} favoriteCompanies
+ * @return {number[]}
+ */
+var peopleIndexes = function(favoriteCompanies) {
+    const maps = favoriteCompanies.map((sequence)=>{
+        return sequence.reduce((obj,item)=>{
+            obj[item] = true;
+            return obj;
+        },{});
+    });
+    const result = [];
+    for(let i=0;i<favoriteCompanies.length;i++){
+        let flag = true;
+        for(let j=0;j<favoriteCompanies.length;j++){
+            if(i === j){
+                continue;
+            }
+            if(isSubset(maps[j],favoriteCompanies[i])){
+                flag = false;
+                break;
+            }
+        }
+        flag && result.push(i);
+    }
+    return result;
+};
+function isSubset(map,list){
+    return list.every((str)=>map[str]);
+}
