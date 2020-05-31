aba0cb0990a866dc4406b057d07819d3b6a21a44
jiangshanmeta <540118044@qq.com>
2020-05-31 23:01:57 +0800

    1450

---
+++ javascript/1450.number-of-students-doing-homework-at-a-given-time.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number} queryTime
+ * @return {number}
+ */
+var busyStudent = function(startTime, endTime, queryTime) {
+    let result = 0;
+    for(let i=0;i<startTime.length;i++){
+        if(startTime[i]<=queryTime && endTime[i]>=queryTime){
+            result++;
+        }
+    }
+    return result;
+};
