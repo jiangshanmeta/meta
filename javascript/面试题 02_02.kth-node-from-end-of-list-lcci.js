cb749f508ddada4e524f3c79b20cc268eef1b9ea
jiangshanmeta <540118044@qq.com>
2020-06-08 22:51:45 +0800

    2

---
+++ javascript/面试题 02_02.kth-node-from-end-of-list-lcci.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {number}
+ */
+var kthToLast = function(head, k) {
+    let fast = head;
+    while(k--){
+        fast = fast.next;
+    }
+    while(fast){
+        head = head.next;
+        fast = fast.next;
+    }
+
+    return head.val;
+};
