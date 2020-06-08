f9c88a1100b2f76200298b019b0bf6fefc5c8e7f
jiangshanmeta <540118044@qq.com>
2020-06-08 22:51:49 +0800

    7

---
+++ javascript/面试题 02_07.intersection-of-two-linked-lists-lcci.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    let countA = getDepth(headA);
+    let countB = getDepth(headB);
+    while(countA>countB){
+        countA--;
+        headA = headA.next;
+    }
+    while(countB>countA){
+        countB--;
+        headB = headB.next;
+    }
+    while(headA && headB && headA !== headB){
+        headA = headA.next;
+        headB = headB.next;
+    }
+    return headA === headB?headA:null;
+};
+
+function getDepth(head){
+    let result = 0;
+    while(head){
+        result++;
+        head = head.next;
+    }
+    return result;
+}
