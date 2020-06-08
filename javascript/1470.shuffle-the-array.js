5ad9b98018273bb278a076419f3b39b196fa62b9
jiangshanmeta <540118044@qq.com>
2020-06-09 00:03:54 +0800

    1470

---
+++ javascript/1470.shuffle-the-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    const result = [];
+    let index1 = 0;
+    let index2 = n;
+    while(index2<nums.length){
+        result.push(nums[index1++]);
+        result.push(nums[index2++]);
+    }
+    return result;
+};
