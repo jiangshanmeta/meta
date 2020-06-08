35682d72f3d07725bbba3c8a21e1ff3b11981283
jiangshanmeta <540118044@qq.com>
2020-06-09 00:03:58 +0800

    1471

---
+++ javascript/1471.the-k-strongest-values-in-an-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number[]}
+ */
+var getStrongest = function(arr, k) {
+    arr.sort((a,b)=>a-b);
+    const mid = (arr.length-1) >> 1;
+    let index1 = 0;
+    let index2 = arr.length-1;
+    const result = [];
+    while(k>0){
+        const diff1 = Math.abs(arr[index2]-arr[mid]);
+        const diff2 = Math.abs(arr[index1]-arr[mid]);
+        if(diff1>=diff2){
+            result.push(arr[index2--]);
+        }else{
+            result.push(arr[index1++]);
+        }
+        k--;
+    }
+    return result;
+};
