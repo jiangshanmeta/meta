cf37de5087cf8bb6ba90c2cc03c023a01c8944c4
jiangshanmeta <540118044@qq.com>
2020-06-10 00:41:48 +0800

    2

---
+++ javascript/面试题 03_02.min-stack-lcci.js
@@ -0,0 +1,50 @@
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+    this.stack1 = [];
+    this.stack2 = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    this.stack1.push(x);
+    if(this.stack2.length === 0 || this.stack2[this.stack2.length-1]>=x){
+        this.stack2.push(x);
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    if(this.stack1.pop() === this.stack2[this.stack2.length-1]){
+        this.stack2.pop();
+    }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack1[this.stack1.length-1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.stack2[this.stack2.length-1];
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
