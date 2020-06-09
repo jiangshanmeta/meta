6a8637e5efd5c7a77272c21cdd00bf020c4ead70
jiangshanmeta <540118044@qq.com>
2020-06-10 00:41:57 +0800

    4

---
+++ javascript/面试题 04_04.check-balance-lcci.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    return postOrder(root) !== -1;
+};
+
+function postOrder(root){
+    if(!root){
+        return 0;
+    }
+    const left = postOrder(root.left);
+    if(left === -1){
+        return -1;
+    }
+    const right = postOrder(root.right);
+    if(right === -1){
+        return -1;
+    }
+    const diff = Math.abs(left-right);
+    if(diff>1){
+        return -1;
+    }
+    return Math.max(left,right)+1;
+}
