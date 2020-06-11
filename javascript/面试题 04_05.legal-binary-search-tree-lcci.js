5e5724326724d3e95cb784d33e36f2bae49eb31c
jiangshanmeta <540118044@qq.com>
2020-06-11 20:15:00 +0800

    1

---
+++ javascript/面试题 04_05.legal-binary-search-tree-lcci.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    return helper(root,-Infinity,Infinity);
+};
+
+function helper(root,min,max){
+    if(!root){
+        return true;
+    }
+    if(root.val<=min || root.val>=max){
+        return false;
+    }
+    return helper(root.left,min,root.val) && helper(root.right,root.val,max);
+}
