44b6e9278fca16223c2ce72ccd69957f00852540
jiangshanmeta <540118044@qq.com>
2020-06-12 00:07:24 +0800

    1

---
+++ javascript/面试题 04_03.list-of-depth-lcci.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {TreeNode} tree
+ * @return {ListNode[]}
+ */
+var listOfDepth = function(tree) {
+    if(!tree){
+        return [];
+    }
+    const result = [];
+    levelOrder(result,[tree]);
+    return result;
+};
+
+function levelOrder(result,level){
+    let dummyHead = new ListNode();
+    let node = dummyHead;
+    const nextLevel = [];
+    for(let i=0;i<level.length;i++){
+        const lNode = new ListNode(level[i].val);
+        node.next = lNode;
+        node = lNode;
+        
+        level[i].left && nextLevel.push(level[i].left);
+        level[i].right && nextLevel.push(level[i].right);
+    }
+    result.push(dummyHead.next);
+    nextLevel.length && levelOrder(result,nextLevel);
+}
