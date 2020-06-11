35cf35fc71efb5f50158c7da7ea8b2196bc49d40
jiangshanmeta <540118044@qq.com>
2020-06-11 20:15:07 +0800

    1

---
+++ javascript/面试题 16_17.contiguous-sequence-lcci.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let prev = nums[0];
+    let result = nums[0];
+    for(let i=1;i<nums.length;i++){
+        prev = prev<0?nums[i]:prev+nums[i];
+        result = Math.max(result,prev);
+    }
+    return result;
+};
