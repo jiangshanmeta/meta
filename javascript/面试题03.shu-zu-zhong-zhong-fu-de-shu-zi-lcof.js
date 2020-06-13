82836918443f11181fb2c8b3c0e4418f177dc83c
jiangshanmeta <540118044@qq.com>
2020-06-13 19:18:51 +0800

    1

---
+++ javascript/面试题03.shu-zu-zhong-zhong-fu-de-shu-zi-lcof.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findRepeatNumber = function(nums) {
+    const map = Object.create(null);
+    for(let i=0;i<nums.length;i++){
+        if(map[nums[i]]){
+            return nums[i]
+        }
+        map[nums[i]] = true;
+    }
+};
