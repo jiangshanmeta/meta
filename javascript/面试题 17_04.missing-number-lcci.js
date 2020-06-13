34c95623aeed94012b2954b424e1b331403f1ec6
jiangshanmeta <540118044@qq.com>
2020-06-13 19:18:44 +0800

    1

---
+++ javascript/面试题 17_04.missing-number-lcci.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let index = 0;
+    while(index<nums.length){
+        const num = nums[index];
+        if(num === index || num>=nums.length || num<0 || nums[num] === num){
+            index++;
+            continue;
+        }
+        nums[index] = nums[num];
+        nums[num] = num;
+    }
+
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] !== i){
+            return i;
+        }
+    }
+    return nums.length;
+};
