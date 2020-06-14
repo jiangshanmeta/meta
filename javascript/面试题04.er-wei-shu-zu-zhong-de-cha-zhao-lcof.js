5206d90ac822025df47d1a8b9404b66bcedadb33
jiangshanmeta <540118044@qq.com>
2020-06-14 15:42:07 +0800

    1

---
+++ javascript/面试题04.er-wei-shu-zu-zhong-de-cha-zhao-lcof.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var findNumberIn2DArray = function(matrix, target) {
+    if(matrix.length === 0){
+        return false;
+    }
+    let x = 0;
+    let y = matrix[0].length-1;
+    while(x<matrix.length && y>-1){
+        if(matrix[x][y] === target){
+            return true;
+        }else if(matrix[x][y]<target){
+            x++;
+        }else{
+            y--;
+        }
+    }
+    return false;
+};
