25140d5c5cf05e82905b171ef305b85d3272ee8a
jiangshanmeta <540118044@qq.com>
2020-06-16 00:32:47 +0800

    1475

---
+++ javascript/1475.final-prices-with-a-special-discount-in-a-shop.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number[]}
+ */
+var finalPrices = function(prices) {
+    const result = [];
+    const stackIndex = [];
+    for(let i=0;i<prices.length;i++){
+        while(stackIndex.length && prices[stackIndex[stackIndex.length-1]]>=prices[i]){
+            const index = stackIndex.pop();
+            result[index] = prices[index]-prices[i];
+        }
+        result.push(prices[i]);
+        stackIndex.push(i);
+    }
+    return result;
+};
