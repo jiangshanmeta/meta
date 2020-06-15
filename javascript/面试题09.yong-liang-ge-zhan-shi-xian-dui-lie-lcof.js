978a80130259003857296c518d33b2764f7d4690
jiangshanmeta <540118044@qq.com>
2020-06-15 21:41:39 +0800

    1

---
+++ javascript/面试题09.yong-liang-ge-zhan-shi-xian-dui-lie-lcof.js
@@ -0,0 +1,36 @@
+var CQueue = function() {
+    this.stack1 = [];
+    this.stack2 = [];
+};
+
+/** 
+ * @param {number} value
+ * @return {void}
+ */
+CQueue.prototype.appendTail = function(value) {
+    this.stack1.push(value);
+};
+
+/**
+ * @return {number}
+ */
+CQueue.prototype.deleteHead = function() {
+
+    if(this.stack2.length === 0){
+        while(this.stack1.length){
+            this.stack2.push(this.stack1.pop());
+        }
+    }
+    if(this.stack2.length === 0){
+        return -1;
+    }
+
+    return this.stack2.pop();
+};
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * var obj = new CQueue()
+ * obj.appendTail(value)
+ * var param_2 = obj.deleteHead()
+ */
