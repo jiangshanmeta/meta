f6c43a70864756be3806d6b602676373ed75f748
jiangshanmeta <540118044@qq.com>
2020-06-15 21:41:42 +0800

    1

---
+++ javascript/面试题10- I.fei-bo-na-qi-shu-lie-lcof.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const mod = 10**9+7;
+var fib = function(n) {
+    if(n === 0){
+        return 0;
+    }
+    let a = 0;
+    let b = 1;
+    for(let i=2;i<=n;i++){
+        [a,b] = [b,(a+b)%mod];
+    }
+    return b;
+};
