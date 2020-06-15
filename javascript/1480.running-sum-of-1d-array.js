5f1793e094a08bec7d50fd6aa5a1ad895b91b402
jiangshanmeta <540118044@qq.com>
2020-06-16 00:33:03 +0800

    1480

---
+++ javascript/1480.running-sum-of-1d-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    const result = [];
+    let sum = 0;
+    for(let i=0;i<nums.length;i++){
+        sum += nums[i];
+        result.push(sum);
+    }
+    return result;
+};
