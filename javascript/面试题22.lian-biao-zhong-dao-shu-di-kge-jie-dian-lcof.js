d69b07e99138859d8da7b9964ec07c8c36709fa4
jiangshanmeta <540118044@qq.com>
2020-06-18 23:42:46 +0800

    22

---
+++ javascript/面试题22.lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var getKthFromEnd = function(head, k) {
+    let fast = head;
+    while(k--){
+        fast = fast.next;
+    }
+    while(fast){
+        head = head.next;
+        fast = fast.next;
+    }
+    return head;
+};
