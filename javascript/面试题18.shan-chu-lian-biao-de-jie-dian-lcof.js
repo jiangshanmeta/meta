9d5708ffc0c798af9b91c7ec4a2af47efed25551
jiangshanmeta <540118044@qq.com>
2020-06-18 21:05:47 +0800

    18

---
+++ javascript/面试题18.shan-chu-lian-biao-de-jie-dian-lcof.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var deleteNode = function(head, val) {
+    const dummyHead = new ListNode();
+    dummyHead.next = head;
+    let prev = dummyHead;
+    while(head && head.val !== val){
+        prev = prev.next;
+        head = head.next;
+    }
+    prev.next = head.next;
+    return dummyHead.next;
+};
