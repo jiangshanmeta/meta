811cc0d317e7ff761f6e22e6d612f2d40758c1e0
jiangshanmeta <540118044@qq.com>
2020-06-18 21:05:50 +0800

    21

---
+++ javascript/面试题21.diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var exchange = function(nums) {
+    let evenIndex = 0;
+    let oddIndex = nums.length-1;
+    while(evenIndex<oddIndex){
+        while(evenIndex<oddIndex && nums[evenIndex]%2 !== 0){
+            evenIndex++;
+        }
+        while(evenIndex<oddIndex && nums[oddIndex]%2 !== 1){
+            oddIndex--;
+        }
+        if(evenIndex<oddIndex){
+            const tmp = nums[oddIndex];
+            nums[oddIndex] = nums[evenIndex];
+            nums[evenIndex] = tmp;
+            oddIndex--;
+            evenIndex++;
+        }
+    }
+    return nums;
+};
