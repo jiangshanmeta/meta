796784046c38744e735ebcba57ffae8bd9e39ec1
jiangshanmeta <540118044@qq.com>
2020-06-20 17:47:16 +0800

    27

---
+++ javascript/面试题27.er-cha-shu-de-jing-xiang-lcof.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var mirrorTree = function(root) {
+    if(!root){
+        return root;
+    }
+    mirrorTree(root.left);
+    mirrorTree(root.right);
+    const tmp = root.left;
+    root.left = root.right;
+    root.right = tmp;
+    return root;
+};
