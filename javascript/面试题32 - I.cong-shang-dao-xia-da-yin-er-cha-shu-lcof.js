02da6715f3873946e1bb2cfe97e6b9f4d41965be
jiangshanmeta <540118044@qq.com>
2020-06-21 21:41:32 +0800

    32

---
+++ javascript/面试题32 - I.cong-shang-dao-xia-da-yin-er-cha-shu-lcof.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var levelOrder = function(root) {
+    if(!root){
+        return [];
+    }
+    const nodeList = [root];
+    const result = [];
+    let index = 0;
+    while(index<nodeList.length){
+        const node = nodeList[index++];
+        result.push(node.val);
+        node.left && nodeList.push(node.left);
+        node.right && nodeList.push(node.right);
+    }
+    return result;
+};
