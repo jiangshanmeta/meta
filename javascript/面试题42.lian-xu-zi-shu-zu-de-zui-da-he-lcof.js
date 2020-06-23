a4f865c431e773fba9205530cfb3f28fd6d0d9da
jiangshanmeta <540118044@qq.com>
2020-06-23 20:42:37 +0800

    42

---
+++ javascript/面试题42.lian-xu-zi-shu-zu-de-zui-da-he-lcof.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let result = nums[0];
+    let prev = nums[0];
+    for(let i=1;i<nums.length;i++){
+        prev = (prev<0?0:prev)+nums[i];
+        result = Math.max(result,prev);
+    }
+    return result;
+};
