eb9bd93be2bb480d71b4dc66493e740887c3650c
jiangshanmeta <540118044@qq.com>
2020-06-23 20:42:41 +0800

    47

---
+++ javascript/面试题47.li-wu-de-zui-da-jie-zhi-lcof.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxValue = function(grid) {
+    const M = grid.length;
+    const N = grid[0].length;
+    const dp = new Array(N).fill(0);
+    for(let i=0;i<M;i++){
+        dp[0] += grid[i][0];
+        for(let j=1;j<N;j++){
+            dp[j] = Math.max(dp[j-1],dp[j])+grid[i][j];
+        }
+    }
+    return dp[N-1];
+};
