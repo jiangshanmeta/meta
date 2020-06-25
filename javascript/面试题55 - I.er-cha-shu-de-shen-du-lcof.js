647c070c44dc9bfe307aa91f935869b29235826d
jiangshanmeta <540118044@qq.com>
2020-06-26 00:17:16 +0800

    55

---
+++ javascript/面试题55 - I.er-cha-shu-de-shen-du-lcof.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(!root){
+        return 0;
+    }
+    return Math.max(
+        maxDepth(root.left),
+        maxDepth(root.right)
+    )+1;
+};
