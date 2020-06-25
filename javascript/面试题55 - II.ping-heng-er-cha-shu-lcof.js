3665ec4e3519e53a2abdbbeb2a15141f07d713f8
jiangshanmeta <540118044@qq.com>
2020-06-26 00:17:21 +0800

    55

---
+++ javascript/面试题55 - II.ping-heng-er-cha-shu-lcof.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    return postOrder(root)[0]
+};
+
+function postOrder(root){
+    if(!root){
+        return [true,0];
+    }
+    const [leftIs,leftDepth] = postOrder(root.left);
+    if(!leftIs){
+        return [false,0];
+    }
+    const [rightIs,rightDepth] = postOrder(root.right);
+    if(!rightIs){
+        return [false,0];
+    }
+    const diff = Math.abs(leftDepth-rightDepth);
+    if(diff>1){
+        return [false,0];
+    }
+
+    return [
+        true,
+        Math.max(leftDepth,rightDepth)+1,
+    ];
+}
