e7b0cb959cb9e4cc425f6415e54eed1737b7dd2f
jiangshanmeta <540118044@qq.com>
2020-06-27 14:52:21 +0800

    1487

---
+++ javascript/1487.making-file-names-unique.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} names
+ * @return {string[]}
+ */
+var getFolderNames = function(names) {
+    const map = {};
+    const result = [];
+    for(let i=0;i<names.length;i++){
+        const name = names[i];
+        if(map[name] === undefined){
+            result.push(name);
+            map[name] = 1;
+        }else{
+            let index= map[name];
+            while(map[`${name}(${index})`] !== undefined){
+                index++;
+            }
+            result.push(`${name}(${index})`);
+            map[name] = index+1;
+            map[`${name}(${index})`] = 1;
+        }
+    }
+    return result;
+};
