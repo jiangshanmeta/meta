710396164fbc44abb095a9cd69ab7a1dbb8d20fa
jiangshanmeta <540118044@qq.com>
2020-06-28 00:07:09 +0800

    1

---
+++ javascript/面试题 08_09.bracket-lcci.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    const result = [];
+    backTracking(n,n,[],result);
+    return result;
+};
+
+function backTracking(leftCount,rightCount,sequence,result){
+    if(leftCount === 0 && rightCount === 0){
+        result.push(sequence.join(''));
+        return
+    }
+    if(leftCount>0){
+        sequence.push('(');
+        backTracking(leftCount-1,rightCount,sequence,result);
+        sequence.pop();
+    }
+    
+    if(rightCount>leftCount){
+        sequence.push(')');
+        backTracking(leftCount,rightCount-1,sequence,result);
+        sequence.pop();
+    }
+}
