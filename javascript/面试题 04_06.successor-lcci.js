277fcf1a32455e3734be9fcb8de107e23a56ea31
jiangshanmeta <540118044@qq.com>
2020-06-29 00:35:26 +0800

    1

---
+++ javascript/面试题 04_06.successor-lcci.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @return {TreeNode}
+ */
+var inorderSuccessor = function(root, p) {
+    if(p.right){
+        p = p.right;
+        while(p.left){
+            p = p.left;
+        }
+        return p;
+    }
+    
+    const sequence = [];
+    findNode(root,p,sequence);
+    for(let i=sequence.length-2;i>-1;i--){
+        if(sequence[i+1] === sequence[i].left){
+            return sequence[i];
+        }
+    }
+    return null
+};
+
+function findNode(root,p,sequence){
+    if(!root){
+        return false;
+    }
+    sequence.push(root);
+    if(root.val === p.val){
+        return true;
+    }
+    if(findNode(root.left,p,sequence)){
+        return true;
+    }
+    if(findNode(root.right,p,sequence)){
+        return true;
+    }
+    sequence.pop();
+    return false;
+}
