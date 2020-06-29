c30f09a57b47c093b998d34b1ff6afd790b4d685
jiangshanmeta <540118044@qq.com>
2020-06-30 00:05:45 +0800

    1491

---
+++ javascript/1491.average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} salary
+ * @return {number}
+ */
+var average = function(salary) {
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for(let i=0;i<salary.length;i++){
+        sum += salary[i];
+        min = Math.min(min,salary[i]);
+        max = Math.max(max,salary[i]);
+    }
+    return (sum-min-max)/(salary.length-2);
+};
