917c65566ed4615f5c89573ddb78c43abe7ba9c1
jiangshanmeta <540118044@qq.com>
2020-06-30 00:05:50 +0800

    1492

---
+++ javascript/1492.the-kth-factor-of-n.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var kthFactor = function(n, k) {
+    const result = [];
+    const rest = [];
+    const sqrt = Math.sqrt(n);
+    let index = 1;
+    for(let i=1;i<=sqrt;i++){
+        if(n%i !== 0){
+            continue;
+        }
+        const r = n/i;
+        result.push(i);
+        if(i !== r){
+            rest.push(r);
+        }
+    }
+    while(rest.length){
+        result.push(rest.pop());
+    }
+    if(k>result.length){
+        return -1;
+    }
+    return result[k-1];
+};
