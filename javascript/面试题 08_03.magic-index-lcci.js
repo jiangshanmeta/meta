3d88ff94595deb548d2c96baca8b46c8e8200872
jiangshanmeta <540118044@qq.com>
2020-07-02 23:26:59 +0800

    1

---
+++ javascript/面试题 08_03.magic-index-lcci.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMagicIndex = function(nums) {
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] === i){
+            return i;
+        }
+    }
+    return -1;
+};
