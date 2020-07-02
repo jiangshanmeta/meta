94caf588f32fb606fc057c78f1f7b117aa33febb
jiangshanmeta <540118044@qq.com>
2020-07-03 00:07:13 +0800

    1

---
+++ javascript/面试题 08_08.permutation-ii-lcci.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} S
+ * @return {string[]}
+ */
+var permutation = function(S) {
+    const result = [];
+    backTracking([],S.split('').sort(),new Array(S.length).fill(false),result);
+    return result;
+};
+
+function backTracking(sequence,S,used,result){
+    if(sequence.length === S.length){
+        result.push(sequence.join(''));
+        return;
+    }
+    for(let i=0;i<S.length;i++){
+        if(used[i]){
+            continue;
+        }
+        if(i>0 && S[i] === S[i-1] && !used[i-1]){
+            continue;    
+        }
+        sequence.push(S[i]);
+        used[i] = true;
+        backTracking(sequence,S,used,result);
+        used[i] = false;
+        sequence.pop();
+    }
+}
