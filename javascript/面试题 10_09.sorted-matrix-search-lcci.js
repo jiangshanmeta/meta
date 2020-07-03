4ddac1a2620eac5f35b6d585aa082b9d14909934
jiangshanmeta <540118044@qq.com>
2020-07-04 00:12:16 +0800

    1

---
+++ javascript/面试题 10_09.sorted-matrix-search-lcci.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    if(matrix.length === 0){
+        return false;
+    }
+    let row = 0;
+    let column = matrix[0].length-1;
+    const ROW = matrix.length;
+    while(row<ROW && column>-1){
+        if(matrix[row][column]<target){
+            row++;
+        }else if(matrix[row][column]>target){
+            column--;
+        }else{
+            return true;
+        }
+    }
+    return false;
+};
