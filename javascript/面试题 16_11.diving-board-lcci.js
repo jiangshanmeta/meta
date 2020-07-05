07d3104062b443751a314958a4bd69a461da3163
jiangshanmeta <540118044@qq.com>
2020-07-05 12:08:53 +0800

    1

---
+++ javascript/面试题 16_11.diving-board-lcci.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} shorter
+ * @param {number} longer
+ * @param {number} k
+ * @return {number[]}
+ */
+var divingBoard = function(shorter, longer, k) {
+    if(k === 0){
+        return [];
+    }
+    
+    if(shorter === longer){
+        return [shorter*k];
+    }
+    const result = [];
+    for(let i=0;i<=k;i++){
+        result.push(shorter*(k-i)+longer*i);
+    }
+    return result;
+};
