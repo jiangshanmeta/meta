072251d48058b9c05fa98bf42fdff03a022742b8
jiangshanmeta <540118044@qq.com>
2020-07-06 20:39:09 +0800

    17

---
+++ javascript/面试题 17_12.binode-lcci.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var convertBiNode = function(root) {
+    if(!root){
+        return root;
+    }
+    root.right = convertBiNode(root.right);
+    if(!root.left){
+        return root;
+    }
+    const newRoot = convertBiNode(root.left);
+    let node = newRoot;
+    while(node.right){
+        node = node.right;
+    }
+    node.right = root;
+    root.left = null;
+    return newRoot;
+};
