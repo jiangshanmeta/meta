d418ca4a84c20ca24cfb097c362251f6464d37ce
jiangshanmeta <540118044@qq.com>
2020-07-06 20:39:17 +0800

    56

---
+++ javascript/面试题56 - II.shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    const counts = new Array(32).fill(0);
+    for(let num of nums){
+        let index = 31;
+        while(num>0){
+            if(num&1){
+                counts[index]++;
+            }
+            index--;
+            num >>>= 1;
+        }
+    }
+    let result = 0;
+    for(let i=0;i<32;i++){
+        result = result*2 + (counts[i]%3=== 0 ? 0:1);
+    }
+    return result;
+};
