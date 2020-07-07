61afecee0815e2acb05a4d85bdaa5270b2a885c4
jiangshanmeta <540118044@qq.com>
2020-07-07 21:17:35 +0800

    58

---
+++ javascript/面试题58 - I.fan-zhuan-dan-ci-shun-xu-lcof.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(str) {
+    const sequence = [];
+    let word = '';
+    for(let i=0;i<str.length;i++){
+        if(str[i] === ' '){
+            if(word.length){
+                sequence.unshift(word);
+                word = '';
+            }
+        }else{
+            word += str[i];
+        }
+    }
+    word && sequence.unshift(word);
+    
+    return sequence.join(" ");
+};
