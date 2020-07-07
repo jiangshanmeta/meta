cb6b3ea0d8f45e7fa0a7934229ad3aff1784b81a
jiangshanmeta <540118044@qq.com>
2020-07-07 21:17:31 +0800

    57

---
+++ javascript/面试题57.he-wei-sde-liang-ge-shu-zi-lcof.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let left = 0;
+    let right = nums.length-1;
+    while(left<right){
+        const sum = nums[left]+nums[right];
+        if(sum>target){
+            right--;
+        }else if(sum<target){
+            left++;
+        }else{
+            return [nums[left],nums[right]];
+        }
+    }
+};
