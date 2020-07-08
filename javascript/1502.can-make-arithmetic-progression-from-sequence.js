7d629cad8202bc92f6525d9ba8d45dcf7955309f
jiangshanmeta <540118044@qq.com>
2020-07-09 00:30:02 +0800

    1502

---
+++ javascript/1502.can-make-arithmetic-progression-from-sequence.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canMakeArithmeticProgression = function(arr) {
+    arr.sort((a,b)=>a-b);
+    const diff = arr[1]-arr[0];
+    for(let i=2;i<arr.length;i++){
+        if(arr[i]-arr[i-1] !== diff){
+            return false;
+        }
+    }
+    return true;
+};
