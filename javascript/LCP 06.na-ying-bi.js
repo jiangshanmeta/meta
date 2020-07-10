74cc42fe30480ab75aa9788ada11d39e966fd239
jiangshanmeta <540118044@qq.com>
2020-07-10 21:26:49 +0800

    06

---
+++ javascript/LCP 06.na-ying-bi.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} coins
+ * @return {number}
+ */
+var minCount = function(coins) {
+    let result = 0;
+    for(let i=0;i<coins.length;i++){
+        let coin = coins[i];
+        if(coin&1){
+            coin++
+        }        
+        result += (coin>>1);
+
+    }
+    return result;
+};
