45bb479df86b327d60d4a6a917b909012c9578a8
jiangshanmeta <540118044@qq.com>
2020-07-12 12:47:32 +0800

    35

---
+++ javascript/剑指Offer35.fu-za-lian-biao-de-fu-zhi-lcof.100300.js
@@ -0,0 +1,45 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var copyRandomList = function(head) {
+    if(!head){
+        return head;
+    }
+    let node = head;
+    while(node){
+        const newNode = new Node(node.val);
+        newNode.next = node.next;
+        node.next = newNode;
+        node = newNode.next;
+    }
+    node = head;
+    while(node){
+        if(node.random){
+            node.next.random = node.random.next;
+        }
+        node = node.next.next;
+    }
+    
+    let result = head.next;
+    let node1 = head;
+    let node2 = head.next;
+    while(node1){
+        node1.next = node2.next;
+        if(node2.next){
+            node2.next = node2.next.next;    
+        }
+        node1 = node1.next;
+        node2 = node2.next;
+    }
+    return result;
+};
