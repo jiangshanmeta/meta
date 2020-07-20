fb3ee8d0b0cc173c54ba975985f7666a56f96ecc
jiangshanmeta <540118044@qq.com>
2020-07-21 00:20:37 +0800

    443

---
+++ javascript/0443.string-compression.443.js
@@ -0,0 +1,21 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+    let endIndex = chars.length-1;
+    while(endIndex>-1){
+        let startIndex = endIndex-1;
+        while(startIndex>-1 && chars[startIndex] === chars[endIndex]){
+            startIndex--;
+        }
+        let count = endIndex-startIndex;
+        if(count>1){
+            chars.splice(startIndex+2,count-1,...(count+'').split(''));
+        }
+        
+        endIndex = startIndex;
+    }
+    
+    return chars.length;
+};
