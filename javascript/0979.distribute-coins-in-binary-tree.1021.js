f08edbe1bf494b50b6f4317ac30109e8c950ff2e
jiangshanmeta <540118044@qq.com>
2020-07-27 21:29:37 +0800

    979

---
+++ javascript/0979.distribute-coins-in-binary-tree.1021.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+
+
+var distributeCoins = function(root) {
+    let result = 0;
+    function postOrder(node){
+        if(node === null){
+            return 0;
+        }
+        const leftRst = postOrder(node.left);
+        const rightRst = postOrder(node.right);
+        let centerCoins = node.val;
+
+        result += Math.abs(leftRst);
+        result += Math.abs(rightRst);
+        return leftRst+rightRst+node.val-1;
+    }
+    postOrder(root);
+    return result;
+};
