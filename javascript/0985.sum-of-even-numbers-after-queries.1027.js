8697e4bc169b5e85c1f6999d5a2de86d681ba58f
jiangshanmeta <540118044@qq.com>
2020-07-28 20:13:38 +0800

    985

---
+++ javascript/0985.sum-of-even-numbers-after-queries.1027.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} A
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+var sumEvenAfterQueries = function(A, queries) {
+    const result = new Array(queries.length);
+    A[queries[0][1]] += queries[0][0];
+
+    result[0] = A.reduce((sum,item)=>{
+        if(item%2 === 0){
+            sum += item;
+        }
+        return sum;
+    },0);
+    
+    for(let i=1;i<queries.length;i++){
+        const index = queries[i][1];
+        const val = queries[i][0];
+        const isEven = val%2 === 0;
+        const isOldEven = A[index]%2 === 0;
+        if(isOldEven){
+            if(isEven){
+                result[i] = result[i-1] + val;
+            }else{
+                result[i] = result[i-1] - A[index];
+            }
+        }else{
+            if(isEven){
+                result[i] = result[i-1];
+            }else{
+                result[i] = result[i-1] + val + A[index];
+            }
+        }
+
+        A[index] += val;
+    }
+    
+    
+    return result;
+};
