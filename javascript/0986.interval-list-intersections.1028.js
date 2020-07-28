905daa98c802bb4a53c859233515f41a2a0a1d96
jiangshanmeta <540118044@qq.com>
2020-07-28 20:13:44 +0800

    986

---
+++ javascript/0986.interval-list-intersections.1028.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for an interval.
+ * function Interval(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ * }
+ */
+/**
+ * @param {Interval[]} A
+ * @param {Interval[]} B
+ * @return {Interval[]}
+ */
+var intervalIntersection = function(A, B) {
+    const result = [];
+    let indexA = 0;
+    let indexB = 0;
+    while(indexA<A.length && indexB<B.length){
+        const intervalA = A[indexA];
+        const intervalB = B[indexB];
+        if(intervalA.end<intervalB.start){
+            indexA++;
+        }else if(intervalB.end<intervalA.start){
+            indexB++;
+        }else{
+            const start = Math.max(intervalA.start,intervalB.start);
+            const end = Math.min(intervalA.end,intervalB.end);
+            result.push(new Interval(start,end));
+            intervalA.end === end && indexA++;
+            intervalB.end === end && indexB++;
+            
+        }
+    }
+
+    return result;
+};
