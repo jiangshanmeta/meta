e687ed394689039fefca668a7bbd25e550cc0181
jiangshanmeta <540118044@qq.com>
2020-07-29 21:10:51 +0800

    988

---
+++ javascript/0988.smallest-string-starting-from-leaf.1030.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var smallestFromLeaf = function(root) {
+    let result;
+    function dfs(node,str){
+        str = String.fromCharCode(node.val+97)+str;
+        if(node.left === null && node.right === null){
+            if(result === undefined){
+                result = str;
+            }else if(str<result){
+                result = str;
+            }
+        }else{
+            node.left && dfs(node.left,str);
+            node.right && dfs(node.right,str);
+        }
+        
+    }
+    dfs(root,'');
+    return result;
+};
