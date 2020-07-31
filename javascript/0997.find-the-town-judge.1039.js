4ad9de28662fb8961e5aba25ecdb09afe258c27e
jiangshanmeta <540118044@qq.com>
2020-07-31 21:23:26 +0800

    997

---
+++ javascript/0997.find-the-town-judge.1039.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} N
+ * @param {number[][]} trust
+ * @return {number}
+ */
+var findJudge = function(N, trust) {
+    const trustArr = new Array(N+1).fill(0);
+    const trustedArr = new Array(N+1).fill(0);
+    for(let i=0;i<trust.length;i++){
+        const [a,b] = trust[i];
+        trustArr[a]++;
+        trustedArr[b]++;
+    }
+    // console.log(trustArr,trustedArr)
+    for(let i=1;i<=N;i++){
+        if(trustedArr[i] === N-1 && trustArr[i] === 0){
+            return i;
+        }
+    }
+    
+    return -1;
+};
