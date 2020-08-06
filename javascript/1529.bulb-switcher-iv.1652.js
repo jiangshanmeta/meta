80ab65d3d111a4d67e5c7d057432eebb7ad8ab51
jiangshanmeta <540118044@qq.com>
2020-08-06 21:56:58 +0800

    1529

---
+++ javascript/1529.bulb-switcher-iv.1652.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} target
+ * @return {number}
+ */
+var minFlips = function(target) {
+    let result = 0;
+    let current = 0;
+    for(let i=0;i<target.length;i++){
+        if((+target[i]) !== current ){
+            result++;
+            current ^= 1;
+        }
+    }
+    return result;
+};
