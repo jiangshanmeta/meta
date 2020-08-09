abdd758ff51d3f44cacd01a056957d57f9333bb8
jiangshanmeta <540118044@qq.com>
2020-08-09 10:03:53 +0800

    1

---
+++ javascript/面试题02_06.palindrome-linked-list-lcci.100164.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    const list = [];
+    while(head){
+        list.push(head.val);
+        head = head.next;
+    }
+    let left = 0;
+    let right = list.length-1;
+    while(left<right){
+        if(list[left++] !== list[right--]){
+            return false;
+        }
+    }
+    return true;
+};
