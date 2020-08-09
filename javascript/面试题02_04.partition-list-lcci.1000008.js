664eca04f4d500c118272f95da54edf11fc79073
jiangshanmeta <540118044@qq.com>
2020-08-09 10:03:44 +0800

    1

---
+++ javascript/面试题02_04.partition-list-lcci.1000008.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+    const beforeHead = new ListNode();
+    const afterHead = new ListNode();
+    let beforeNode = beforeHead;
+    let afterNode = afterHead;
+    while(head){
+        const n = head.next;
+        head.next = null;
+        if(head.val<x){
+            beforeNode.next = head;
+            beforeNode = head;
+        }else{
+            afterNode.next = head;
+            afterNode = head;
+        }
+        head = n;
+    }
+    if(!beforeHead.next){
+        return afterHead.next;
+    }
+    beforeNode.next = afterHead.next;
+    return beforeHead.next;
+};
