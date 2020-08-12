043cd7129c256b5b0c60542a670dc274ca1ee91d
jiangshanmeta <540118044@qq.com>
2020-08-13 00:05:29 +0800

    1539

---
+++ javascript/1539.kth-missing-positive-number.1646.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findKthPositive = function(arr, k) {
+    let number = 1;
+    let index = 0;
+    while(true){
+        if(index<arr.length && arr[index] === number){
+            index++;
+            number++;
+            continue;
+        }
+        k--;
+        if(k === 0){
+            return number;
+        }
+        number++;
+    }
+};
