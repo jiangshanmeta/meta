4d59cd5b39d6db8df789b7c339d1dc1cd44f630b
jiangshanmeta <540118044@qq.com>
2020-08-14 21:38:39 +0800

    1542

---
+++ javascript/1542.find-longest-awesome-substring.1668.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestAwesome = function(s) {
+    let result = 0;
+    let binary = 0;
+    const map = {
+        0:-1,
+    };
+    for(let i=0;i<s.length;i++){
+        const mask = 1 << (+s[i]);
+        binary ^= mask;
+        if(map[binary] !== undefined){
+            result = Math.max(result,i-map[binary]);
+        }else{
+            map[binary] = i;
+        }
+        for(let j=0;j<10;j++){
+            const newBinary = binary ^ (1 << (j));
+            if(map[newBinary] !== undefined){
+               result = Math.max(result,i-map[newBinary]); 
+            }
+        }
+        
+        
+    }
+    return result;
+};
