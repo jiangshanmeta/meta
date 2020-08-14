419e0c79272e0289f80fd22e5279c4cce59ecf86
jiangshanmeta <540118044@qq.com>
2020-08-14 21:38:44 +0800

    1544

---
+++ javascript/1544.make-the-string-great.1666.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeGood = function(s) {
+    const stack = [];
+    const codeStack = [];
+    for(let i=0;i<s.length;i++){
+        const code = s.charCodeAt(i);
+        if(stack.length === 0){
+            stack.push(s[i]);
+            codeStack.push(code);
+            continue;
+        }
+        const lastCode = codeStack[codeStack.length-1];
+        if(Math.abs(code-lastCode) === 32){
+            stack.pop();
+            codeStack.pop();
+        }else{
+            stack.push(s[i]);
+            codeStack.push(code);
+        }
+        
+    }
+    return stack.join('');
+};
