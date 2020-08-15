a0fa0e3777bfd7b1422539c843a08e4ee263232c
jiangshanmeta <540118044@qq.com>
2020-08-15 10:00:26 +0800

    1005

---
+++ javascript/面试题10_05.sparse-array-search-lcci.100242.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} words
+ * @param {string} s
+ * @return {number}
+ */
+var findString = function(words, s,left=0,right=words.length-1) {
+    if(left>right){
+        return -1;
+    }
+    const mid = (left+right) >> 1;
+    if(words[mid] === ''){
+        const leftRst = findString(words,s,left,mid-1);
+        if(leftRst === -1){
+            return findString(words,s,mid+1,right);
+        }
+        return leftRst;
+    }else if(words[mid]<s){
+        return findString(words,s,mid+1,right);
+    }else if(words[mid]>s){
+        return findString(words,s,left,mid-1);
+    }else {
+        return mid;
+    }
+};
