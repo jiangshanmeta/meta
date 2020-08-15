62a2cff0c1fdaa7c849491aea936dabc3962ca03
jiangshanmeta <540118044@qq.com>
2020-08-15 10:00:20 +0800

    1002

---
+++ javascript/面试题10_02.group-anagrams-lcci.1000040.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const groupBy = strs.reduce((obj,str)=>{
+        const sorted = str.split('').sort();
+        if(!obj[sorted]){
+            obj[sorted] = [str];
+        }else{
+            obj[sorted].push(str);
+        }
+        return obj;
+    },Object.create(null));
+
+    return Object.values(groupBy);
+};
