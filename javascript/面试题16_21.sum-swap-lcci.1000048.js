e656842a9840126dee3f1a3e3b3bbe68367f8d5a
jiangshanmeta <540118044@qq.com>
2020-08-17 20:51:48 +0800

    1621

---
+++ javascript/面试题16_21.sum-swap-lcci.1000048.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} array1
+ * @param {number[]} array2
+ * @return {number[]}
+ */
+var findSwapValues = function(array1, array2) {
+    const map2 = Object.create(null);
+    let sum1 = 0;
+    let sum2 = 0;
+    for(let i=0;i<array1.length;i++){
+        sum1 += array1[i];
+    }
+    for(let i=0;i<array2.length;i++){
+        map2[array2[i]] = true;
+        sum2 += array2[i];
+    }
+    const diff = sum2-sum1;
+    if(diff&1){
+        return [];
+    }
+    const half = diff >> 1;
+    for(let i=0;i<array1.length;i++){
+        if(map2[array1[i]+half]){
+            return [array1[i],array1[i]+half];
+        }
+    }
+    
+    return []
+};
