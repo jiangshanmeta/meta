67d2fd4e26b932c07441b88a2097b3a1730be45b
jiangshanmeta <540118044@qq.com>
2020-08-21 21:30:33 +0800

    1553

---
+++ javascript/1553.minimum-number-of-days-to-eat-n-oranges.1676.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const cache = Object.create(null);
+cache[0] = 0;
+var minDays = function(n) {
+    if(n in cache){
+        return cache[n];
+    }
+    cache[n] = Math.min(
+        1+n%2+minDays((n-n%2)/2),
+        1+n%3+minDays((n-n%3)/3),
+        n
+    )
+    return cache[n]
+};
