09b4219d27a21f947982a7c1e94193d495289fdf
jiangshanmeta <540118044@qq.com>
2020-08-21 21:30:24 +0800

    1551

---
+++ javascript/1551.minimum-operations-to-make-array-equal.1674.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var minOperations = function(n) {
+    const pairs = n >> 1;
+    if(n&1){
+        return pairs*(pairs+1);
+    }else{
+        return pairs*pairs;
+    }
+};
