7a7326cc6042f4f3b2da51e82c9f21a4f10d5d2c
jiangshanmeta <540118044@qq.com>
2020-08-21 21:30:16 +0800

    1550

---
+++ javascript/1550.three-consecutive-odds.1293.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+    for(let i=0;i<arr.length-2;i++){
+        if(arr[i]%2 === 1 && arr[i+1]%2 === 1 && arr[i+2]%2 === 1){
+            return true;
+        }
+        
+    }
+    return false;
+};
