de904ef2951bd8c8ef136430519a60ed521780d3
jiangshanmeta <540118044@qq.com>
2020-09-06 22:47:31 +0800

    1556

---
+++ javascript/1556.thousand-separator.1660.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var thousandSeparator = function(n) {
+    const str = String(n);
+    const helper = [];
+    const result = [];
+    let index = str.length-1;
+    while(index>-1){
+        while(index>-1 && helper.length<3){
+            helper.push(str[index--]);
+        }
+        helper.reverse();
+        result.push(helper.join(''));
+        helper.length = 0;
+    }
+    result.reverse();
+    return result.join('.')
+};
