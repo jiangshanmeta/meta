a585c4ae230d09f73d2ca190596bb458c4314b8d
jiangshanmeta <540118044@qq.com>
2020-09-06 22:47:35 +0800

    1557

---
+++ javascript/1557.minimum-number-of-vertices-to-reach-all-nodes.1661.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
+var findSmallestSetOfVertices = function(n, edges) {
+    const canReachByOther = new Array(n).fill(false);
+    for(let i=0;i<edges.length;i++){
+        canReachByOther[edges[i][1]] = true;
+    }
+    const result = [];
+    for(let i=0;i<n;i++){
+        if(!canReachByOther[i]){
+            result.push(i);
+        }
+    }
+    return result;
+};
