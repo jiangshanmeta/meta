3789a29b6fcf074bf58c48d5f30d5db7b81c3d1c
jiangshanmeta <540118044@qq.com>
2020-09-06 22:47:39 +0800

    1560

---
+++ javascript/1560.most-visited-sector-in-a-circular-track.1682.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} n
+ * @param {number[]} rounds
+ * @return {number[]}
+ */
+var mostVisited = function(n, rounds) {
+    const counts = new Array(n+1).fill(0);
+    counts[rounds[0]]++;
+    for(let i=0;i<rounds.length-1;i++){
+        const from = rounds[i];
+        const to = rounds[i+1];
+        if(from<to){
+            for(let j=from+1;j<=to;j++){
+                counts[j]++;
+            }
+        }else{
+            for(let j=from+1;j<=n;j++){
+                counts[j]++;
+            }
+            for(let j=1;j<=to;j++){
+                counts[j]++;
+            }
+        }
+    }
+    let result = [];
+    let maxCount = 0;
+    for(let i=1;i<n+1;i++){
+        if(counts[i]>maxCount){
+            result = [i];
+            maxCount = counts[i];
+        }else if(counts[i] === maxCount){
+            result.push(i);
+        }
+    }
+    return result;
+};
