2b5f07a05cb4d9f732732fd2099d8c1032b75f5f
jiangshanmeta <540118044@qq.com>
2020-09-26 13:16:49 +0800

    104 ts

---
+++ typescript/0104.maximum-depth-of-binary-tree.104.ts
@@ -0,0 +1,17 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function maxDepth(root: TreeNode | null): number {
+    if(root === null){
+        return 0;
+    }
+    return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+};
