908e3c89d394faf333fb3e9431f298bc66c69ae7
jiangshanmeta <540118044@qq.com>
2020-09-27 22:56:18 +0800

    110

---
+++ typescript/0110.balanced-binary-tree.110.ts
@@ -0,0 +1,36 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function isBalanced(root: TreeNode | null): boolean {
+    return helper(root)[0];
+};
+
+function helper(root:TreeNode|null):[boolean,number]{
+    if(root === null){
+        return [true,0];
+    }
+    const [LB,LH] = helper(root.left);
+    if(!LB){
+        return [false,0];
+    }
+    const [RB,RH] = helper(root.right);
+    if(!RB){
+        return [false,0];
+    }
+    const diff = Math.abs(LH-RH);
+    if(diff>1){
+        return [false,0];
+    }
+    return [
+        true,
+        Math.max(LH,RH)+1
+    ];
+}
