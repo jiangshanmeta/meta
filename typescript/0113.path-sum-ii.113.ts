1d6afee71d8f5ba3a79150b79b13f8b4be5c2d4a
jiangshanmeta <540118044@qq.com>
2020-09-29 00:02:09 +0800

    113

---
+++ typescript/0113.path-sum-ii.113.ts
@@ -0,0 +1,30 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function pathSum(root: TreeNode | null, sum: number): number[][] {
+    if(root === null){
+        return [];
+    }
+    const result:number[][] = [];
+    dfs(root,sum,[],result);
+    return result;
+};
+
+function dfs(root:TreeNode,rest:number,path:number[],result:number[][]):void{
+    rest -= root.val;
+    path.push(root.val);
+    if(root.left === null && root.right === null && rest === 0){
+        result.push(path.slice());
+    }
+    root.left !== null && dfs(root.left,rest,path,result);
+    root.right !== null && dfs(root.right,rest,path,result);
+    path.pop();
+}
