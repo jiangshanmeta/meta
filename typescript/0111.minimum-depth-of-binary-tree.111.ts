7c34e7da45218db0f2a5ea72108e20778559a6af
jiangshanmeta <540118044@qq.com>
2020-09-29 00:02:04 +0800

    111

---
+++ typescript/0111.minimum-depth-of-binary-tree.111.ts
@@ -0,0 +1,25 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function minDepth(root: TreeNode | null): number {
+    if(root === null){
+        return 0;
+    }
+    if(root.left !== null && root.right !== null){
+        return Math.min(minDepth(root.left),minDepth(root.right))+1;
+    }else if(root.left !== null){
+        return minDepth(root.left)+1;
+    }else if(root.right !== null){
+        return minDepth(root.right)+1;
+    }else{
+        return 1;
+    }
+};
