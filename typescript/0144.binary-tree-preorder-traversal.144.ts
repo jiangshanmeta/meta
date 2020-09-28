5eb33dc3424d0da9227c3c425c4fca93ed28ae4c
jiangshanmeta <540118044@qq.com>
2020-09-28 23:19:13 +0800

    144

---
+++ typescript/0144.binary-tree-preorder-traversal.144.ts
@@ -0,0 +1,25 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function preorderTraversal(root: TreeNode | null): number[] {
+    if(root === null){
+        return [];
+    }
+    const result:number[] = [];
+    preorder(root,result);
+    return result;
+};
+
+function preorder(root:TreeNode,result:number[]){
+    result.push(root.val);
+    root.left !== null && preorder(root.left,result);
+    root.right !== null && preorder(root.right,result);
+}
