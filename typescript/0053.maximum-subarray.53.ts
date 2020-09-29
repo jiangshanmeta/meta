b637ba631102873eb9174c5ae43b8750e8572d6b
jiangshanmeta <540118044@qq.com>
2020-09-30 00:03:07 +0800

    53 ts

---
+++ typescript/0053.maximum-subarray.53.ts
@@ -0,0 +1,9 @@
+function maxSubArray(nums: number[]): number {
+    let result:number = nums[0];
+    let pre:number = nums[0];
+    for(let i=1;i<nums.length;i++){
+        pre = Math.max(0,pre)+nums[i];
+        result = Math.max(result,pre);
+    }
+    return result;
+};
