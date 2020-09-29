3048c7957f0aa99802e988ccf5db558ef3a70099
jiangshanmeta <540118044@qq.com>
2020-09-30 00:02:48 +0800

    1 ts

---
+++ typescript/0001.two-sum.1.ts
@@ -0,0 +1,10 @@
+function twoSum(nums: number[], target: number): number[] {
+    const map = new Map<number,number>();
+    for(let i=0;i<nums.length;i++){
+        if(map.has(target-nums[i])){
+            return [<number>map.get(target-nums[i]),i];
+        }
+        map.set(nums[i],i);
+    }
+    return []
+};
