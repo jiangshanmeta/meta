1ac857ea017ea44aad560d60fb3a387fad047d69
jiangshanmeta <540118044@qq.com>
2020-09-30 00:02:54 +0800

    49 ts

---
+++ typescript/0049.group-anagrams.49.ts
@@ -0,0 +1,13 @@
+function groupAnagrams(strs: string[]): string[][] {
+    const map = new Map<string,string[]>();
+    for(let i=0;i<strs.length;i++){
+        const key = strs[i].split('').sort().join('');
+        if(map.has(key)){
+            const list = <string[]>map.get(key);
+            list.push(strs[i]);
+        }else{
+            map.set(key,[strs[i]]);
+        }
+    }
+    return [...map.values()];
+};
