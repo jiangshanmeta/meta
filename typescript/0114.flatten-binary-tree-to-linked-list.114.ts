dc7ea953227d9fbc4c410bef37e89532b92dd025
jiangshanmeta <540118044@qq.com>
2020-10-01 08:48:45 +0800

    114

---
+++ typescript/0114.flatten-binary-tree-to-linked-list.114.ts
@@ -0,0 +1,37 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+/**
+ Do not return anything, modify root in-place instead.
+ */
+function flatten(root: TreeNode | null): void {
+    if(root === null){
+        return;
+    }
+    flatten(root.left);
+    flatten(root.right);
+    if(root.left !== null){
+        if(root.right === null){
+            root.right = root.left;
+            root.left = null;
+        }else{
+            const leftH = root.left;
+            root.left = null;
+            let node = leftH;
+            const rightH = root.right;
+            while(node.right !== null){
+                node = node.right;
+            }
+            node.right = rightH;
+            root.right = leftH;
+        }
+    }
+};
