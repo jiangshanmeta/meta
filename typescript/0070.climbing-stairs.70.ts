d5d7fc0494c48423a5cafaf0da2c5e5238586dc9
jiangshanmeta <540118044@qq.com>
2020-10-01 08:48:25 +0800

    70

---
+++ typescript/0070.climbing-stairs.70.ts
@@ -0,0 +1,9 @@
+function climbStairs(n: number): number {
+    const dp = new Array(n+1);
+    dp[0] = 1;
+    dp[1] = 1;
+    for(let i=2;i<dp.length;i++){
+        dp[i] = dp[i-1]+dp[i-2];
+    }
+    return dp[n]
+};
