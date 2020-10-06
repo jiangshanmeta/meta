c7a97880f041f0f6fb974e2c75f4d65c8bcde609
jiangshanmeta <540118044@qq.com>
2020-10-06 14:29:09 +0800

    257

---
+++ typescript/0257.binary-tree-paths.257.ts
@@ -0,0 +1,29 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function binaryTreePaths(root: TreeNode | null): string[] {
+    if(root === null){
+        return [];
+    }
+    const result:string[] = [];
+    dfs(root,[],result);
+    return result;
+};
+
+function dfs(root:TreeNode,path:number[],result:string[]):void{
+    path.push(root.val);
+    if(root.left === null && root.right === null){
+        result.push(path.join('->'));
+    }
+    root.left !== null && dfs(root.left,path,result);
+    root.right !== null && dfs(root.right,path,result);
+    path.pop();
+}
