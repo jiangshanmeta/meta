04e953ccc55497fccc7790edc1da9fa4a7ddc721
jiangshanmeta <540118044@qq.com>
2020-10-07 16:09:29 +0800

    344

---
+++ typescript/0344.reverse-string.344.ts
@@ -0,0 +1,14 @@
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+    let left:number = 0;
+    let right:number = s.length-1;
+    while(left<right){
+        const tmp = s[left];
+        s[left] = s[right];
+        s[right] = tmp;
+        left++;
+        right--;
+    }
+};
