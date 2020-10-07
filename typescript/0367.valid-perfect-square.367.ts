7c4d2beadb02d916e7d513db3284e48778398ab9
jiangshanmeta <540118044@qq.com>
2020-10-07 16:09:34 +0800

    367

---
+++ typescript/0367.valid-perfect-square.367.ts
@@ -0,0 +1,16 @@
+function isPerfectSquare(num: number): boolean {
+    let low = 1;
+    let high = num;
+    while(low<=high){
+        const mid = low + ((high-low)>>>1);
+        const square = mid*mid;
+        if(square>num){
+            high = mid-1;
+        }else if(square<num){
+            low = mid+1;
+        }else{
+            return true;
+        }
+    }
+    return false;
+};
