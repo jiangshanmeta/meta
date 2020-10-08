f33babeba9a95aa90404b652fada40b9e54ed222
jiangshanmeta <540118044@qq.com>
2020-10-08 16:01:20 +0800

    412

---
+++ typescript/0412.fizz-buzz.412.ts
@@ -0,0 +1,17 @@
+function fizzBuzz(n: number): string[] {
+    const result:string[] = [];
+    for(let i=1;i<=n;i++){
+        const mod3 = i%3 === 0;
+        const mod5 = i%5 === 0;
+        if(mod3 && mod5){
+            result.push('FizzBuzz');
+        }else if(mod3){
+            result.push('Fizz')
+        }else if(mod5){
+            result.push('Buzz')
+        }else{
+            result.push(String(i));
+        }
+    }
+    return result
+};
