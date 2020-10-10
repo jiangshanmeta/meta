2cf3166f358c2f8c6eaf5e86861b5e235bb6d95f
jiangshanmeta <540118044@qq.com>
2020-10-10 22:14:48 +0800

    617

---
+++ typescript/0617.merge-two-binary-trees.617.ts
@@ -0,0 +1,19 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function mergeTrees(t1: TreeNode | null, t2: TreeNode | null): TreeNode | null {
+    if(t1 !== null && t2 !== null){
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left,t2.left);
+        t1.right = mergeTrees(t1.right,t2.right);
+    }
+    return t1 || t2;
+};
