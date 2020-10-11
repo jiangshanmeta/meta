02fdec070cb3f22207d61962fb956583f1a320dd
jiangshanmeta <540118044@qq.com>
2020-10-11 19:04:21 +0800

    700

---
+++ typescript/0700.search-in-a-binary-search-tree.783.ts
@@ -0,0 +1,23 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+    if(root === null){
+        return root;
+    }
+    if(root.val>val){
+        return searchBST(root.left,val);
+    }else if(root.val<val){
+        return searchBST(root.right,val);
+    }else{
+        return root;
+    }
+};
