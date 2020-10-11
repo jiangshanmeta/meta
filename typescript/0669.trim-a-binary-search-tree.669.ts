de5dda20750be10b813a55cfb24b40ab9abb6d69
jiangshanmeta <540118044@qq.com>
2020-10-11 19:04:16 +0800

    669

---
+++ typescript/0669.trim-a-binary-search-tree.669.ts
@@ -0,0 +1,24 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function trimBST(root: TreeNode | null, low: number, high: number): TreeNode | null {
+    if(root === null){
+        return null;
+    }
+    if(root.val<low){
+        return trimBST(root.right,low,high);
+    }else if(root.val>high){
+        return trimBST(root.left,low,high);
+    }
+    root.left = trimBST(root.left,low,high);
+    root.right = trimBST(root.right,low,high);
+    return root;
+};
