017c5535029c67af1f5821d142bc5f9e35fdd06d
jiangshanmeta <540118044@qq.com>
2020-10-12 19:35:17 +0800

    814

---
+++ typescript/0814.binary-tree-pruning.832.ts
@@ -0,0 +1,22 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function pruneTree(root: TreeNode | null): TreeNode | null {
+    if(root === null){
+        return null;
+    }
+    root.left = pruneTree(root.left);
+    root.right = pruneTree(root.right);
+    if(root.val === 0 && root.left === null && root.right === null){
+        return null;
+    }
+    return root;
+};
