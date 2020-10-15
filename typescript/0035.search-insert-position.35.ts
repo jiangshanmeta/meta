2c04d953e3569b8127c459305d7fca4a83f7dfe2
jiangshanmeta <540118044@qq.com>
2020-10-15 19:58:31 +0800

    35

---
+++ typescript/0035.search-insert-position.35.ts
@@ -0,0 +1,17 @@
+function searchInsert(nums: number[], target: number): number {
+    let result = nums.length-1;
+    let low = 0;
+    let high = nums.length;
+    while(low<=high){
+        const mid = (low+high)>>1;
+        if(nums[mid]<target){
+            low = mid+1;
+        }else if(nums[mid]>target){
+            high = mid-1;
+            result = Math.min(result,mid);
+        }else{
+            return mid;
+        }
+    }
+    return result;
+};
