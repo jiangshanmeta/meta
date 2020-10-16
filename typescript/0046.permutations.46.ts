52468a285adad0e7d45cf35473cb859aa68d830f
jiangshanmeta <540118044@qq.com>
2020-10-16 19:06:08 +0800

    46

---
+++ typescript/0046.permutations.46.ts
@@ -0,0 +1,27 @@
+function permute(nums: number[]): number[][] {
+    const result:number[][] = [];
+    backTracking(
+        nums,
+        new Array(nums.length).fill(false),
+        [] as number[],
+        result
+    );
+    return result;
+};
+
+function backTracking(nums:number[],used:boolean[],sequence:number[],result:number[][]){
+    if(sequence.length === nums.length){
+        result.push(sequence.slice())
+        return;
+    }
+    for(let i=0;i<nums.length;i++){
+        if(used[i]){
+            continue;
+        }
+        sequence.push(nums[i]);
+        used[i] = true;
+        backTracking(nums,used,sequence,result);
+        used[i] = false;
+        sequence.pop();
+    }
+}
