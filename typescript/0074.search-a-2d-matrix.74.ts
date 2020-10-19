b2c3af51fc43ed6d18d8d5991646d4198cbc3f72
jiangshanmeta <540118044@qq.com>
2020-10-19 18:20:02 +0800

    74

---
+++ typescript/0074.search-a-2d-matrix.74.ts
@@ -0,0 +1,22 @@
+function searchMatrix(matrix: number[][], target: number): boolean {
+    const M = matrix.length;
+    if(M === 0){
+        return false;
+    }
+    const N = matrix[0].length;
+    if(N === 0){
+        return false;
+    }
+    let row = 0;
+    let column = N-1;
+    while(row<M && column>-1){
+        if(matrix[row][column]<target){
+            row++;
+        }else if(matrix[row][column]>target){
+            column--;
+        }else{
+            return true;
+        }
+    }
+    return false;
+};
