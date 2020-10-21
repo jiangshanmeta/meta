d74b30ca3dd4eb0094be8c3032bac46954c2b3eb
jiangshanmeta <540118044@qq.com>
2020-10-21 18:25:35 +0800

    78

---
+++ typescript/0078.subsets.78.ts
@@ -0,0 +1,21 @@
+function subsets(nums: number[]): number[][] {
+    const result:number[][] = [];
+    backTracking(
+        nums,
+        0,
+        [] as number[],
+        result
+    );
+    return result;
+};
+
+function backTracking(nums:number[],index:number,sequence:number[],result:number[][]){
+    if(index === nums.length){
+        result.push(sequence.slice());
+        return;
+    }
+    backTracking(nums,index+1,sequence,result);
+    sequence.push(nums[index++]);
+    backTracking(nums,index,sequence,result);
+    sequence.pop();
+}
