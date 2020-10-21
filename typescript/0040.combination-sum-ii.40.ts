2dc901247181ba5a14612f1e2871d52574b7df3d
jiangshanmeta <540118044@qq.com>
2020-10-21 18:25:25 +0800

    40

---
+++ typescript/0040.combination-sum-ii.40.ts
@@ -0,0 +1,42 @@
+function combinationSum2(candidates: number[], target: number): number[][] {
+    candidates.sort((a,b)=>a-b);
+    const result:number[][] = [];
+    backTracking(
+        candidates,
+        0,
+        target,
+        new Array(candidates.length).fill(false),
+        [] as number[],
+        result
+    );
+    return result;
+};
+
+function backTracking(candidates:number[],index:number,rest:number,used:boolean[],sequence:number[],result:number[][]){
+    if(rest === 0){
+        result.push(sequence.slice());
+        return;
+    }
+    
+    if(rest<0 || index === candidates.length){
+        return;
+    }
+    backTracking(
+        candidates,
+        index+1,
+        rest,
+        used,
+        sequence,
+        result
+    );
+    if(index>0 && candidates[index] === candidates[index-1] && !used[index-1]){
+        return;
+    }
+    sequence.push(candidates[index]);
+    used[index] = true;
+    backTracking(candidates,index+1,rest-candidates[index],used,sequence,result)
+    used[index] = false;
+    sequence.pop();
+
+
+}
