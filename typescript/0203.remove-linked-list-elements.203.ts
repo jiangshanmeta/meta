c5c91578eb6c8b87586d643bf68aee5ee52f6c89
jiangshanmeta <540118044@qq.com>
2020-10-23 18:06:06 +0800

    203

---
+++ typescript/0203.remove-linked-list-elements.203.ts
@@ -0,0 +1,22 @@
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+    const dummyHead = new ListNode(0);
+    let node:ListNode = dummyHead;
+    node.next = head;
+    while(node.next !== null){
+        if(node.next.val === val){
+            node.next = node.next.next;
+        }else{
+            node = <ListNode>node.next;
+        }
+    }
+    return dummyHead.next;
+};
