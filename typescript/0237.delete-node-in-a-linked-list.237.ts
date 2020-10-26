476d6cc0b56f25438520a0661c952ffa278efe1f
jiangshanmeta <540118044@qq.com>
2020-10-26 19:09:12 +0800

    237

---
+++ typescript/0237.delete-node-in-a-linked-list.237.ts
@@ -0,0 +1,18 @@
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+/**
+ Do not return anything, modify it in-place instead.
+ */
+function deleteNode(root: ListNode | null): void {
+    if(root !== null && root.next !== null){
+        root.val = root.next.val;
+        root.next = root.next.next
+    }
+};
