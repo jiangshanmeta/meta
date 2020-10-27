a6a5497b217a0c4e43433481e7cc6bd94a14eb84
jiangshanmeta <540118044@qq.com>
2020-10-27 19:03:51 +0800

    278

---
+++ typescript/0278.first-bad-version.278.ts
@@ -0,0 +1,25 @@
+/**
+ * The knows API is defined in the parent class Relation.
+ * isBadVersion(version: number): boolean {
+ *     ...
+ * };
+ */
+
+var solution = function(isBadVersion: any) {
+
+    return function(n: number): number {
+        let low = 1;
+        let high = n;
+        while(low<=high){
+            const mid = low + ((high-low)>>>1);
+            const is = <boolean>isBadVersion(mid);
+            if(is){
+                n = Math.min(n,mid);
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return n;
+    };
+};
