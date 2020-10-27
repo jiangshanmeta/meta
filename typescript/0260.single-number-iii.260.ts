432d138e71d2d515c320ee604b8d835976841fb1
jiangshanmeta <540118044@qq.com>
2020-10-27 19:03:34 +0800

    268

---
+++ typescript/0260.single-number-iii.260.ts
@@ -0,0 +1,17 @@
+function singleNumber(nums: number[]): number[] {
+    let xor = 0;
+    for(let i=0;i<nums.length;i++){
+        xor ^= nums[i];
+    }
+    let mask = 1;
+    while( (xor&mask) === 0){
+        mask <<= 1;
+    }
+    let result = 0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]&mask){
+            result ^= nums[i];
+        }
+    }
+    return [result,xor^result];
+};
