91ff8c394f3185b6cb8217ccca2cd09cbb17cdd6
jiangshanmeta <540118044@qq.com>
2020-11-08 13:16:00 +0800

    965

---
+++ typescript/0965.univalued-binary-tree.1005.ts
@@ -0,0 +1,24 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function isUnivalTree(root: TreeNode | null): boolean {
+    if(root === null){
+        return true;
+    }
+    return check(root,root.val);
+};
+
+function check(root:TreeNode|null,val:number):boolean{
+    if(root === null){
+        return true;
+    }
+    return root.val === val && check(root.left,val) && check(root.right,val);
+}
