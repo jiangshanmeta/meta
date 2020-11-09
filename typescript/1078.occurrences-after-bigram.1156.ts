2ccf977556830f80c9a084fd1ddae35694c43961
jiangshanmeta <540118044@qq.com>
2020-11-09 20:13:37 +0800

    1078

---
+++ typescript/1078.occurrences-after-bigram.1156.ts
@@ -0,0 +1,10 @@
+function findOcurrences(text: string, first: string, second: string): string[] {
+    const words:string[] = text.split(' ');
+    const result:string[] = [];
+    for(let i=2;i<words.length;i++){
+        if(words[i-1] === second && words[i-2] === first){
+            result.push(words[i]);
+        }
+    }
+    return result;
+};
