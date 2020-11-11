e97f4b979d27a7d3c817ec15826ebb6d73227d20
jiangshanmeta <540118044@qq.com>
2020-11-11 18:10:38 +0800

    1302

---
+++ typescript/1302.deepest-leaves-sum.1254.ts
@@ -0,0 +1,33 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function deepestLeavesSum(root: TreeNode | null): number {
+    if(root === null){
+        return 0;
+    }
+    return getLastLevelSum([root]);
+};
+
+function getLastLevelSum(levelNodes:TreeNode[]):number{
+    let sum = 0;
+    const nextLevel:TreeNode[] = [];
+    for(let i=0;i<levelNodes.length;i++){
+        const node = levelNodes[i];
+        sum += node.val;
+        node.left !== null && nextLevel.push(node.left);
+        node.right !== null && nextLevel.push(node.right);
+    }
+    if(nextLevel.length === 0){
+        return sum;
+    }else{
+        return getLastLevelSum(nextLevel);
+    }
+}
