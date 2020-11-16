8ef071a30606e46bff8373f644cfee3957f6b601
jiangshanmeta <540118044@qq.com>
2020-11-16 18:33:07 +0800

    1446

---
+++ typescript/1446.consecutive-characters.1542.ts
@@ -0,0 +1,14 @@
+function maxPower(s: string): number {
+    let result = 0;
+    let index = 0;
+    while(index<s.length){
+        const char = s[index++];
+        let count = 1;
+        while(index<s.length && s[index] === char){
+            index++;
+            count++;
+        }
+        result = Math.max(result,count)
+    }
+    return result;
+};
