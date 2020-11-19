8e23044904ca0ce15a3804ec63681dcbe7381f37
jiangshanmeta <540118044@qq.com>
2020-11-19 19:07:37 +0800

    1592

---
+++ typescript/1592.rearrange-spaces-between-words.1714.ts
@@ -0,0 +1,23 @@
+function reorderSpaces(text: string): string {
+    const words:string[] = [];
+    let count = 0;
+    let index = 0;
+    while(index<text.length){
+        while(index<text.length && text[index] === ' '){
+            index++;
+            count++;
+        }
+        const word:string[] = [];
+        while(index<text.length && text[index] !== ' '){
+            word.push(text[index++]);
+        }
+        word.length && words.push(word.join(''));
+    }
+    if(words.length === 1){
+        return words[0]+' '.repeat(count);
+    }
+
+    const rest = count%(words.length-1);
+    const per = (count-rest)/(words.length-1);
+    return words.join(' '.repeat(per))+' '.repeat(rest);
+};
