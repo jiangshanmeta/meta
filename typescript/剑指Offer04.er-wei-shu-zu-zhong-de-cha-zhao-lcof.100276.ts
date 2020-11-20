a31658fd381447a4dafb679231937ae96951e601
jiangshanmeta <540118044@qq.com>
2020-11-20 18:34:57 +0800

    4

---
+++ typescript/剑指Offer04.er-wei-shu-zu-zhong-de-cha-zhao-lcof.100276.ts
@@ -0,0 +1,17 @@
+function findNumberIn2DArray(matrix: number[][], target: number): boolean {
+    if(matrix.length === 0){
+        return false;
+    }
+    let row:number = 0;
+    let column:number = matrix[0].length-1;
+    while(row<matrix.length && column>-1){
+        if(matrix[row][column]>target){
+            column--;
+        }else if(matrix[row][column]<target){
+            row++;
+        }else{
+            return true;
+        }
+    }
+    return false;
+};
