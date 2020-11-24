75eb5e1cacd16a40835112be2af5578773cbd445
jiangshanmeta <540118044@qq.com>
2020-11-24 19:02:22 +0800

    18

---
+++ typescript/剑指Offer18.shan-chu-lian-biao-de-jie-dian-lcof.100299.ts
@@ -0,0 +1,22 @@
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+function deleteNode(head: ListNode | null, val: number): ListNode | null {
+    const dummyHead = new ListNode(0);
+    dummyHead.next = head;
+    let node:ListNode = dummyHead;
+    while(node.next !== null){
+        if(node.next.val === val){
+            node.next = node.next.next;
+        }else{
+            node = node.next;
+        }
+    }
+    return dummyHead.next;
+};
