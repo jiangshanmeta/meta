fbb12096ea43703db77360670d1b2d5307b780ad
jiangshanmeta <540118044@qq.com>
2020-11-25 19:17:22 +0800

    27

---
+++ typescript/剑指Offer27.er-cha-shu-de-jing-xiang-lcof.100288.ts
@@ -0,0 +1,23 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+
+function mirrorTree(root: TreeNode | null): TreeNode | null {
+    if(root === null){
+        return null;
+    }
+    const tmp = root.left;
+    root.left = root.right;
+    root.right = tmp;
+    mirrorTree(root.left);
+    mirrorTree(root.right);
+    return root;
+};
