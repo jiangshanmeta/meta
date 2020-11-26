e8ddb359eb86bbc2aa8b99670f2753172e762aa9
jiangshanmeta <540118044@qq.com>
2020-11-26 19:59:02 +0800

    32

---
+++ typescript/剑指Offer32-II.cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof.100312.ts
@@ -0,0 +1,32 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function levelOrder(root: TreeNode | null): number[][] {
+    if(root === null){
+        return [];
+    }
+    const result:number[][] = [];
+    bfs([root],result);
+    return result;
+};
+
+function bfs(levelNodes:TreeNode[],result:number[][]):void{
+    const levelVals:number[] = [];
+    const nextNodes:TreeNode[] = [];
+    for(let i=0;i<levelNodes.length;i++){
+        const node = levelNodes[i];
+        levelVals.push(node.val);
+        node.left !== null && nextNodes.push(node.left);
+        node.right !== null && nextNodes.push(node.right);
+    }
+    result.push(levelVals);
+    nextNodes.length>0 && bfs(nextNodes,result);
+}
