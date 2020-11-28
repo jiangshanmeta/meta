4ed37df6cc4eb72596ed35b151d8e323d707df1e
jiangshanmeta <540118044@qq.com>
2020-11-28 10:57:35 +0800

    53

---
+++ typescript/剑指Offer53-I.zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof.100329.ts
@@ -0,0 +1,39 @@
+function search(nums: number[], target: number): number {
+    let left1 = 0;
+    let right2 = nums.length-1;
+    while(left1<=right2){
+        const mid = left1 + ((right2-left1)>>1);
+        if(nums[mid]<target){
+            left1 = mid+1;
+        }else if(nums[mid]>target){
+            right2 = mid-1;
+        }else{
+            let right1 = mid-1;
+            let left2 = mid+1;
+            let low = mid;
+            let high = mid;
+            while(left1<=right1){
+                const mid1 = left1+ ((right1-left1)>>1);
+                if(nums[mid1]<target){
+                    left1 = mid1+1;
+                }else{
+                    low = Math.min(low,mid1);
+                    right1 = mid1-1;
+                }
+            }
+            while(left2<=right2){
+                const mid2 = left2+((right2-left2)>>1);
+                if(nums[mid2]>target){
+                    right2 = mid2-1;
+                }else{
+                    high = Math.max(high,mid2);
+                    left2 = mid2+1;
+                }
+            }
+            return high-low+1;
+        }
+
+    }
+
+    return 0;
+};
