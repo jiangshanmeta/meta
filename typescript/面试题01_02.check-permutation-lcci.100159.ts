a51705f6af295ff85faae5dba37f0ed6dcdd2b46
jiangshanmeta <540118044@qq.com>
2020-11-30 18:43:51 +0800

    2

---
+++ typescript/面试题01_02.check-permutation-lcci.100159.ts
@@ -0,0 +1,25 @@
+function CheckPermutation(s1: string, s2: string): boolean {
+    if(s1.length !== s2.length){
+        return false;
+    }
+    const map = new Map<string,number>();
+    for(let char of s1){
+        if(map.has(char)){
+            map.set(char,<number>map.get(char)+1)
+        }else{
+            map.set(char,1);
+        }
+    }
+    for(let char of s2){
+        if(!map.has(char)){
+            return false;
+        }
+        const count = <number>map.get(char);
+        if(count === 0){
+            return false;
+        }
+        map.set(char,count-1);
+    }
+
+    return true;
+};
