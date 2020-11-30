9277a1281771a378a441f3c6470d47d53755042e
jiangshanmeta <540118044@qq.com>
2020-11-30 18:43:44 +0800

    66

---
+++ typescript/剑指Offer66.gou-jian-cheng-ji-shu-zu-lcof.100338.ts
@@ -0,0 +1,19 @@
+function constructArr(a: number[]): number[] {
+    const befores:number[] = new Array(a.length);
+    let before = 1;
+    for(let i=0;i<a.length;i++){
+        befores[i] = before;
+        before *= a[i];
+    }
+    const afters:number[] = new Array(a.length);
+    let after = 1;
+    for(let i=a.length-1;i>-1;i--){
+        afters[i] = after;
+        after *= a[i];
+    }
+    const result:number[] = new Array(a.length);
+    for(let i=0;i<a.length;i++){
+        result[i] = befores[i]*afters[i];
+    }
+    return result;
+};
