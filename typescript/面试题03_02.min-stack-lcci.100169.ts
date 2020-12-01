cb3101e2ff1c18db1f75390eed082e5ff76dbecd
jiangshanmeta <540118044@qq.com>
2020-12-01 19:49:14 +0800

    3

---
+++ typescript/面试题03_02.min-stack-lcci.100169.ts
@@ -0,0 +1,38 @@
+class MinStack {
+    private stack1:number[] = [];
+    private stack2:number[] = [];
+    constructor() {
+
+    }
+
+    push(x: number): void {
+        this.stack1.push(x);
+        if(this.stack2.length === 0|| x<=this.stack2[this.stack2.length-1]){
+            this.stack2.push(x);
+        }
+    }
+
+    pop(): void {
+        const val = this.stack1.pop() as number;
+        if(this.stack2[this.stack2.length-1] === val){
+            this.stack2.pop()
+        }
+    }
+
+    top(): number {
+        return this.stack1[this.stack1.length-1];
+    }
+
+    getMin(): number {
+        return this.stack2[this.stack2.length-1];
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
