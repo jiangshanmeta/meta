e76ea3dbd6c9195ddbd0a0f0ba6884d3f4c99b6a
jiangshanmeta <540118044@qq.com>
2020-12-01 19:49:11 +0800

    2

---
+++ typescript/面试题02_02.kth-node-from-end-of-list-lcci.1000007.ts
@@ -0,0 +1,21 @@
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+function kthToLast(head: ListNode | null, k: number): number {
+    let fast:ListNode|null = head as ListNode;
+    while(k--){
+        fast = (fast as ListNode).next;
+    }
+    let slow:ListNode|null = head;
+    while(fast !== null){
+        fast = fast.next;
+        slow = (slow as ListNode).next;
+    }
+    return (slow as ListNode).val;
+};
