043dac86fa9772f6e4922dd852bbff64e63084ff
jiangshanmeta <540118044@qq.com>
2020-12-04 19:10:30 +0800

    17

---
+++ typescript/面试题17_12.binode-lcci.1000019.ts
@@ -0,0 +1,28 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function convertBiNode(root: TreeNode | null): TreeNode | null {
+    if(root === null){
+        return null;
+    }
+    root.right = convertBiNode(root.right);
+    if(root.left === null){
+        return root;
+    }
+    const newRoot = <TreeNode>convertBiNode(root.left);
+    root.left = null;
+    let node = newRoot;
+    while(node.right !== null){
+        node = node.right;
+    }
+    node.right = root;
+    return newRoot;
+};
