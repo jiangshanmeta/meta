3ca9560abd722f7fe01b96f54444266343870437
jiangshanmeta <540118044@qq.com>
2020-12-04 19:10:02 +0800

    8

---
+++ typescript/面试题08_08.permutation-ii-lcci.100348.ts
@@ -0,0 +1,24 @@
+function permutation(S: string): string[] {
+    const sList = S.split('').sort();
+    const used:boolean[] = new Array(sList.length).fill(false);
+    const result:string[] = [];
+    backTracking(sList,used,[] as string[],result);
+    return result;
+};
+
+function backTracking(sList:string[],used:boolean[],sequence:string[],result:string[]){
+    if(sequence.length === sList.length){
+        result.push(sequence.join(''));
+        return;
+    }
+    for(let i=0;i<sList.length;i++){
+        if(used[i] || (i>0 && sList[i-1] === sList[i] && !used[i-1])){
+            continue;
+        }
+        used[i] = true;
+        sequence.push(sList[i]);
+        backTracking(sList,used,sequence,result);
+        sequence.pop();
+        used[i] = false;
+    }
+}
