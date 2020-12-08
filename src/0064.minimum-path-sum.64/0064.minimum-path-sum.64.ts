b65e5009493ee53499f12307e3effb9253ed8652
jiangshanmeta <540118044@qq.com>
2020-12-08 19:17:59 +0800

    64

---
+++ src/0064.minimum-path-sum.64/0064.minimum-path-sum.64.ts
@@ -0,0 +1,19 @@
+function minPathSum(grid: number[][]): number {
+    const M = grid.length;
+    if(M === 0){
+        return 0;
+    }
+    const N = grid[0].length;
+    const dp:number[] = new Array(N);
+    dp[0] = grid[0][0];
+    for(let j=1;j<N;j++){
+        dp[j] = dp[j-1]+grid[0][j];
+    }
+    for(let i=1;i<M;i++){
+        dp[0] += grid[i][0];
+        for(let j=1;j<N;j++){
+            dp[j] = Math.min(dp[j-1],dp[j])+grid[i][j];
+        }
+    }
+    return dp[N-1];
+};
