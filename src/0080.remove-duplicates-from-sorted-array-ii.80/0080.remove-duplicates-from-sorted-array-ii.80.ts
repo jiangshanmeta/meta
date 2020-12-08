c4329fe68b2fca2d6965701aa470f76403cfac7d
jiangshanmeta <540118044@qq.com>
2020-12-08 19:18:04 +0800

    80

---
+++ src/0080.remove-duplicates-from-sorted-array-ii.80/0080.remove-duplicates-from-sorted-array-ii.80.ts
@@ -0,0 +1,24 @@
+function removeDuplicates(nums: number[]): number {
+    if(nums.length<3){
+        return nums.length;
+    }
+
+    let prev = nums[0];
+    let count = 1;
+    let index = 1;
+    for(let i=1;i<nums.length;i++){
+        if(nums[i] !== prev){
+            count = 1;
+            prev = nums[i];
+            nums[index++] = nums[i];
+        }else{
+            if(count === 2){
+                continue;
+            }
+            nums[index++] = nums[i];
+            count++;
+        }
+    }
+
+    return index;
+};
