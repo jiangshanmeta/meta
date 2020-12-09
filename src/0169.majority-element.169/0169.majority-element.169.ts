b69e98f4a97660eea8fd2dee9d16123daea97585
jiangshanmeta <540118044@qq.com>
2020-12-09 19:15:42 +0800

    169

---
+++ src/0169.majority-element.169/0169.majority-element.169.ts
@@ -0,0 +1,15 @@
+function majorityElement(nums: number[]): number {
+    let result = nums[0];
+    let count = 1;
+    for(let i=1;i<nums.length;i++){
+        if(nums[i] === result){
+            count++;
+        }else if(count>0){
+            count--;
+        }else{
+            count = 1;
+            result = nums[i]
+        }
+    }
+    return result;
+};
