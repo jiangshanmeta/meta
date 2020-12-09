328b1916860ab2c782b562ffd9ffd3e7a9fd9eec
jiangshanmeta <540118044@qq.com>
2020-12-09 19:15:32 +0800

    119

---
+++ src/0119.pascals-triangle-ii.119/0119.pascals-triangle-ii.119.ts
@@ -0,0 +1,9 @@
+function getRow(rowIndex: number): number[] {
+    const result:number[] = new Array(rowIndex+1).fill(1);
+    for(let i=2;i<rowIndex+1;i++){
+        for(let j=i-1;j>0;j--){
+            result[j] += result[j-1];
+        }
+    }
+    return result;
+};
