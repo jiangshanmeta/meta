2210e592ab9f09813934ca66624475b421a989df
jiangshanmeta <540118044@qq.com>
2020-12-10 19:03:02 +0800

    345

---
+++ src/0345.reverse-vowels-of-a-string.345/0345.reverse-vowels-of-a-string.345.ts
@@ -0,0 +1,35 @@
+function reverseVowels(s: string): string {
+    const list = s.split('');
+    let left = 0;
+    let right = s.length-1;
+    while(left<right){
+        while(left<right && !isVowel(list[left])){
+            left++;
+        }
+        while(left<right && !isVowel(list[right])){
+            right--;
+        }
+        if(left<right){
+            const tmp = list[left];
+            list[left++] = list[right];
+            list[right--] = tmp;
+        }
+    }
+    return list.join('');
+};
+const set = new Set<string>([
+    'a',
+    'e',
+    'i',
+    'o',
+    'u',
+    'A',
+    'E',
+    'I',
+    'O',
+    'U'
+])
+
+function isVowel(c:string){
+    return set.has(c);
+}
