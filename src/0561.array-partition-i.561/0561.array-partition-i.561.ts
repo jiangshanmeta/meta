02a95ff366f7c61cab9cc7ae9a43d503eb6c4c9a
jiangshanmeta <540118044@qq.com>
2020-12-14 20:17:37 +0800

    561

---
+++ src/0561.array-partition-i.561/0561.array-partition-i.561.ts
@@ -0,0 +1,8 @@
+function arrayPairSum(nums: number[]): number {
+    nums.sort((a,b)=>a-b);
+    let result = 0;
+    for(let i=0;i<nums.length;i+=2){
+        result += nums[i];
+    }
+    return result
+};
