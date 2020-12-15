47ae6932654bc4c9967d212027c064bce45dcff1
jiangshanmeta <540118044@qq.com>
2020-12-15 19:48:52 +0800

    746

---
+++ src/0746.min-cost-climbing-stairs.747/0746.min-cost-climbing-stairs.747.ts
@@ -0,0 +1,9 @@
+function minCostClimbingStairs(cost: number[]): number {
+    const dp:number[] = new Array(cost.length);
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+    for(let i=2;i<cost.length;i++){
+        dp[i] = Math.min(dp[i-1],dp[i-2])+cost[i];
+    }
+    return Math.min(dp[dp.length-1],dp[dp.length-2]);
+};
