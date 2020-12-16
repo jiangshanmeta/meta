2d422b8464a9570b21aec0ff9ed040169d26a8dd
jiangshanmeta <540118044@qq.com>
2020-12-16 19:04:13 +0800

    941

---
+++ src/0941.valid-mountain-array.978/0941.valid-mountain-array.978.ts
@@ -0,0 +1,25 @@
+function validMountainArray(A: number[]): boolean {
+    if(A.length<3){
+        return false;
+    }
+    let hasDecreasing = false;
+    for(let i=1;i<A.length;i++){
+        if(A[i]>A[i-1]){
+            continue;
+        }
+        if(A[i] === A[i-1]){
+            return false;
+        }
+        if(i === 1){
+            return false;
+        }
+        hasDecreasing = true;
+        for(let j=i+1;j<A.length;j++){
+            if(A[j]>=A[j-1]){
+                return false;
+            }
+        }
+        break;
+    }
+    return hasDecreasing;
+};
