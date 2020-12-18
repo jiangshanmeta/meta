077173f15641d910e6c520982a5019dd20869ca6
jiangshanmeta <540118044@qq.com>
2020-12-18 18:54:17 +0800

    901

---
+++ src/0901.online-stock-span.937/0901.online-stock-span.937.js
@@ -0,0 +1,26 @@
+
+var StockSpanner = function() {
+    this.priceStack = [];
+    this.countStack = [];
+};
+
+/** 
+ * @param {number} price
+ * @return {number}
+ */
+StockSpanner.prototype.next = function(price) {
+    let count = 1;
+    while(this.priceStack.length && this.priceStack[this.priceStack.length-1]<=price){
+        count += this.countStack.pop();
+        this.priceStack.pop();
+    }
+    this.priceStack.push(price);
+    this.countStack.push(count);
+    return count;
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * var obj = new StockSpanner()
+ * var param_1 = obj.next(price)
+ */
