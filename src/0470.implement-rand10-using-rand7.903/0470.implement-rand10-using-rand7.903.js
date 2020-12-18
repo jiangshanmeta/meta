48b165883486f8632bbe3e42af4c72a839f45e3a
jiangshanmeta <540118044@qq.com>
2020-12-18 18:54:12 +0800

    470

---
+++ src/0470.implement-rand10-using-rand7.903/0470.implement-rand10-using-rand7.903.js
@@ -0,0 +1,14 @@
+/**
+ * The rand7() API is already defined for you.
+ * var rand7 = function() {}
+ * @return {number} a random integer in the range 1 to 7
+ */
+var rand10 = function() {
+    let row, col, idx;
+    do {
+        row = rand7();
+        col = rand7();
+        idx = col + (row - 1) * 7;
+    } while (idx > 40);
+    return 1 + (idx - 1) % 10;
+};
