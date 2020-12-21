b3efa944c8249536d80635c209583479f4620193
jiangshanmeta <540118044@qq.com>
2020-12-21 18:54:58 +0800

    1380

---
+++ src/1380.lucky-numbers-in-a-matrix.1496/1380.lucky-numbers-in-a-matrix.1496.ts
@@ -0,0 +1,21 @@
+function luckyNumbers (matrix: number[][]): number[] {
+    const M = matrix.length;
+    const N = matrix[0].length;
+    const rowMins = new Array(M).fill(Infinity);
+    const columnMax = new Array(N).fill(-Infinity);
+    for(let i=0;i<M;i++){
+        for(let j=0;j<N;j++){
+            rowMins[i] = Math.min(rowMins[i],matrix[i][j]);
+            columnMax[j] = Math.max(columnMax[j],matrix[i][j]);
+        }
+    }
+    const result:number[] = [];
+    for(let i=0;i<M;i++){
+        for(let j=0;j<N;j++){
+            if(matrix[i][j] === rowMins[i] && matrix[i][j] === columnMax[j]){
+                result.push(matrix[i][j])
+            }
+        }
+    }
+    return result;
+};
