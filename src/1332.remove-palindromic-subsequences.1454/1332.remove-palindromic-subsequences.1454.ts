1386addb198b84abb94a577dbd5eeba64a80fa53
jiangshanmeta <540118044@qq.com>
2020-12-21 18:54:47 +0800

    1332

---
+++ src/1332.remove-palindromic-subsequences.1454/1332.remove-palindromic-subsequences.1454.ts
@@ -0,0 +1,36 @@
+function removePalindromeSub(s: string): number {
+    if(s.length === 0){
+        return 0;
+    }
+    let hasA = false;
+    let hasB = false;
+    let left = 0;
+    let right = s.length-1;
+    let isPalindrome = true;
+    while(left<=right){
+        if(s[left] === 'a'){
+            hasA = true;
+        }else{
+            hasB = true
+        }
+        if(s[right] === 'a'){
+            hasA = true;
+        }else{
+            hasB = true;
+        }
+        if(s[left++] !== s[right--]){
+            isPalindrome = false;
+        }
+    }
+
+    if(isPalindrome){
+        return 1;
+    }
+
+    if(hasA && hasB){
+        return 2;
+    }else{
+        return 1;
+    }
+
+};
