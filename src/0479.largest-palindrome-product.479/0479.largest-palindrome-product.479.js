747ba0d9d9ef07307b7076e601a8d18fe9374627
jiangshanmeta <540118044@qq.com>
2020-12-22 19:16:42 +0800

    479

---
+++ src/0479.largest-palindrome-product.479/0479.largest-palindrome-product.479.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var largestPalindrome = function(n) {
+    switch(n){
+        case 1:
+            return 9;
+        case 2:
+            return 987;
+        case 3:
+            return 123;
+        case 4:
+            return 597;
+        case 5:
+            return 677;
+        case 6:
+            return 1218;
+        case 7:
+            return 877;
+        case 8:
+            return 475;
+        default:
+            return 0;
+    }
+};
