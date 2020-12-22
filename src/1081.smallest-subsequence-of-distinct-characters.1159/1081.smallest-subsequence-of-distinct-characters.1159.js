f039a51a60b02caf8acda04e29bc41d2ef2f36e3
jiangshanmeta <540118044@qq.com>
2020-12-22 19:16:48 +0800

    1081

---
+++ src/1081.smallest-subsequence-of-distinct-characters.1159/1081.smallest-subsequence-of-distinct-characters.1159.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var smallestSubsequence = function(s) {
+    const counts = new Array(26).fill(0);
+    for(let i=0;i<s.length;i++){
+        counts[s.charCodeAt(i)-97]++;
+    }
+    
+    const used = new Array(26).fill(false);
+    const result = [];
+    for(let i=0;i<s.length;i++){
+        const code = s.charCodeAt(i)-97;
+        if(!used[code]){
+            while(result.length && result[result.length-1]>s[i]){
+                const prevCode = result[result.length-1].charCodeAt(0)-97;
+                if(counts[prevCode]>0){
+                    used[prevCode] = false;
+                    result.pop();
+                }else{
+                    break;
+                }
+            }
+            result.push(s[i]);
+            used[code] = true;
+        }
+        counts[code]--;
+    }
+    return result.join('')
+};
