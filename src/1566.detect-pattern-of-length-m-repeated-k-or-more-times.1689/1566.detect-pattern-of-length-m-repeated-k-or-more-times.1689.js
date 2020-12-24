47d714aa461f7c62d3485391f9c10d6319380dd3
jiangshanmeta <540118044@qq.com>
2020-12-24 18:52:58 +0800

    1566

---
+++ src/1566.detect-pattern-of-length-m-repeated-k-or-more-times.1689/1566.detect-pattern-of-length-m-repeated-k-or-more-times.1689.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} arr
+ * @param {number} m
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsPattern = function(arr, m, k) {
+    for(let i=0;i<=arr.length-m*k;i++){
+        if(check(arr,m,k,i)){
+            return true;
+        }
+    }
+    return false
+};
+
+function check(arr,m,k,start){
+    for(let i=1;i<k;i++){
+        for(let j=0;j<m;j++){
+            if(arr[start+j] !== arr[start+i*m+j]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
