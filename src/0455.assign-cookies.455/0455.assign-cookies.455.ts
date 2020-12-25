11a7861a73a1583c553238a6a9b89e0e77782ba0
jiangshanmeta <540118044@qq.com>
2020-12-25 18:10:18 +0800

    455

---
+++ src/0455.assign-cookies.455/0455.assign-cookies.455.ts
@@ -0,0 +1,17 @@
+function findContentChildren(g: number[], s: number[]): number {
+    g.sort((a,b)=>a-b);
+    s.sort((a,b)=>a-b);
+    let indexG = 0;
+    let indexS = 0;
+    let result = 0;
+    while(indexG<g.length && indexS<s.length){
+        if(s[indexS]>=g[indexG]){
+            result++;
+            indexS++;
+            indexG++;
+        }else{
+            indexS++;
+        }
+    }
+    return result
+};
