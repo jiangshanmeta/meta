350ec4d919f90418b61f034831debc418b410a8d
jiangshanmeta <540118044@qq.com>
2020-12-25 18:10:23 +0800

    1566

---
+++ src/1566.detect-pattern-of-length-m-repeated-k-or-more-times.1689/1566.detect-pattern-of-length-m-repeated-k-or-more-times.1689.ts
@@ -0,0 +1,19 @@
+function containsPattern(arr: number[], m: number, k: number): boolean {
+    for(let i=0;i<=arr.length-m*k;i++){
+        if(check(arr,m,k,i)){
+            return true;
+        }
+    }
+    return false
+};
+
+function check(arr:number[],m:number,k,start:number):boolean{
+    for(let i=1;i<k;i++){
+        for(let j=0;j<m;j++){
+            if(arr[start+j] !== arr[start+i*m+j]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
