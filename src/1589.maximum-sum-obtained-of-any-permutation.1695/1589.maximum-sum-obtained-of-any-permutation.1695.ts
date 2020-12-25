95b0a8c2d477f4b70b34f2427e78453cb247ed93
jiangshanmeta <540118044@qq.com>
2020-12-25 18:10:32 +0800

    1589

---
+++ src/1589.maximum-sum-obtained-of-any-permutation.1695/1589.maximum-sum-obtained-of-any-permutation.1695.ts
@@ -0,0 +1,23 @@
+
+function maxSumRangeQuery(nums: number[], requests: number[][]): number {
+    const counts:number[] = new Array(nums.length).fill(0);
+    for(let i=0;i<requests.length;i++){
+        const [start,end] = requests[i];
+        counts[start]++;
+        if(end+1<nums.length){
+            counts[end+1]--;
+        }
+    }
+    for(let i=1;i<counts.length;i++){
+        counts[i] += counts[i-1];
+    }
+
+    counts.sort((a,b)=>b-a);
+    nums.sort((a,b)=>b-a);
+    let result = 0;
+    const mod = 10**9+7;
+    for(let i=0;i<nums.length;i++){
+        result = (result+counts[i]*nums[i])%mod;
+    }
+    return result;
+};
