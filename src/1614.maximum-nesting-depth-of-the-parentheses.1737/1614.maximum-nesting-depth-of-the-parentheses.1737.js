fe6b84d109de434527a9e7bfb5509ecaeb785989
jiangshanmeta <540118044@qq.com>
2020-12-28 18:34:58 +0800

    1614

---
+++ src/1614.maximum-nesting-depth-of-the-parentheses.1737/1614.maximum-nesting-depth-of-the-parentheses.1737.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function(s) {
+    let result = 0;
+    let current = 0;
+    for(let i=0;i<s.length;i++){
+        if(s[i] === '('){
+            current++;
+            result = Math.max(current,result);
+        }else if(s[i] === ')'){
+            current--;
+        }
+    }
+    return result
+};
