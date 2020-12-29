c94699adf960fdd599f87532eea4f8f2cb2bd7cc
jiangshanmeta <540118044@qq.com>
2020-12-29 19:12:23 +0800

    1636

---
+++ src/1636.sort-array-by-increasing-frequency.1741/1636.sort-array-by-increasing-frequency.1741.ts
@@ -0,0 +1,27 @@
+function frequencySort(nums: number[]): number[] {
+    const map = new Map<number,number>();
+    for(let i=0;i<nums.length;i++){
+        if(map.has(nums[i])){
+            map.set(nums[i], map.get(nums[i])! + 1 )
+        }else{
+            map.set(nums[i],1);
+        }
+    }
+    return [
+        ...map.entries()
+    ].sort(([aNum,aCount],[bNum,bCount])=>{
+        if(aCount !== bCount){
+            return aCount-bCount;
+        }else{
+            return bNum-aNum;
+        }
+    }).reduce((arr,[num,count])=>{
+        for(let i=0;i<count;i++){
+            arr.push(num)
+        }
+        return arr;
+    },[] as number[])
+    
+
+
+};
