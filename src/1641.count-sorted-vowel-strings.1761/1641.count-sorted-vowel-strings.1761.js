d84a4e37a25bd8f1d7ea918442e8bc931912b69c
jiangshanmeta <540118044@qq.com>
2020-12-30 18:33:35 +0800

    1641

---
+++ src/1641.count-sorted-vowel-strings.1761/1641.count-sorted-vowel-strings.1761.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countVowelStrings = function(n) {
+    const dp = [1,1,1,1,1];
+    for(let i=2;i<=n;i++){
+        for(let j=4;j>0;j--){
+            for(let k=0;k<j;k++){
+                dp[j] += dp[k]
+            }
+        }
+    }
+    return dp.reduce((sum,item)=>{
+        return sum+item;
+    },0);
+};
