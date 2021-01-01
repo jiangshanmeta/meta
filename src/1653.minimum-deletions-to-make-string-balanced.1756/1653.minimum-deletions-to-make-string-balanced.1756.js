195bc7d00b5c77b5889b3b9400c971437b91bb11
jiangshanmeta <540118044@qq.com>
2021-01-01 10:23:42 +0800

    1653

---
+++ src/1653.minimum-deletions-to-make-string-balanced.1756/1653.minimum-deletions-to-make-string-balanced.1756.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minimumDeletions = function(s) {
+    let lastA = 0;
+    let lastB = 0;
+    for(let i=0;i<s.length;i++){
+        let nextA = Infinity;
+        let nextB = Infinity;
+        if(s[i] === 'a'){
+            nextA = lastA;
+            nextB = lastB+1;
+        }else{
+            nextA = lastA+1;
+            nextB = Math.min(lastA,lastB);
+        }
+        lastA = nextA;
+        lastB = nextB;
+    }
+    return Math.min(lastA,lastB)
+};
