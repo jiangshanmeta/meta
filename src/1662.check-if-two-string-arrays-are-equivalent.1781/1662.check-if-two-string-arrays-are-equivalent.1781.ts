9787d6880a3943b4a27ddcbb02d1c76ed24fd641
jiangshanmeta <540118044@qq.com>
2021-01-02 10:24:09 +0800

    1662

---
+++ src/1662.check-if-two-string-arrays-are-equivalent.1781/1662.check-if-two-string-arrays-are-equivalent.1781.ts
@@ -0,0 +1,21 @@
+function arrayStringsAreEqual(word1: string[], word2: string[]): boolean {
+    let i1 = 0;
+    let j1 = 0;
+    let i2 = 0;
+    let j2 = 0;
+    while(i1<word1.length && i2<word2.length){
+        if(word1[i1][j1++] !== word2[i2][j2++]){
+            return false;
+        }
+        if(j1 === word1[i1].length){
+            i1++;
+            j1 = 0;
+        }
+        if(j2 === word2[i2].length){
+            i2++;
+            j2 = 0;
+        }
+    }
+
+    return i1 === word1.length && i2 === word2.length;
+};
