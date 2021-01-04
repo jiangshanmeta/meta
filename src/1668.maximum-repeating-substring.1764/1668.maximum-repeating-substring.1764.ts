ce60680e02fafd79134e021625baf7f00add9078
jiangshanmeta <540118044@qq.com>
2021-01-04 21:43:10 +0800

    1668

---
+++ src/1668.maximum-repeating-substring.1764/1668.maximum-repeating-substring.1764.ts
@@ -0,0 +1,12 @@
+function maxRepeating(sequence: string, word: string): number {
+    let result = 0;
+    let flag = true;
+    while(flag){
+        if(sequence.includes(word.repeat(result+1))){
+            result++;
+        }else{
+            flag = false;
+        }
+    }
+    return result
+};
