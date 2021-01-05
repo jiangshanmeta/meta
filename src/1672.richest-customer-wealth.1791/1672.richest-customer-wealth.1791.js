6968d2f199b71c388afb00e18c70d409635b672b
jiangshanmeta <540118044@qq.com>
2021-01-05 19:36:56 +0800

    1672

---
+++ src/1672.richest-customer-wealth.1791/1672.richest-customer-wealth.1791.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    let result = 0;
+    for(let i=0;i<accounts.length;i++){
+        let sum = 0;
+        for(let j=0;j<accounts[i].length;j++){
+            sum += accounts[i][j];
+        }
+        result = Math.max(result,sum);
+    }
+    return result;
+};
