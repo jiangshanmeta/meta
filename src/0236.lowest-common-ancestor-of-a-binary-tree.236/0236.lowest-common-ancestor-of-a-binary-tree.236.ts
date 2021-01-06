c2fe88381d5deee6a8d989b3a5d77f601448abc3
jiangshanmeta <540118044@qq.com>
2021-01-06 21:21:21 +0800

    236

---
+++ src/0236.lowest-common-ancestor-of-a-binary-tree.236/0236.lowest-common-ancestor-of-a-binary-tree.236.ts
@@ -0,0 +1,22 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+         this.val = (val===undefined ? 0 : val)
+         this.left = (left===undefined ? null : left)
+         this.right = (right===undefined ? null : right)
+    }
+}
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+	if(root === null || root === p || root === q){
+        return root;
+    }
+    const left = lowestCommonAncestor(root.left,p,q);
+    const right = lowestCommonAncestor(root.right,p,q);
+    if(left && right){
+        return root;
+    }
+    return left || right;
+};
