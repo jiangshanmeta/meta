cfb2d14e52abb87c6972ecffa9c13f2b15590044
jiangshanmeta <540118044@qq.com>
2021-01-07 21:13:51 +0800

    232

---
+++ src/0232.implement-queue-using-stacks.232/0232.implement-queue-using-stacks.232.ts
@@ -0,0 +1,44 @@
+class MyQueue {
+    private stack1:number[];
+    private stack2:number[];
+    constructor() {
+        this.stack1 = [];
+        this.stack2 = [];
+    }
+
+    push(x: number): void {
+        this.stack1.push(x);
+    }
+
+    adjust():void{
+        if(this.stack2.length !== 0){
+            return;
+        }
+        while(this.stack1.length){
+            this.stack2.push(this.stack1.pop()!);
+        }
+    }
+
+    pop(): number {
+        this.adjust();
+        return this.stack2.pop()!;
+    }
+
+    peek(): number {
+        this.adjust();
+        return this.stack2[this.stack2.length-1];
+    }
+
+    empty(): boolean {
+        return this.stack1.length === 0 && this.stack2.length === 0;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
