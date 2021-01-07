17d3d7b1efd68ef9c2a802e4b4022884f51f47d0
jiangshanmeta <540118044@qq.com>
2021-01-07 21:13:45 +0800

    228

---
+++ src/0228.summary-ranges.228/0228.summary-ranges.228.ts
@@ -0,0 +1,18 @@
+function summaryRanges(nums: number[]): string[] {
+    const result:string[] = [];
+    let start = 0;
+    let end = 0;
+    while(end<nums.length){
+        end++;
+        while(end<nums.length && nums[end] === nums[end-1]+1){
+            end++;
+        }
+        if(end-start === 1){
+            result.push(String(nums[start]));
+        }else{
+            result.push(`${nums[start]}->${nums[end-1]}`);
+        }
+        start = end;
+    }
+    return result
+};
