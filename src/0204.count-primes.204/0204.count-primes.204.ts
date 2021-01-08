207d157cf2dfdbb9e27fbebd441e914d3c889af5
jiangshanmeta <540118044@qq.com>
2021-01-08 21:38:43 +0800

    204

---
+++ src/0204.count-primes.204/0204.count-primes.204.ts
@@ -0,0 +1,17 @@
+function countPrimes(n: number): number {
+    const isPrimes:boolean[] = new Array(n).fill(true);
+    let result = 0;
+    for(let i=2;i<n;i++){
+        if(!isPrimes[i]){
+            continue;
+        }
+        result++;
+        let num = i*2;
+        while(num<n){
+            isPrimes[num] = false;
+            num += i;
+        }
+    }
+
+    return result
+};
