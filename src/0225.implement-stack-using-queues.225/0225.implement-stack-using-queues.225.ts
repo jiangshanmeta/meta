5f4b67aa7cbb952958bc32468cd6b8c416c82212
jiangshanmeta <540118044@qq.com>
2021-01-08 21:38:47 +0800

    225

---
+++ src/0225.implement-stack-using-queues.225/0225.implement-stack-using-queues.225.ts
@@ -0,0 +1,36 @@
+class MyStack {
+    private queue:number[];
+    constructor() {
+        this.queue = [];
+    }
+
+    push(x: number): void {
+        let count = this.queue.length;
+        this.queue.push(x);
+        while(count){
+            this.queue.push(this.queue.shift());
+            count--;
+        }
+    }
+
+    pop(): number {
+        return this.queue.shift()
+    }
+
+    top(): number {
+        return this.queue[0];
+    }
+
+    empty(): boolean {
+        return this.queue.length === 0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
