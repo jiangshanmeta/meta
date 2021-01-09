9b2451e3a7196f9f4026e34c8b19ce09f167a2f6
jiangshanmeta <540118044@qq.com>
2021-01-09 19:28:32 +0800

    507

---
+++ src/0507.perfect-number.507/0507.perfect-number.507.ts
@@ -0,0 +1,17 @@
+function checkPerfectNumber(num: number): boolean {
+    if(num<2){
+        return false;
+    }
+    let sum = 1;
+    for(let i=2;i<=Math.sqrt(num);i++){
+        if(num%i !== 0){
+            continue;
+        }
+        const rest = num/i;
+        sum += i;
+        if(i !== rest){
+            sum += rest;
+        }
+    }
+    return sum === num
+};
