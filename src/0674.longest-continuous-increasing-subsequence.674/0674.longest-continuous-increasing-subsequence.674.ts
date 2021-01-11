6e8de689f4aedeb09024cc4ef59f4493bbfa49ad
jiangshanmeta <540118044@qq.com>
2021-01-11 20:37:45 +0800

    674

---
+++ src/0674.longest-continuous-increasing-subsequence.674/0674.longest-continuous-increasing-subsequence.674.ts
@@ -0,0 +1,14 @@
+function findLengthOfLCIS(nums: number[]): number {
+    let result = 0;
+    let index = 0;
+    while(index<nums.length){
+        let count = 1;
+        index++;
+        while(index<nums.length && nums[index]>nums[index-1]){
+            count++;
+            index++;
+        }
+        result = Math.max(result,count);
+    }
+    return result;
+};
