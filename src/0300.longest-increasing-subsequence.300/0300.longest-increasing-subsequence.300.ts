8f732e64934605f5191e7f3cb6d3f7135fa127bc
jiangshanmeta <540118044@qq.com>
2021-01-12 21:10:58 +0800

    300

---
+++ src/0300.longest-increasing-subsequence.300/0300.longest-increasing-subsequence.300.ts
@@ -0,0 +1,11 @@
+function lengthOfLIS(nums: number[]): number {
+    const result:number[] = new Array(nums.length).fill(1);
+    for(let i=1;i<nums.length;i++){
+        for(let j=0;j<i;j++){
+            if(nums[i]>nums[j]){
+                result[i] = Math.max(result[i],result[j]+1);
+            }
+        }
+    }
+    return Math.max(...result);
+};
