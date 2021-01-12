5cb04b78a849744451958b9b98ec90bad0082abd
jiangshanmeta <540118044@qq.com>
2021-01-12 21:11:02 +0800

    316

---
+++ src/0316.remove-duplicate-letters.316/0316.remove-duplicate-letters.316.ts
@@ -0,0 +1,22 @@
+function removeDuplicateLetters(s: string): string {
+    const counts:number[] = new Array(26).fill(0);
+    const visited:boolean[] = new Array(26).fill(false);
+    for(let i=0;i<s.length;i++){
+        counts[s.charCodeAt(i)-97]++;
+    }
+    const stack:string[] = [];
+    for(let i=0;i<s.length;i++){
+        const c = s[i];
+        const code = c.charCodeAt(0)-97;
+        if(!visited[code]){
+            while(stack.length && c<=stack[stack.length-1] && counts[stack[stack.length-1].charCodeAt(0)-97]){
+                const code2 = stack.pop().charCodeAt(0)-97;
+                visited[code2] = false;
+            }
+            visited[code] = true;
+            stack.push(c);
+        }
+        counts[code]--;
+    }
+    return stack.join('')
+};
