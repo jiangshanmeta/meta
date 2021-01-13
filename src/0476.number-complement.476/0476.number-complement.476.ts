2e9f27479a85641cdd9e2ce70571f5ba841a3272
jiangshanmeta <540118044@qq.com>
2021-01-13 21:18:54 +0800

    476

---
+++ src/0476.number-complement.476/0476.number-complement.476.ts
@@ -0,0 +1,21 @@
+function findComplement(num: number): number {
+    let count = getDigit(num);
+    let mask = 1;
+    while(count){
+        if(mask&num){
+            count--;
+        }
+        mask <<= 1;
+    }
+    return ((~num)>>>0)&(mask-1)
+
+};
+
+function getDigit(num:number):number{
+    let result = 0;
+    while(num){
+        result++;
+        num = num&(num-1);
+    }
+    return result
+}
