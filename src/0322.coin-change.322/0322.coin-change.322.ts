543717eee24a02afe8a0807cc0c5fafb113c8a25
jiangshanmeta <540118044@qq.com>
2021-01-13 21:18:38 +0800

    322

---
+++ src/0322.coin-change.322/0322.coin-change.322.ts
@@ -0,0 +1,13 @@
+function coinChange(coins: number[], amount: number): number {
+    const dp = new Array(amount+1).fill(Infinity);
+    dp[0] = 0;
+    for(let i=0;i<coins.length;i++){
+        for(let j=coins[i];j<dp.length;j++){
+            dp[j] = Math.min(dp[j],dp[j-coins[i]]+1)
+        }
+    }
+    if(dp[amount] === Infinity){
+        return -1;
+    }
+    return dp[amount];
+};
