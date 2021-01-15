6c11efefc81fe600c965cd8f82162857d31d2326
jiangshanmeta <540118044@qq.com>
2021-01-15 22:42:54 +0800

    482

---
+++ src/0482.license-key-formatting.482/0482.license-key-formatting.482.ts
@@ -0,0 +1,22 @@
+function licenseKeyFormatting(S: string, K: number): string {
+    const seq:string[] = [];
+    for(let i=0;i<S.length;i++){
+        if(S[i] === '-'){
+            continue;
+        }
+        seq.push(S[i]);
+    }
+    const result:string[] = [];
+    const mod = seq.length%K;
+    if(mod){
+        result.push(seq.slice(0,mod).join(''));
+    }
+    let index = mod;
+    while(index<seq.length){
+        result.push(seq.slice(index,index+K).join(''))
+        index += K;
+    }
+
+
+    return result.join('-').toUpperCase()
+};
