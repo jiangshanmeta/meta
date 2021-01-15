4afcaa495069a535a0f0de1d4f71e6e2fe6b6c30
jiangshanmeta <540118044@qq.com>
2021-01-15 22:43:15 +0800

    682

---
+++ src/0682.baseball-game.682/0682.baseball-game.682.ts
@@ -0,0 +1,17 @@
+function calPoints(ops: string[]): number {
+    const stack:number[] = [];
+    for(let i=0;i<ops.length;i++){
+        if(ops[i] === '+'){
+            stack.push(stack[stack.length-1]+stack[stack.length-2]);
+        }else if(ops[i] === 'D'){
+            stack.push(stack[stack.length-1]*2);
+        }else if(ops[i] === 'C'){
+            stack.pop();
+        }else{
+            stack.push(+ops[i])
+        }
+    }
+    return stack.reduce((sum,item)=>{
+        return sum+item;
+    },0)
+};
