3b75be52e3f7132fee609012d892b971b5c0697a
jiangshanmeta <540118044@qq.com>
2021-01-17 10:27:35 +0800

    784

---
+++ src/0784.letter-case-permutation.800/0784.letter-case-permutation.800.ts
@@ -0,0 +1,28 @@
+function letterCasePermutation(S: string): string[] {
+    const result:string[] = [];
+    backTracking(S,[] as string[],result);
+    return result
+};
+
+function backTracking(S:string,sequence:string[],result:string[]){
+    if(sequence.length === S.length){
+        result.push(sequence.join(''));
+        return;
+    }
+    const code = S.charCodeAt(sequence.length);
+    if(code>47 && code <58){
+        sequence.push(S[sequence.length]);
+        backTracking(S,sequence,result);
+        sequence.pop();
+    }else{
+        sequence.push(S[sequence.length].toLowerCase());
+        backTracking(S,sequence,result);
+        sequence.pop();
+
+        sequence.push(S[sequence.length].toUpperCase());
+        backTracking(S,sequence,result)
+        sequence.pop()
+    }
+
+
+}
