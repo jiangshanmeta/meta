fa759abdea8a529d96db20eedcf45af79d732c08
jiangshanmeta <540118044@qq.com>
2021-01-18 11:07:32 +0800

    868

---
+++ src/0868.binary-gap.899/0868.binary-gap.899.ts
@@ -0,0 +1,16 @@
+function binaryGap(n: number): number {
+    let lastIndex = -1;
+    let index = 0;
+    let result = 0;
+    while(n){
+        if(n&1){
+            if(lastIndex !== -1){
+                result = Math.max(result,index-lastIndex);
+            }
+            lastIndex = index;
+        }
+        index++;
+        n >>= 1;
+    }
+    return result
+};
