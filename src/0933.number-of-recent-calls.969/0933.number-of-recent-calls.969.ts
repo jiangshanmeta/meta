0b40b8196fb06c14b552e3950f319252fe85c15e
jiangshanmeta <540118044@qq.com>
2021-01-18 11:07:55 +0800

    933

---
+++ src/0933.number-of-recent-calls.969/0933.number-of-recent-calls.969.ts
@@ -0,0 +1,20 @@
+class RecentCounter {
+    private queen:number[];
+    constructor() {
+        this.queen = [];
+    }
+
+    ping(t: number): number {
+        this.queen.push(t);
+        while(this.queen[0]<t-3000){
+            this.queen.shift();
+        }
+        return this.queen.length;
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
