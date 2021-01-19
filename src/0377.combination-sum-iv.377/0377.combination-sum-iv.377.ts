990760002b61e241aa870d96c05b4d780fe0a0c7
jiangshanmeta <540118044@qq.com>
2021-01-19 21:31:36 +0800

    377

---
+++ src/0377.combination-sum-iv.377/0377.combination-sum-iv.377.ts
@@ -0,0 +1,14 @@
+function combinationSum4(nums: number[], target: number): number {
+    const dp:number[] = new Array(target+1).fill(0);
+    dp[0] = 1;
+    nums.sort((a,b)=>a-b);
+    for(let i=1;i<dp.length;i++){
+        for(let j=0;j<nums.length;j++){
+            if(i-nums[j]<0){
+                break;
+            }
+            dp[i] += dp[i-nums[j]];
+        }
+    }
+    return dp[dp.length-1]
+};
