0004ad5e8df6fc794777f5675d0faf5f05fc674f
jiangshanmeta <540118044@qq.com>
2021-01-20 00:01:06 +0800

    589

---
+++ src/0589.n-ary-tree-preorder-traversal.775/0589.n-ary-tree-preorder-traversal.775.ts
@@ -0,0 +1,26 @@
+class Node1 {
+    val: number
+    children: Node1[]
+    constructor(val?: number) {
+        this.val = (val===undefined ? 0 : val)
+        this.children = []
+    }
+}
+
+
+function preorder(root: Node1 | null): number[] {
+    if(!root){
+        return [];
+    }
+    const stack:Node1[] = [root];
+    const result:number[] = [];
+    while(stack.length){
+        const node = stack.pop()!;
+        result.push(node.val);
+        for(let i=node.children.length-1;i>-1;i--){
+            stack.push(node.children[i]);
+        }
+
+    }
+    return result
+};
