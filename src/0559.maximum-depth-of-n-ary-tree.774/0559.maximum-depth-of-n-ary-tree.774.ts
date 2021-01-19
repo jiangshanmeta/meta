8c547b2d95226a9c2743a794a858f3f1d88f666d
jiangshanmeta <540118044@qq.com>
2021-01-20 00:01:01 +0800

    559

--- src/0559.maximum-depth-of-n-ary-tree.774/0559.maximum-depth-of-n-ary-tree.774.ts
+++ src/0559.maximum-depth-of-n-ary-tree.774/0559.maximum-depth-of-n-ary-tree.774.ts
@@ -1,14 +1,9 @@
 class Node1 {
     val: number
-    left: Node1 | null
-    right: Node1 | null
-    next: Node1 | null
-    children:Node1[]
-    constructor(val?: number, left?: Node1, right?: Node1, next?: Node1) {
+    children: Node1[]
+    constructor(val?: number) {
         this.val = (val===undefined ? 0 : val)
-        this.left = (left===undefined ? null : left)
-        this.right = (right===undefined ? null : right)
-        this.next = (next===undefined ? null : next)
+        this.children = []
     }
 }
 
