333308e0ba7777dc4f860736bbdb7c1f73da86a2
jiangshanmeta <540118044@qq.com>
2021-01-20 00:01:17 +0800

    633

---
+++ src/0633.sum-of-square-numbers.633/0633.sum-of-square-numbers.633.ts
@@ -0,0 +1,15 @@
+function judgeSquareSum(c: number): boolean {
+    let low = 0;
+    let high = Math.floor(Math.sqrt(c));
+    while(low<=high){
+        const sum = low*low+high*high;
+        if(sum === c){
+            return true;
+        }else if(sum<c){
+            low++;
+        }else{
+            high--;
+        }
+    }
+    return false
+};
