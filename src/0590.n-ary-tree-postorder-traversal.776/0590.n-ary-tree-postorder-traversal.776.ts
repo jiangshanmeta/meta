e969e71d0f87dd14e5faa6498f7652a30baa3cf5
jiangshanmeta <540118044@qq.com>
2021-01-20 00:01:11 +0800

    590

---
+++ src/0590.n-ary-tree-postorder-traversal.776/0590.n-ary-tree-postorder-traversal.776.ts
@@ -0,0 +1,24 @@
+class Node1 {
+    val: number
+    children: Node1[]
+    constructor(val?: number) {
+        this.val = (val===undefined ? 0 : val)
+        this.children = []
+    }
+}
+
+function postorder(root: Node1 | null): number[] {
+    if(!root){
+        return [];
+    }
+    const result:number[] = [];
+    postOrder(root,result);
+    return result
+};
+
+function postOrder(root:Node1,result:number[]){
+    root.children.forEach((node)=>{
+        postOrder(node,result);
+    });
+    result.push(root.val)
+}
