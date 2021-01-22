580c8bec97c90d9d422a6b8101e254a88010241b
jiangshanmeta <540118044@qq.com>
2021-01-22 23:19:32 +0800

    371

---
+++ src/0371.sum-of-two-integers.371/0371.sum-of-two-integers.371.ts
@@ -0,0 +1,13 @@
+function getSum(a: number, b: number): number {
+    return helper(a>>>0,b>>>0)>>0;
+};
+
+function helper(a:number,b:number):number{
+    if(a === 0){
+        return b
+    }
+    if(b === 0){
+        return a;
+    }
+    return helper(a^b,(a&b)<<1);
+}
