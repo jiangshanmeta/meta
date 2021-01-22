3c08f58fd5d290a6c942604439e125529040ecfc
jiangshanmeta <540118044@qq.com>
2021-01-22 23:19:59 +0800

    849

---
+++ src/0849.maximize-distance-to-closest-person.879/0849.maximize-distance-to-closest-person.879.ts
@@ -0,0 +1,17 @@
+function maxDistToClosest(seats: number[]): number {
+    let index = -1;
+    let result = 0;
+    for(let i=0;i<seats.length;i++){
+        if(seats[i] === 0){
+            continue;
+        }
+        if(index === -1){
+            result = i;
+        }else{
+            result = Math.max(result,(i-index)>>1);
+        }
+        index = i;
+    }
+    result = Math.max(result,seats.length-index-1);
+    return result;
+};
