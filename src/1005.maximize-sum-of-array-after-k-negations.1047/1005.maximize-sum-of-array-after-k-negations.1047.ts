d6108b28c472e10a6aa45910c59088826706679c
jiangshanmeta <540118044@qq.com>
2021-01-25 22:34:15 +0800

    1005

---
+++ src/1005.maximize-sum-of-array-after-k-negations.1047/1005.maximize-sum-of-array-after-k-negations.1047.ts
@@ -0,0 +1,24 @@
+function largestSumAfterKNegations(A: number[], K: number): number {
+    A.sort((a,b)=>a-b);
+    let index = 0;
+    while(index<A.length && K>0 && A[index]<0){
+        A[index] = -A[index];
+        index++;
+        K--;
+    }
+    K %= 2;
+    if(K === 1){
+        let i = index;
+        let minval = A[i];
+        if(index+1<A.length && A[index+1]<minval){
+            i = index+1;
+        }
+        if(index-1>-1 && A[index-1]<minval){
+            i = index-1;
+        }
+        A[i] = -A[i];
+    }
+    return A.reduce((sum,item)=>{
+        return sum+item;
+    },0);
+};
