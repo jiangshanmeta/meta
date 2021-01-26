796deaad6879b50f1e6d2a50b9c92b5c26938417
jiangshanmeta <540118044@qq.com>
2021-01-26 20:44:49 +0800

    1038

---
+++ src/1038.binary-search-tree-to-greater-sum-tree.1114/1038.binary-search-tree-to-greater-sum-tree.1114.ts
@@ -0,0 +1,25 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+         this.val = (val===undefined ? 0 : val)
+         this.left = (left===undefined ? null : left)
+         this.right = (right===undefined ? null : right)
+    }
+}
+
+function bstToGst(root: TreeNode | null): TreeNode | null {
+    if(root === null){
+        return null;
+    }
+    let greaterSum = 0;
+    function traversal(root:TreeNode){
+        root.right !== null && traversal(root.right);
+        root.val += greaterSum;
+        greaterSum = root.val;
+        root.left !== null && traversal(root.left);
+    }
+    traversal(root);
+    return root;
+};
