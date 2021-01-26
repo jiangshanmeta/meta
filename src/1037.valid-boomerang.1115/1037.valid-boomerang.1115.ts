ea1051686222d5c5a3b89c5ed88b0bac8653b35a
jiangshanmeta <540118044@qq.com>
2021-01-26 20:44:43 +0800

    1037

---
+++ src/1037.valid-boomerang.1115/1037.valid-boomerang.1115.ts
@@ -0,0 +1,15 @@
+function isBoomerang(points: number[][]): boolean {
+    for(let i=0;i<points.length-1;i++){
+        for(let j=i+1;j<points.length;j++){
+            if(points[i][0] === points[j][0] && points[i][1] === points[j][1]){
+                return false;
+            }
+        }
+    }
+    if(points[0][0] === points[1][0]){
+        return points[2][0] !== points[0][0];
+    }
+    const k = (points[1][1]-points[0][1])/(points[1][0]-points[0][0]);
+    const b = points[0][1]-k*points[0][0];
+    return points[2][1] !== k*points[2][0]+b;
+};
