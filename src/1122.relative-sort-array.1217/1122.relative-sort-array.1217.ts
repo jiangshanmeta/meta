149416984725ff3c55667d2f7a0ccf7471311bec
jiangshanmeta <540118044@qq.com>
2021-01-26 20:45:05 +0800

    1122

---
+++ src/1122.relative-sort-array.1217/1122.relative-sort-array.1217.ts
@@ -0,0 +1,24 @@
+function relativeSortArray(arr1: number[], arr2: number[]): number[] {
+    const map = new Map<number,number>();
+    for(let i=0;i<arr2.length;i++){
+        map.set(arr2[i],0);
+    }
+    const result:number[] = [];
+    const rest:number[] = [];
+    for(let i=0;i<arr1.length;i++){
+        if(map.has(arr1[i])){
+            map.set(arr1[i],map.get(arr1[i])!+1 );
+        }else{
+            rest.push(arr1[i]);
+        }
+    }
+    for(let i=0;i<arr2.length;i++){
+        const count = map.get(arr2[i])!;
+        for(let j=0;j<count;j++){
+            result.push(arr2[i]);
+        }
+    }
+    rest.sort((a,b)=>a-b);
+    result.push(...rest)
+    return result;
+};
