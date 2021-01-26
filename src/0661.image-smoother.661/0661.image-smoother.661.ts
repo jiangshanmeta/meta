51bb8e268a000cb42a61ec69e74a977b1b70eae8
jiangshanmeta <540118044@qq.com>
2021-01-27 00:10:25 +0800

    661

---
+++ src/0661.image-smoother.661/0661.image-smoother.661.ts
@@ -0,0 +1,29 @@
+function imageSmoother(M: number[][]): number[][] {
+    const R = M.length;
+    const C = M[0].length;
+    const result:number[][] = new Array(R);
+    for(let i=0;i<R;i++){
+        result[i] = new Array(C);
+        for(let j=0;j<C;j++){
+            let sum = 0;
+            let count = 0;
+            for(let dx=-1;dx<2;dx++){
+                const x = i+dx;
+                if(x === -1 || x === R){
+                    continue;
+                }
+                for(let dy=-1;dy<2;dy++){
+                    const y = j+dy;
+                    if(y === -1 || y === C){
+                        continue;
+                    }
+                    sum += M[x][y];
+                    count++;
+                }
+            }
+            result[i][j] = Math.floor(sum/count)
+        }
+    }
+
+    return result;
+};
