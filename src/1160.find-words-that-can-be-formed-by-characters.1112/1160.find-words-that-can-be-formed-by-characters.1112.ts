e1b8c72c6b53a4a67690adef577c33d90c4fa300
jiangshanmeta <540118044@qq.com>
2021-01-28 00:01:55 +0800

    1160

---
+++ src/1160.find-words-that-can-be-formed-by-characters.1112/1160.find-words-that-can-be-formed-by-characters.1112.ts
@@ -0,0 +1,20 @@
+function countCharacters(words: string[], chars: string): number {
+    const counts1 = getCount(chars);
+    return words.reduce((sum,word)=>{
+        const counts2 = getCount(word);
+        for(let i=0;i<26;i++){
+            if(counts2[i]>counts1[i]){
+                return sum;
+            }
+        }
+        return sum+word.length
+    },0);
+};
+
+function getCount(s:string):number[]{
+    const result:number[] = new Array(26).fill(0);
+    for(let i=0;i<s.length;i++){
+        result[s.charCodeAt(i)-97]++;
+    }
+    return result;
+}
