dd32e5e0715711bb383af58a289822938399059c
jiangshanmeta <540118044@qq.com>
2021-01-28 00:01:41 +0800

    1123

---
+++ src/1123.lowest-common-ancestor-of-deepest-leaves.1218/1123.lowest-common-ancestor-of-deepest-leaves.1218.ts
@@ -0,0 +1,38 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function lcaDeepestLeaves(root: TreeNode | null): TreeNode | null {
+    return postOrder(root)[1]
+};
+
+function postOrder(root:TreeNode|null):[number,TreeNode|null]{
+    if(root === null){
+        return [0,null];
+    }
+    const [LH,LN] = postOrder(root.left);
+    const [RH,RN] = postOrder(root.right);
+    if(LH === RH){
+        return [
+            LH+1,
+            root
+        ]
+    }else if(LH>RH){
+        return [
+            LH+1,
+            LN,
+        ]
+    }else{
+        return [
+            RH+1,
+            RN
+        ]
+    }
+}
