9794cf1382d70990580951717a84836a99c5485c
jiangshanmeta <540118044@qq.com>
2021-01-29 13:00:19 +0800

    1362

---
+++ src/1362.closest-divisors.1276/1362.closest-divisors.1276.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} num
+ * @return {number[]}
+ */
+var closestDivisors = function(num) {
+    const [a,b] = calc(num+1);
+    const [c,d] = calc(num+2);
+    if(a-b<c-d){
+        return [a,b];
+    }else{
+        return [c,d]
+    }
+};
+
+function calc(num){
+    let w = Math.floor(Math.sqrt(num));
+    while(num%w !== 0){
+        w = Math.floor(num/Math.ceil(num/w));
+    }
+    return [
+        num/w,
+        w
+    ]
+}
