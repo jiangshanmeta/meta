6f4516d25bf8e62fd55f2570fde75803c5f0bafa
jiangshanmeta <540118044@qq.com>
2021-01-31 10:08:32 +0800

    1679

---
+++ src/1679.max-number-of-k-sum-pairs.1798/1679.max-number-of-k-sum-pairs.1798.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxOperations = function(nums, k) {
+    const set = [];
+    const map = new Map();
+    for(let i=0;i<nums.length;i++){
+        if(map.has(nums[i])){
+            map.set(nums[i],(map.get(nums[i])) + 1);
+        }else{
+            set.push(nums[i]);
+            map.set(nums[i],1);
+        }
+    }
+
+    let result = 0;
+    for(let i=0;i<set.length;i++){
+        const rest = k-set[i];
+        if(rest<set[i]){
+            continue;
+        }
+        if(rest === set[i]){
+            const count = map.get(rest);
+            result += (count >> 1);
+        }else if(map.has(rest)){
+            result += Math.min(map.get(set[i]),map.get(rest))
+        }
+    }
+    return result;
+};
