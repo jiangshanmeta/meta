a7c79e5f84c69fb7f29f323249882dfaf44d2a44
jiangshanmeta <540118044@qq.com>
2021-01-31 10:08:22 +0800

    1678

---
+++ src/1678.goal-parser-interpretation.1797/1678.goal-parser-interpretation.1797.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function(command) {
+    let result = [];
+    let index = 0;
+    while(index<command.length){
+        if(command[index] === 'G'){
+            result.push(command[index++]);
+            continue;
+        }
+        if(command[index+1] === ')'){
+            result.push('o');
+            index += 2;
+        }else{
+            result.push('al');
+            index +=4;
+        }
+    }
+    return result.join('');
+};
