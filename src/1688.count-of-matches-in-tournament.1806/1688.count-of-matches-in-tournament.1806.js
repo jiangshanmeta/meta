e1cad887f51c287dc67ace4cb9000ea405970af2
jiangshanmeta <540118044@qq.com>
2021-02-01 21:31:25 +0800

    1688

---
+++ src/1688.count-of-matches-in-tournament.1806/1688.count-of-matches-in-tournament.1806.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numberOfMatches = function(n) {
+    let result = 0;
+    while(n>1){
+        const m = n >>>1;
+        result += m;
+        if(n&1){
+            n = m+1;
+        }else{
+            n = m;
+        }
+    }
+    return result
+};
