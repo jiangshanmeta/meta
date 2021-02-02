af6800e0b77fcad0d5f9d7c255aaef1494aada7b
jiangshanmeta <540118044@qq.com>
2021-02-02 20:23:03 +0800

    1700

---
+++ src/1700.number-of-students-unable-to-eat-lunch.1802/1700.number-of-students-unable-to-eat-lunch.1802.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+var countStudents = function(students, sandwiches) {
+    let one = 0;
+    let zero = 0;
+    for(let i=0;i<students.length;i++){
+        if(students[i] === 0){
+            zero++;
+        }else{
+            one++;
+        }
+    }
+    for(let i=0;i<sandwiches.length;i++){
+        if(sandwiches[i] === 0){
+            if(zero>0){
+                zero--;
+            }else{
+                break;
+            }
+        }else{
+            if(one>0){
+                one--;
+            }else{
+                break;
+            }
+        }
+    }
+    return zero+one;
+};
