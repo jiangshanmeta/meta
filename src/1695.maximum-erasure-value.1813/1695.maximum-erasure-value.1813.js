39ef4bbfc0413d0e547804cc1935f94efae8d583
jiangshanmeta <540118044@qq.com>
2021-02-02 20:22:45 +0800

    1695

---
+++ src/1695.maximum-erasure-value.1813/1695.maximum-erasure-value.1813.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumUniqueSubarray = function(nums) {
+    const map = {};
+    let result = 0;
+    let current = 0;
+    let left = 0;
+    let right = 0;
+    while(right<nums.length){
+        const index = map[nums[right]];
+
+        if(index === undefined || index<left){
+            current += nums[right];
+            result = Math.max(result,current);
+        }else{
+            for(let i=left;i<index;i++){
+                current -= nums[i]
+            }
+            left = index+1;
+        }
+        map[nums[right]] = right;
+        right++;
+    }
+
+    return result;
+};
