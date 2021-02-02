4af3600010dea4d91f6c93c0d92d2c60439a2733
jiangshanmeta <540118044@qq.com>
2021-02-02 20:22:56 +0800

    1696

---
+++ src/1696.jump-game-vi.1814/1696.jump-game-vi.1814.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxResult = function(nums, k) {
+    const seqScores = [nums[0]];
+    const seqIndexs = [0];
+    for(let i=1;i<nums.length;i++){
+        const diff = i-seqIndexs[0];
+        if(diff>k){
+            seqScores.shift();
+            seqIndexs.shift();
+        }
+        const curScore = seqScores[0]+nums[i];
+        while(seqScores.length && curScore>=seqScores[seqScores.length-1]){
+            seqScores.pop();
+            seqIndexs.pop();
+        }
+        seqScores.push(curScore);
+        seqIndexs.push(i);
+    }
+    return seqScores[seqScores.length-1]
+};
