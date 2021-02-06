df93f8939eacaa3676443d7d18f21a8c34e57ddd
jiangshanmeta <540118044@qq.com>
2021-02-06 14:24:18 +0800

    1736

---
+++ src/1736.latest-time-by-replacing-hidden-digits.1858/1736.latest-time-by-replacing-hidden-digits.1858.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} time
+ * @return {string}
+ */
+var maximumTime = function(time) {
+    const list = time.split('');
+    if(list[0] === '?'){
+        if(list[1] === '?'){
+            list[0] = '2';
+            list[1] = '3';
+        }else if(+time[1]>3){
+            list[0] = '1';
+        }else{
+            list[0] = '2';
+        }
+    }
+    
+    if(list[1] === '?'){
+        if(list[0] === '2'){
+            list[1] = '3';
+        }else{
+            list[1] = '9'
+        }
+    }
+    
+    if(list[3] === '?'){
+        list[3] = '5';
+    }
+    if(list[4] === '?'){
+        list[4] = '9';
+    }
+    
+    
+    return list.join('')
+};
