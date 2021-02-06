2371a1cfaed19b980576724ca69ff9d2ef902ed9
jiangshanmeta <540118044@qq.com>
2021-02-06 14:24:38 +0800

    1738

---
+++ src/1738.find-kth-largest-xor-coordinate-value.1860/1738.find-kth-largest-xor-coordinate-value.1860.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+var kthLargestValue = function(matrix, k) {
+    const M = matrix.length;
+    const N = matrix[0].length;
+    const row = new Array(N).fill(0);
+    const vertical = new Array(N).fill(0);
+    const candidates = [];
+    for(let i=0;i<M;i++){
+        vertical[0] ^= matrix[i][0];
+        row[0] = vertical[0];
+        for(let j=1;j<N;j++){
+            vertical[j] ^= matrix[i][j];
+            row[j] = row[j-1]^vertical[j];
+        }
+        candidates.push(...row)
+
+    }
+    
+    return candidates.sort((a,b)=>b-a)[k-1];
+};
