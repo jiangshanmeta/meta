fd989a47030359150f425e53b88c56fa27fdeb20
jiangshanmeta <540118044@qq.com>
2021-02-07 20:15:29 +0800

    18

---
+++ src/LCP18.2vYnGI.1000146/LCP18.2vYnGI.1000146.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} staple
+ * @param {number[]} drinks
+ * @param {number} x
+ * @return {number}
+ */
+var breakfastNumber = function(staple, drinks, x) {
+    let result = 0;
+    staple.sort((a,b)=>a-b);
+    drinks.sort((a,b)=>a-b);
+    let index = drinks.length-1;
+    const mod = 1000000007;
+    for(let i=0;i<staple.length;i++){
+        while(index>-1 && staple[i]+drinks[index]>x){
+            index--;
+        }
+        result = (result+index+1)%mod;
+    }
+    return result;
+};
