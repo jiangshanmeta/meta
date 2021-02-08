6b503f40bdd0030d84b495deb672f50f10270227
jiangshanmeta <540118044@qq.com>
2021-02-08 21:41:06 +0800

    44

---
+++ src/剑指Offer44.shu-zi-xu-lie-zhong-mou-yi-wei-de-shu-zi-lcof.100313/剑指Offer44.shu-zi-xu-lie-zhong-mou-yi-wei-de-shu-zi-lcof.100313.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var findNthDigit = function(n) {
+    let digit = 1;
+    let a = 9;
+    while(n-a*digit>0){
+        n -= a*digit;
+        a *= 10;
+        digit++;
+    }
+    a /= 9;
+    n--;
+    return (a+(n/digit | 0)).toString()[n%digit];
+};
