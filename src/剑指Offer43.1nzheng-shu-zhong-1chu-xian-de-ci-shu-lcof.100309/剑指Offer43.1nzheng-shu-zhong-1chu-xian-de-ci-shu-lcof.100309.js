dc316477a0a346f681f6b874511db5464a8b7e1a
jiangshanmeta <540118044@qq.com>
2021-02-08 21:40:54 +0800

    43

---
+++ src/剑指Offer43.1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof.100309/剑指Offer43.1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof.100309.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countDigitOne = function(n) {
+    if(n<1){
+        return 0;
+    }
+    if(n<10){
+        return 1;
+    }
+    const digit = Math.floor(Math.log10(n));
+    const base = Math.pow(10,digit);
+    const first = Math.floor(n/base);
+
+    return countDigitOne(n-base*first)+Math.min(n,base*2-1)-base+1+first*digit*Math.pow(10,digit-1);
+};
