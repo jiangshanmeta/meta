5ab91363e9abd758bcd6d1a266318138d8a2b681
jiangshanmeta <540118044@qq.com>
2021-02-09 11:02:58 +0800

    1748

---
+++ src/1748.sum-of-unique-elements.1848/1748.sum-of-unique-elements.1848.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfUnique = function(nums) {
+    const map = new Map();
+    let result = 0;
+    for(let i=0;i<nums.length;i++){
+        if(map.has(nums[i])){
+            const count = map.get(nums[i]);
+            if(count === 1){
+                result -= nums[i];
+                map.set(nums[i],2);
+            }
+        }else{
+            result += nums[i];
+            map.set(nums[i],1);
+        }
+    }
+    return result;
+};
