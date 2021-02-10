36df9c87749e73e5eb4d45a9117be8ee7f4d8439
jiangshanmeta <540118044@qq.com>
2021-02-10 18:05:47 +0800

    57

---
+++ src/剑指Offer57-II.he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof.100324/剑指Offer57-II.he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof.100324.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} target
+ * @return {number[][]}
+ */
+var findContinuousSequence = function(target) {
+    const result = [];
+    let left = 1;
+    let right = 1;
+    let sum = 1;
+    while(left<target){
+        while(sum<target && right<target){
+            right++;
+            sum += right;
+            
+        }
+        if(left !== right && sum === target){
+            const sub = [];
+            for(let i=left;i<=right;i++){
+                sub.push(i);
+            }
+            result.push(sub);
+        }
+        sum -= left;
+        left++;
+        if(left>right){
+            right++;
+            sum = left;
+        }
+    }
+    return result;
+};
