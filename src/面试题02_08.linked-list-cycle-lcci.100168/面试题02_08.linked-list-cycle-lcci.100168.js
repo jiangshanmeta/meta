88750c47fd8f0f0ac46150f9db5e7f59c36da9fe
jiangshanmeta <540118044@qq.com>
2021-02-11 10:21:18 +0800

    208

---
+++ src/面试题02_08.linked-list-cycle-lcci.100168/面试题02_08.linked-list-cycle-lcci.100168.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    if(!head || !head.next){
+        return null;
+    }
+    let slow = head;
+    let fast = head;
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow === fast){
+            break;
+        }
+    }
+    if(slow !== fast){
+        return null;
+    }
+    fast = head;
+    while(fast !== slow){
+        slow = slow.next;
+        fast = fast.next;
+    }
+    return fast
+};
