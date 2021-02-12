af0ade36d20312e0ec1b62312cf190ee49b9e65b
jiangshanmeta <540118044@qq.com>
2021-02-12 11:06:05 +0800

    811

---
+++ src/面试题08_11.coin-lcci.100231/面试题08_11.coin-lcci.100231.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var waysToChange = function(n) {
+    const dp = new Array(n+1).fill(1);
+    const coins = [
+        5,
+        10,
+        25,
+    ];
+    const mod = 1000000007;
+
+    for(let i=0;i<coins.length;i++){
+        const coin = coins[i];
+        for(let j=coin;j<=n;j++){
+            dp[j] = (dp[j]+dp[j-coin])%mod;
+        }
+    }
+    return dp[n]
+};
