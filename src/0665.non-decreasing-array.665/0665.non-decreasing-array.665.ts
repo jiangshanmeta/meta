96e8a68c9f3a8388bb3800663821737cc45a6947
jiangshanmeta <540118044@qq.com>
2021-02-12 11:06:37 +0800

    665

---
+++ src/0665.non-decreasing-array.665/0665.non-decreasing-array.665.ts
@@ -0,0 +1,22 @@
+function checkPossibility(nums: number[]): boolean {
+    let changed = false;
+
+    for(let i=1;i<nums.length;i++){
+        if(nums[i]<nums[i-1]){
+            if(changed){
+                return false;
+            }
+            changed = true;
+            if(i === 1){
+                nums[0] = nums[1];
+            }else{
+                if(nums[i]>=nums[i-2]){
+                    nums[i-1] = nums[i-2];
+                }else{
+                    nums[i] = nums[i-1];
+                }
+            }
+        }
+    }
+    return true
+};
