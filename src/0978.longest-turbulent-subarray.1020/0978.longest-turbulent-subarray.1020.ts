21718fb0ba3c0b76b84b4375151c763fe32306bd
jiangshanmeta <540118044@qq.com>
2021-02-13 00:10:16 +0800

    978

---
+++ src/0978.longest-turbulent-subarray.1020/0978.longest-turbulent-subarray.1020.ts
@@ -0,0 +1,38 @@
+function maxTurbulenceSize(arr: number[]): number {
+    let result = 1;
+    let l1 = 1;
+    let l2 = 1;
+    for(let i=1;i<arr.length;i++){
+        if(i&1){
+            if(arr[i]>arr[i-1]){
+                l1++;
+                result = Math.max(result,l1);
+            }else{
+                l1 = 1;
+            }
+
+            if(arr[i]<arr[i-1]){
+                l2++;
+                result = Math.max(result,l2);
+            }else{
+                l2 = 1;
+            }
+        }else{
+            if(arr[i]<arr[i-1]){
+                l1++;
+                result = Math.max(result,l1);
+            }else{
+                l1 = 1;
+            }
+
+            if(arr[i]>arr[i-1]){
+                l2++;
+                result = Math.max(result,l2);
+            }else{
+                l2 = 1;
+            }
+
+        }
+    }
+    return result
+};
