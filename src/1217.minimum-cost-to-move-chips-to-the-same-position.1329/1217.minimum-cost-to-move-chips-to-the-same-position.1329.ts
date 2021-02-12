00f68a2ab4cfcfdcaa1a55e4f9b4d3fbd1bebb91
jiangshanmeta <540118044@qq.com>
2021-02-13 00:10:27 +0800

    1217

---
+++ src/1217.minimum-cost-to-move-chips-to-the-same-position.1329/1217.minimum-cost-to-move-chips-to-the-same-position.1329.ts
@@ -0,0 +1,9 @@
+function minCostToMoveChips(position: number[]): number {
+    let odd = 0;
+    for(let i=0;i<position.length;i++){
+        if(position[i]&1){
+            odd++;
+        }
+    }
+    return Math.min(odd,position.length-odd)
+};
