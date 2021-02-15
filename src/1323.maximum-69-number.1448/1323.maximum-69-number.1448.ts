6f68eacf6be6a37cb91a11327aecee13e1318294
jiangshanmeta <540118044@qq.com>
2021-02-15 11:40:55 +0800

    1323

---
+++ src/1323.maximum-69-number.1448/1323.maximum-69-number.1448.ts
@@ -0,0 +1,10 @@
+function maximum69Number (num: number): number {
+    const sList = String(num).split('');
+    for(let i=0;i<sList.length;i++){
+        if(sList[i] === '6'){
+            sList[i] = '9';
+            break;
+        }
+    }
+    return +sList.join('')
+};
