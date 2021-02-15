9dc24a032b59b48f67ccf8f426c4f4d30163cd79
jiangshanmeta <540118044@qq.com>
2021-02-15 11:41:01 +0800

    1337

---
+++ src/1337.the-k-weakest-rows-in-a-matrix.1463/1337.the-k-weakest-rows-in-a-matrix.1463.ts
@@ -0,0 +1,20 @@
+function kWeakestRows(mat: number[][], k: number): number[] {
+    return mat.map((row,index)=>{
+        let count = 0;
+        for(let i=0;i<row.length;i++){
+            if(row[i] === 0){
+                break;
+            }
+            count++;
+        }
+        return {
+            count,
+            index,
+        }
+    }).sort((a,b)=>{
+        if(a.count !== b.count){
+            return a.count-b.count;
+        }        
+        return a.index-b.index
+    }).slice(0,k).map(item=>item.index)
+};
