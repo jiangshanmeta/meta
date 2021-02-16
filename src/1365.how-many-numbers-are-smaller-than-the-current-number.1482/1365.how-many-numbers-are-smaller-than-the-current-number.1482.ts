9bfbb56a0ff0a055af57ba429613fec7555614c5
jiangshanmeta <540118044@qq.com>
2021-02-16 11:17:54 +0800

    1365

---
+++ src/1365.how-many-numbers-are-smaller-than-the-current-number.1482/1365.how-many-numbers-are-smaller-than-the-current-number.1482.ts
@@ -0,0 +1,23 @@
+function smallerNumbersThanCurrent(nums: number[]): number[] {
+    const numIndexsMap = new Map<number,number[]>();
+    for(let i=0;i<nums.length;i++){
+        if(numIndexsMap.has(nums[i])){
+            numIndexsMap.get(nums[i])!.push(i);
+        }else{
+            numIndexsMap.set(nums[i],[i]);
+        }
+    }
+    nums.sort((a,b)=>a-b);
+    const result:number[] = new Array(nums.length);
+    for(let i=0;i<nums.length;i++){
+        if(i>0 && nums[i] === nums[i-1]){
+            continue;
+        }
+        const indexs = numIndexsMap.get(nums[i])!;
+        for(let j=0;j<indexs.length;j++){
+            result[indexs[j]] = i;
+        }
+    }
+
+    return result;
+};
