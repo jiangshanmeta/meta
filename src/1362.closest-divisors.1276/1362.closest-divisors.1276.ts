75c16d062280b1e44b0181a2220c232e1a65a82d
jiangshanmeta <540118044@qq.com>
2021-02-16 11:17:43 +0800

    1362

---
+++ src/1362.closest-divisors.1276/1362.closest-divisors.1276.ts
@@ -0,0 +1,20 @@
+function closestDivisors(num: number): number[] {
+    const [a,b] = calc(num+1);
+    const [c,d] = calc(num+2);
+    if(a-b<c-d){
+        return [a,b];
+    }else{
+        return [c,d]
+    }
+};
+
+function calc(num:number):[number,number]{
+    let w = Math.floor(Math.sqrt(num));
+    while(num%w !== 0){
+        w = Math.floor(num/Math.ceil(num/w));
+    }
+    return [
+        num/w,
+        w
+    ]
+}
