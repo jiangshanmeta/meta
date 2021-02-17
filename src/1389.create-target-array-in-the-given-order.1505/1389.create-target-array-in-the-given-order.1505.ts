84534577b9f31733a2ad23ba9b39ca0a65c5546b
jiangshanmeta <540118044@qq.com>
2021-02-17 11:54:37 +0800

    1389

---
+++ src/1389.create-target-array-in-the-given-order.1505/1389.create-target-array-in-the-given-order.1505.ts
@@ -0,0 +1,7 @@
+function createTargetArray(nums: number[], index: number[]): number[] {
+    const result:number[] = [];
+    for(let i=0;i<nums.length;i++){
+        result.splice(index[i],0,nums[i])
+    }
+    return result
+};
