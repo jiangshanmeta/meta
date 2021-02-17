853eb2d281a2883a99d9db1846c2806246f72f3f
jiangshanmeta <540118044@qq.com>
2021-02-17 11:54:43 +0800

    1394

---
+++ src/1394.find-lucky-integer-in-an-array.1510/1394.find-lucky-integer-in-an-array.1510.ts
@@ -0,0 +1,18 @@
+function findLucky(arr: number[]): number {
+    const map = new Map<number,number>();
+    for(let i=0;i<arr.length;i++){
+        if(map.has(arr[i])){
+            map.set(arr[i],map.get(arr[i])!+1  );
+        }else{
+            map.set(arr[i],1)
+        }
+    }
+    let result = -1;
+    for(let [num,count] of map.entries()){
+        if(num !== count){
+            continue;
+        }
+        result = Math.max(result,num);
+    }
+    return result;
+};
