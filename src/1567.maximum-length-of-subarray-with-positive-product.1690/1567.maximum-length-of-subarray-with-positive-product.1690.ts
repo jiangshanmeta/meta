2dd72ffb18c8b1b9223c743a092cb17f374076ad
jiangshanmeta <540118044@qq.com>
2021-02-19 01:27:55 +0800

    1567

---
+++ src/1567.maximum-length-of-subarray-with-positive-product.1690/1567.maximum-length-of-subarray-with-positive-product.1690.ts
@@ -0,0 +1,34 @@
+function getMaxLen(nums: number[]): number {
+    let result = 0;
+    let left = 0;
+    while(left<nums.length){
+        while(left<nums.length && nums[left] === 0){
+            left++;
+        }
+        if(left === nums.length){
+            break;
+        }
+        const oddIndexs:number[] = [];
+        let right = left;
+        while(right<nums.length && nums[right] !== 0){
+            if(nums[right]<0){
+                oddIndexs.push(right);
+            }
+            right++;
+        }
+        if(oddIndexs.length%2 === 0){
+            result = Math.max(result,right-left);
+        }else if(oddIndexs.length === 1){
+            result = Math.max(result,oddIndexs[0]-left,right-(oddIndexs[0]+1));
+        }else{
+            result = Math.max(
+                result,
+                oddIndexs[oddIndexs.length-1]-left,
+                right-(oddIndexs[0]+1),
+            );
+        }
+
+        left = right;
+    }
+    return result;
+};
