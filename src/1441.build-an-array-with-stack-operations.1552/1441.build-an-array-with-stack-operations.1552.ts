a3b8bd93b18c13b5f9e86f322cdda590d945d317
jiangshanmeta <540118044@qq.com>
2021-02-18 20:13:02 +0800

    1441

---
+++ src/1441.build-an-array-with-stack-operations.1552/1441.build-an-array-with-stack-operations.1552.ts
@@ -0,0 +1,16 @@
+function buildArray(target: number[], n: number): string[] {
+    let i = 0;
+    let j = 1;
+    const result:string[] = [];
+    while(i<target.length && j<=n){
+        if(target[i] === j){
+            result.push('Push');
+            i++;
+            j++;
+        }else{
+            result.push('Push','Pop');
+            j++;
+        }
+    }
+    return result
+};
