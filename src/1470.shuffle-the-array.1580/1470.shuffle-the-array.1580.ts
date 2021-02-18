4eab1923325461c63a010d491dc217548680cecd
jiangshanmeta <540118044@qq.com>
2021-02-18 20:13:18 +0800

    1470

---
+++ src/1470.shuffle-the-array.1580/1470.shuffle-the-array.1580.ts
@@ -0,0 +1,8 @@
+function shuffle(nums: number[], n: number): number[] {
+    const result:number[] = new Array(nums.length);
+    for(let i=0;i<n;i++){
+        result[2*i] = nums[i];
+        result[2*i+1] = nums[i+n];
+    }
+    return result
+};
