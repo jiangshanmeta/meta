e99c28ac3d179447b3058c0878c8faddd1eed59e
jiangshanmeta <540118044@qq.com>
2021-02-19 01:27:44 +0800

    1518

---
+++ src/1518.water-bottles.1642/1518.water-bottles.1642.ts
@@ -0,0 +1,12 @@
+function numWaterBottles(numBottles: number, numExchange: number): number {
+    let result = 0;
+    let empty = 0;
+    while(numBottles>0){
+        result += numBottles;
+        empty += numBottles;
+        const rest = empty%numExchange;
+        numBottles = (empty-rest)/numExchange;
+        empty = rest;
+    }
+    return result;
+};
