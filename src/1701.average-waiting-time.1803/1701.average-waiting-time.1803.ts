4cffd238723ff0381929c9ad8e03bf494158aeec
jiangshanmeta <540118044@qq.com>
2021-02-22 20:37:38 +0800

    1701

---
+++ src/1701.average-waiting-time.1803/1701.average-waiting-time.1803.ts
@@ -0,0 +1,16 @@
+function averageWaitingTime(customers: number[][]): number {
+    let time = 0;
+    let result = 0;
+    for(let i=0;i<customers.length;i++){
+        const [arrival,wait] = customers[i];
+        if(time>arrival){
+            time += wait;
+            result += time-arrival;
+        }else{
+            result += wait;
+            time = arrival+wait;
+        }
+
+    }
+    return result/customers.length;
+};
