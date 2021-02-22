6c41c25c92b4489e35efb146a1b6029f0b44de1b
jiangshanmeta <540118044@qq.com>
2021-02-22 20:37:08 +0800

    1680

---
+++ src/1680.concatenation-of-consecutive-binary-numbers.1800/1680.concatenation-of-consecutive-binary-numbers.1800.ts
@@ -0,0 +1,23 @@
+function concatenatedBinary(n: number): number {
+    let result = 1;
+    const mod = 10**9+7;
+    for(let i=2;i<=n;i++){
+        const length = getBit(i);
+        let mask = 1 << (length-1);
+        while(mask>0){
+            let bit = (i&mask)>0?1:0;
+            result = (result*2+ bit)%mod;
+            mask >>= 1;
+        }
+    }
+    return result;
+};
+
+function getBit(n:number):number{
+    let result = 0;
+    while(n>0){
+        result++;
+        n >>= 1;
+    }
+    return result;
+}
