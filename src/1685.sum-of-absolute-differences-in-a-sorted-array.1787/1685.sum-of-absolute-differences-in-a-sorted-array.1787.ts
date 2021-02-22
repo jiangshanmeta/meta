83841b8f66942d6e334ec90e3f78d7743bdac4fc
jiangshanmeta <540118044@qq.com>
2021-02-22 20:37:18 +0800

    1685

---
+++ src/1685.sum-of-absolute-differences-in-a-sorted-array.1787/1685.sum-of-absolute-differences-in-a-sorted-array.1787.ts
@@ -0,0 +1,15 @@
+function getSumAbsoluteDifferences(nums: number[]): number[] {
+    const result:number[] = new Array(nums.length).fill(0);
+    let prefix = 0;
+    for(let i=0;i<nums.length;i++){
+        result[i] += nums[i]*i-prefix;
+        prefix += nums[i];
+    }
+    let suffix = 0;
+    for(let i=nums.length-1;i>-1;i--){
+        result[i] += suffix-nums[i]*(nums.length-1-i);
+        suffix += nums[i];
+    }
+
+    return result;
+};
