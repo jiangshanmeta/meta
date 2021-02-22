4af281e92897e13aac67834cbab0c6a11004640e
jiangshanmeta <540118044@qq.com>
2021-02-22 20:36:51 +0800

    1673

---
+++ src/1673.find-the-most-competitive-subsequence.1792/1673.find-the-most-competitive-subsequence.1792.ts
@@ -0,0 +1,23 @@
+function mostCompetitive(nums: number[], k: number): number[] {
+    const helper:number[] = [];
+    let start = 0;
+    const result:number[] = [];
+    let end = nums.length-k;
+    for(let i=0;i<end;i++){
+        while(helper.length>0 && nums[i]<helper[helper.length-1]){
+            helper.pop();
+        }
+        helper.push(nums[i]);
+    }
+    while(end<nums.length){
+        const num = nums[end++];
+
+        while(helper.length>start && num<helper[helper.length-1]){
+            helper.pop();
+        }
+        helper.push(num);
+        result.push(helper[start++]);
+    }
+
+    return result;
+};
