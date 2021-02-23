234936a5e3f7a5fdd629c3f2791fbc9e9994baf0
jiangshanmeta <540118044@qq.com>
2021-02-23 21:38:25 +0800

    1704

---
+++ src/1704.determine-if-string-halves-are-alike.1823/1704.determine-if-string-halves-are-alike.1823.ts
@@ -0,0 +1,28 @@
+function halvesAreAlike(s: string): boolean {
+    const set = new Set([
+        'a',
+        'e',
+        'i',
+        'o',
+        'u',
+        'A',
+        'E',
+        'I',
+        'O',
+        'U',
+    ]);
+
+    let count = 0;
+    const half = s.length>>1;
+    for(let i=0;i<half;i++){
+        if(set.has(s[i])){
+            count++;
+        }
+    }
+    for(let i=half;i<s.length;i++){
+        if(set.has(s[i])){
+            count--;
+        }
+    }
+    return count === 0;
+};
