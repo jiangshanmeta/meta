7567bee05c7faa79d1028144a7483fa13078eef1
jiangshanmeta <540118044@qq.com>
2021-02-25 21:16:43 +0800

    1732

---
+++ src/1732.find-the-highest-altitude.1833/1732.find-the-highest-altitude.1833.ts
@@ -0,0 +1,9 @@
+function largestAltitude(gain: number[]): number {
+    let result = 0;
+    let height = 0;
+    for(let i=0;i<gain.length;i++){
+        height += gain[i];
+        result = Math.max(result,height)
+    }
+    return result;
+};
