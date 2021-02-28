48bd56bbb35bab3aff1418c6e216efd80dbe2020
jiangshanmeta <540118044@qq.com>
2021-02-28 10:25:32 +0800

    1758

---
+++ src/1758.minimum-changes-to-make-alternating-binary-string.1884/1758.minimum-changes-to-make-alternating-binary-string.1884.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minOperations = function(s) {
+    let result1 = 0;
+    let result2 = 0;
+    for(let i=0;i<s.length;i++){
+        if(i&1){
+            if(s[i] === '1'){
+                result2++;
+            }else{
+                result1++;
+            }
+        }else{
+            if(s[i] === '1'){
+                result1++;
+            }else{
+                result2++;
+            }
+        }
+    }
+    return Math.min(result1,result2)
+};
