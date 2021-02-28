d9cee12b6a253da59226c3a48de92dbe84d5bf43
jiangshanmeta <540118044@qq.com>
2021-02-28 10:25:08 +0800

    1750

---
+++ src/1750.minimum-length-of-string-after-deleting-similar-ends.1850/1750.minimum-length-of-string-after-deleting-similar-ends.1850.ts
@@ -0,0 +1,23 @@
+function minimumLength(s: string): number {
+    let left = 0;
+    let right = s.length-1;
+    while(left<right){
+        const cLeft = s[left];
+        const cRight = s[right];
+        if(cLeft !== cRight){
+            break;
+        }
+        left++;
+        right--;
+        while(left<=right && s[left] === cLeft){
+            left++;
+        }
+        while(left<=right && s[right] === cRight){
+            right--;
+        }
+    }
+    if(left>right){
+        return 0;
+    }
+    return right-left+1;
+};
