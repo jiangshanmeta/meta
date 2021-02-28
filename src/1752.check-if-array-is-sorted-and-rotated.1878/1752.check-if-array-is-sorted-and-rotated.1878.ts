1cfe9a404bdc17bc8b597ea3fe8ccc498d6f031b
jiangshanmeta <540118044@qq.com>
2021-02-28 10:25:13 +0800

    1752

---
+++ src/1752.check-if-array-is-sorted-and-rotated.1878/1752.check-if-array-is-sorted-and-rotated.1878.ts
@@ -0,0 +1,15 @@
+function check(nums: number[]): boolean {
+    let changed = false;
+    for(let i=1;i<nums.length;i++){
+        if(nums[i]<nums[i-1]){
+            if(changed){
+                return false;
+            }
+            if(nums[nums.length-1]>nums[0]){
+                return false;
+            }
+            changed = true;
+        }
+    }
+    return true;
+};
