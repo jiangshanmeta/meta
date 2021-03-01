72143f011e90bbf2ba803c6cd1a76be420adca31
jiangshanmeta <540118044@qq.com>
2021-03-01 21:11:10 +0800

    1760

---
+++ src/1760.minimum-limit-of-balls-in-a-bag.1886/1760.minimum-limit-of-balls-in-a-bag.1886.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} maxOperations
+ * @return {number}
+ */
+var minimumSize = function(nums, maxOperations) {
+    let low = 1;
+    let high = Math.max(...nums);
+    let result = high;
+    nums.sort((a,b)=>b-a);
+    while(low<=high){
+        const mid = low + ((high-low)>>1);
+        if(check(nums,mid,maxOperations)){
+            result = Math.min(result,mid);
+            high = mid-1;
+        }else{
+            low = mid+1;
+        }
+    }
+    return result;
+};
+
+function check(nums,profit,maxOperations){
+    let index = 0;
+    while(index<nums.length && maxOperations>=0){
+        maxOperations -= (Math.ceil(nums[index++]/profit)-1);
+    }
+    return index === nums.length && maxOperations>=0;
+}
