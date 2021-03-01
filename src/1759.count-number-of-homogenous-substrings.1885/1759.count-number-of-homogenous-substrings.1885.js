80abc2df9cc6b9304b20b778aeb80a078cc9f6d3
jiangshanmeta <540118044@qq.com>
2021-03-01 21:10:58 +0800

    1759

---
+++ src/1759.count-number-of-homogenous-substrings.1885/1759.count-number-of-homogenous-substrings.1885.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countHomogenous = function(s) {
+    let result = 0;
+    let mod = 10**9+7;
+    let index = 0;
+    while(index<s.length){
+        const c = s[index++];
+        let count = 1;
+        while(index<s.length && s[index] === c){
+            index++;
+            count++;
+        }
+        result = (result+ (1+count)*count/2 )%mod;
+    }
+    return result;
+};
