09da44bb686895abd5a837053763bf1504d10bcc
jiangshanmeta <540118044@qq.com>
2021-03-02 23:27:10 +0800

    1764

---
+++ src/1764.form-array-by-concatenating-subarrays-of-another-array.1874/1764.form-array-by-concatenating-subarrays-of-another-array.1874.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} groups
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canChoose = function(groups, nums) {
+    let index = 0;
+    for(let i=0;i<groups.length;i++){
+        while(index<nums.length && !isMatch(groups[i],nums,index)){
+            index++;
+        }
+        index += groups[i].length;
+        if(index>nums.length){
+            return false;
+        }
+    }
+    return true;
+    
+};
+
+function isMatch(group,nums,index){
+    for(let i=0;i<group.length;i++){
+        if(index === nums.length || group[i] !== nums[index++]){
+            return false;
+        }
+    }
+    return true;
+}
