508ad826ffb82b8357b9aa8dfbd9e26e10998855
jiangshanmeta <540118044@qq.com>
2021-03-03 21:32:40 +0800

    1769

---
+++ src/1769.minimum-number-of-operations-to-move-all-balls-to-each-box.1895/1769.minimum-number-of-operations-to-move-all-balls-to-each-box.1895.ts
@@ -0,0 +1,22 @@
+function minOperations(boxes: string): number[] {
+    const result:number[] = new Array(boxes.length);
+    let leftDis = 0;
+    let leftCount = 0;
+    for(let i=0;i<boxes.length;i++){
+        result[i] = leftDis;
+        if(boxes[i] === '1'){
+            leftCount++;
+        }
+        leftDis += leftCount;
+    }
+    let rightDis = 0;
+    let rightCount = 0;
+    for(let i=boxes.length-1;i>-1;i--){
+        result[i] += rightDis;
+        if(boxes[i] === '1'){
+            rightCount++;
+        }
+        rightDis += rightCount;
+    }
+    return result
+};
