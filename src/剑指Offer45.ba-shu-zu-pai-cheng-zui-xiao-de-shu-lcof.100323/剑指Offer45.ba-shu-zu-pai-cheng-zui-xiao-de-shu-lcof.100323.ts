2af87f77133943fb60577cf8b985549a4f5f2779
jiangshanmeta <540118044@qq.com>
2021-03-07 11:05:02 +0800

    45

---
+++ src/剑指Offer45.ba-shu-zu-pai-cheng-zui-xiao-de-shu-lcof.100323/剑指Offer45.ba-shu-zu-pai-cheng-zui-xiao-de-shu-lcof.100323.ts
@@ -0,0 +1,11 @@
+function minNumber(nums: number[]): string {
+    return nums.sort((a,b)=>{
+        const strA = String(a);
+        const strB = String(b);
+        if(strA[0] !== strB[0]){
+            return +strA[0]-(+strB[0]);
+        }
+        return `${strA}${strB}`<`${strB}${strA}`?-1:1;
+
+    }).join('')
+};
