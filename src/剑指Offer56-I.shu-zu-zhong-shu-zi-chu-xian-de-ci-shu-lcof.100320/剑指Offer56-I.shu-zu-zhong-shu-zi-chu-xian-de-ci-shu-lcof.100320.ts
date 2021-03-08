655d8d72704f0dde5858db3e1c8b66b47ccb4492
jiangshanmeta <540118044@qq.com>
2021-03-08 20:48:07 +0800

    56

---
+++ src/剑指Offer56-I.shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof.100320/剑指Offer56-I.shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof.100320.ts
@@ -0,0 +1,20 @@
+function singleNumbers(nums: number[]): number[] {
+    let xors = 0;
+    for(let i=0;i<nums.length;i++){
+        xors ^= nums[i];
+    }
+    let mask = 1;
+    while( (xors&mask) === 0 ){
+        mask <<= 1;
+    }
+    let result = 0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]&mask){
+            result ^= nums[i];
+        }
+    }
+    return [
+        result,
+        xors^result
+    ]
+};
