3fffeec1ed6dc7dc61961078d3bbff66def93e7b
jiangshanmeta <540118044@qq.com>
2021-03-09 22:03:36 +0800

    1780

---
+++ src/1780.check-if-number-is-a-sum-of-powers-of-three.1889/1780.check-if-number-is-a-sum-of-powers-of-three.1889.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var checkPowersOfThree = function(n) {
+    const candidates = [];
+    let base = 1;
+    while(base<=n){
+        candidates.push(base);
+        base *= 3;
+    }
+    let index = candidates.length-1;
+    while(index>-1){
+        if(candidates[index]>n){
+            index--;
+            continue;
+        }
+        n -= candidates[index--];
+    }
+    return n === 0;
+};
