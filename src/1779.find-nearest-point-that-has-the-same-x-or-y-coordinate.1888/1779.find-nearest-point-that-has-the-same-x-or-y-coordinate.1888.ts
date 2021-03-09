9f29f7ade5fd685e0f700906219ee16a765ffa05
jiangshanmeta <540118044@qq.com>
2021-03-09 22:03:31 +0800

    1779

---
+++ src/1779.find-nearest-point-that-has-the-same-x-or-y-coordinate.1888/1779.find-nearest-point-that-has-the-same-x-or-y-coordinate.1888.ts
@@ -0,0 +1,15 @@
+function nearestValidPoint(x: number, y: number, points: number[][]): number {
+    let result = -1;
+    let minDis = Infinity;
+    for(let i=0;i<points.length;i++){
+        const [xi,yi] = points[i];
+        if(xi === x || yi === y){
+            const dis = Math.abs(x-xi)+Math.abs(y-yi);
+            if(dis<minDis){
+                minDis = dis;
+                result = i;
+            }
+        }
+    }
+    return result
+};
