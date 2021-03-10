3870e07ca02b2b2e94d3c9e31dc32b01e4940ddb
jiangshanmeta <540118044@qq.com>
2021-03-10 21:03:53 +0800

    1784

---
+++ src/1784.check-if-binary-string-has-at-most-one-segment-of-ones.1910/1784.check-if-binary-string-has-at-most-one-segment-of-ones.1910.ts
@@ -0,0 +1,10 @@
+function checkOnesSegment(s: string): boolean {
+    let index = 0;
+    while(index<s.length && s[index] === '1'){
+        index++;
+    }
+    while(index<s.length && s[index] === '0'){
+        index++;
+    }
+    return index === s.length;
+};
