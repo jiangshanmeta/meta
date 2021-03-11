92ce0f82eba04fe6bb10cceebc28cd6744ea4f1e
jiangshanmeta <540118044@qq.com>
2021-03-11 20:16:25 +0800

    1785

---
+++ src/1785.minimum-elements-to-add-to-form-a-given-sum.1911/1785.minimum-elements-to-add-to-form-a-given-sum.1911.ts
@@ -0,0 +1,7 @@
+function minElements(nums: number[], limit: number, goal: number): number {
+    const sum = nums.reduce((sum,item)=>{
+        return sum+item;
+    },0);
+    const diff = Math.abs(sum-goal);
+    return Math.ceil(diff/limit);
+};
