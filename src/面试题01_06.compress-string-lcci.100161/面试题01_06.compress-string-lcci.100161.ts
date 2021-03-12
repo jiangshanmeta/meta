0ce435f9a2fa35b315b95126bb88b879c477245c
jiangshanmeta <540118044@qq.com>
2021-03-12 20:31:39 +0800

    1

---
+++ src/面试题01_06.compress-string-lcci.100161/面试题01_06.compress-string-lcci.100161.ts
@@ -0,0 +1,16 @@
+function compressString(S: string): string {
+    const result:Array<string|number> = [];
+    let index = 0;
+    while(index<S.length){
+        const c = S[index++];
+        let count = 1;
+        while(index<S.length && S[index] === c){
+            index++;
+            count++;
+        }
+        result.push(c);
+        result.push(count);
+    }
+    const encodedS = result.join('');
+    return encodedS.length<S.length?encodedS:S;
+};
