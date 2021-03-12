4c0a1ca1e46a92d54c0a2d03b008e758a0042b4d
jiangshanmeta <540118044@qq.com>
2021-03-12 20:31:35 +0800

    1

---
+++ src/面试题01_03.string-to-url-lcci.100160/面试题01_03.string-to-url-lcci.100160.ts
@@ -0,0 +1,11 @@
+function replaceSpaces(S: string, length: number): string {
+    const result:string[] = [];
+    for(let i=0;i<length;i++){
+        if(S[i] === ' '){
+            result.push('%20');
+        }else{
+            result.push(S[i])
+        }
+    }
+    return result.join('')
+};
