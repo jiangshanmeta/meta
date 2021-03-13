34bcb69fe1a0dcf907cdfd92a004809804a2427a
jiangshanmeta <540118044@qq.com>
2021-03-13 22:02:08 +0800

    121

---
+++ src/0121.best-time-to-buy-and-sell-stock.121/0121.best-time-to-buy-and-sell-stock.121.ts
@@ -0,0 +1,12 @@
+function maxProfit(prices: number[]): number {
+    let prevMin = prices[0]
+    let result = 0;
+    for(let i=1;i<prices.length;i++){
+        if(prices[i]>prevMin){
+            result = Math.max(result,prices[i]-prevMin);
+        }else{
+            prevMin = Math.min(prevMin,prices[i])
+        }
+    }
+    return result;
+};
