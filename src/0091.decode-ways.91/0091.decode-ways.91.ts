27506b165eb8c494ea332be828a63bef77122b78
jiangshanmeta <540118044@qq.com>
2021-03-13 22:01:53 +0800

    91

---
+++ src/0091.decode-ways.91/0091.decode-ways.91.ts
@@ -0,0 +1,14 @@
+function numDecodings(s: string): number {
+    const dp = new Array(s.length).fill(0);
+    dp[0] = s[0] === '0'?0:1;
+    for(let i=1;i<s.length;i++){
+        if(s[i] !== '0'){
+            dp[i] += dp[i-1];
+        }
+        const num = +s.slice(i-1,i+1);
+        if(num>9 && num<27){
+            dp[i] += i>1?dp[i-2]:1;
+        }
+    }
+    return dp[dp.length-1];
+};
