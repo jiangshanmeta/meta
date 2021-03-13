4c94ac031e6d78815e11a0c11889d11e358f3726
jiangshanmeta <540118044@qq.com>
2021-03-13 22:02:15 +0800

    150

---
+++ src/0150.evaluate-reverse-polish-notation.150/0150.evaluate-reverse-polish-notation.150.ts
@@ -0,0 +1,30 @@
+function evalRPN(tokens: string[]): number {
+    const stack:number[] = [];
+    for(let i=0;i<tokens.length;i++){
+        if(tokens[i] === '+'){
+            const b = stack.pop();
+            const a = stack.pop();
+            stack.push(a+b);
+        }else if(tokens[i] === '-'){
+            const b = stack.pop();
+            const a = stack.pop();
+            stack.push(a-b);
+        }else if(tokens[i] === '*'){
+            const b = stack.pop();
+            const a = stack.pop();
+            stack.push(a*b);
+        }else if(tokens[i] === '/'){
+            const b = stack.pop();
+            const a = stack.pop();
+            const r = a/b;
+            if(r<0){
+                stack.push(Math.ceil(r));
+            }else{
+                stack.push(Math.floor(r));
+            }
+        }else{
+            stack.push(+tokens[i]);
+        }
+    }
+    return stack.pop();
+};
