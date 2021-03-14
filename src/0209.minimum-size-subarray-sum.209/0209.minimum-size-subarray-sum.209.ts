6aaf7bc8561458ec2d4e5e9e36fcc589c30ef7d2
jiangshanmeta <540118044@qq.com>
2021-03-14 10:07:26 +0800

    209

---
+++ src/0209.minimum-size-subarray-sum.209/0209.minimum-size-subarray-sum.209.ts
@@ -0,0 +1,17 @@
+function minSubArrayLen(target: number, nums: number[]): number {
+    let result = nums.length+1;
+    let left = 0;
+    let right = -1;
+    let sum = 0;
+    while(left<nums.length){
+        while(right+1<nums.length && sum<target){
+            sum += nums[++right];
+        }
+        if(sum<target){
+            break;
+        }
+        result = Math.min(result,right-left+1);
+        sum -= nums[left++];
+    }
+    return result === nums.length+1?0:result;
+};
