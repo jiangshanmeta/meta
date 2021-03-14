ae56863dc728d925aee010246b8195a484d7a009
jiangshanmeta <540118044@qq.com>
2021-03-14 10:07:37 +0800

    230

---
+++ src/0230.kth-smallest-element-in-a-bst.230/0230.kth-smallest-element-in-a-bst.230.ts
@@ -0,0 +1,32 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+         this.val = (val===undefined ? 0 : val)
+         this.left = (left===undefined ? null : left)
+         this.right = (right===undefined ? null : right)
+    }
+}
+
+
+function kthSmallest(root: TreeNode | null, k: number): number {
+    const stack:TreeNode[] = [];
+    while(root){
+        stack.push(root);
+        root = root.left;
+    }
+    while(stack.length){
+        let root:TreeNode|null = stack.pop()!;
+        k--;
+        if(k === 0){
+            return root!.val;
+        }
+        root = root.right;
+        while(root){
+            stack.push(root);
+            root = root.left;
+        }
+    }
+    return 0;
+};
