7f44a729fc8f19b969af788093f870528e72ff5d
jiangshanmeta <540118044@qq.com>
2021-03-14 10:07:21 +0800

    202

---
+++ src/0202.happy-number.202/0202.happy-number.202.ts
@@ -0,0 +1,18 @@
+function isHappy(n: number): boolean {
+    const set = new Set<number>();
+    set.add(n);
+    while(n !== 1){
+        let sum = 0;
+        while(n>0){
+           const digit = n%10;
+           sum += digit**2;
+           n = (n-digit)/10; 
+        }
+        if(set.has(sum)){
+            return false;
+        }
+        set.add(sum);
+        n= sum;
+    }
+    return true;
+};
