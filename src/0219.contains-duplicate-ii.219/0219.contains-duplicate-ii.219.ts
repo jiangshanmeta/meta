410eb789508f74624225a8d6cfce3d67ef01c767
jiangshanmeta <540118044@qq.com>
2021-03-14 10:07:32 +0800

    219

---
+++ src/0219.contains-duplicate-ii.219/0219.contains-duplicate-ii.219.ts
@@ -0,0 +1,10 @@
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+    const map = new Map<number,number>();
+    for(let i=0;i<nums.length;i++){
+        if(map.has(nums[i]) && (i-map.get(nums[i])) <=k ){
+            return true;
+        }
+        map.set(nums[i],i);
+    }
+    return false;
+};
