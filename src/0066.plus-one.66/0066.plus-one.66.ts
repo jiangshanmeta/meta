8b1960a8306fd9025278aef4bc946995d85edf92
jiangshanmeta <540118044@qq.com>
2021-03-15 21:26:09 +0800

    66

---
+++ src/0066.plus-one.66/0066.plus-one.66.ts
@@ -0,0 +1,15 @@
+function plusOne(digits: number[]): number[] {
+    let addon = 1;
+    let index = digits.length-1;
+    while(addon && index>-1){
+        const sum = digits[index]+addon;
+        const digit = sum%10;
+        digits[index] = digit;
+        addon = (sum-digit)/10;
+        index--;
+    }
+    if(addon){
+        digits.unshift(addon);
+    }
+    return digits;
+};
