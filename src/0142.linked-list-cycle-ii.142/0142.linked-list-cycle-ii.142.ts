a5dd92c819b5427679551de7341637d7a63d60d3
jiangshanmeta <540118044@qq.com>
2021-03-16 22:12:15 +0800

    142

---
+++ src/0142.linked-list-cycle-ii.142/0142.linked-list-cycle-ii.142.ts
@@ -0,0 +1,28 @@
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+function detectCycle(head: ListNode | null): ListNode | null {
+    if(head === null){
+        return null;
+    }
+    let slow = head;
+    let fast = head;
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow === fast){
+            while(head !== slow){
+                head = head.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+    }
+    return null;
+};
