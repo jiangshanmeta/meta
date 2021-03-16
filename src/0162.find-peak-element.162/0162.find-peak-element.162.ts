75f91a1bb1d519b7753d935cf813c8d56398eee9
jiangshanmeta <540118044@qq.com>
2021-03-17 01:14:16 +0800

    162

---
+++ src/0162.find-peak-element.162/0162.find-peak-element.162.ts
@@ -0,0 +1,17 @@
+function findPeakElement(nums: number[]): number {
+    if(nums.length === 1){
+        return 0;
+    }
+    if(nums[0]>nums[1]){
+        return 0;
+    }
+    if(nums[nums.length-1]>nums[nums.length-2]){
+        return nums.length-1;
+    }
+    for(let i=1;i<nums.length-1;i++){
+        if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
+            return i;
+        }
+    }
+    return -1;
+};
