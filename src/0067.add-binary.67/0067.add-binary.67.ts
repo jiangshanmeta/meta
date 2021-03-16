9c370f62c29182e4ed759539dc811d1ef62daa89
jiangshanmeta <540118044@qq.com>
2021-03-16 22:11:58 +0800

    67

---
+++ src/0067.add-binary.67/0067.add-binary.67.ts
@@ -0,0 +1,29 @@
+function addBinary(a: string, b: string): string {
+    const stack:number[] = [];
+    let indexA = a.length-1;
+    let indexB = b.length-1;
+    let addon = 0;
+    while(indexA>-1 && indexB>-1){
+        const sum = addon+(+a[indexA--])+(+b[indexB--]);
+        const digit = sum&1;
+        stack.push(digit);
+        addon = sum >> 1;
+    }
+    while(indexA>-1){
+        const sum = addon+(+a[indexA--]);
+        const digit = sum&1;
+        stack.push(digit);
+        addon = sum >>1;
+    }
+    while(indexB>-1){
+        const sum = addon+(+b[indexB--]);
+        const digit = sum&1;
+        stack.push(digit);
+        addon = sum >>1;
+    }
+    if(addon){
+        stack.push(addon);
+    }
+
+    return stack.reverse().join('');
+};
