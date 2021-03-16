c2e3d48f78911cbdb25f71c2746dbd189b08307e
jiangshanmeta <540118044@qq.com>
2021-03-17 01:14:31 +0800

    171

---
+++ src/0171.excel-sheet-column-number.171/0171.excel-sheet-column-number.171.ts
@@ -0,0 +1,7 @@
+function titleToNumber(columnTitle: string): number {
+    let result = 0;
+    for(let i=0;i<columnTitle.length;i++){
+        result = result*26+columnTitle.charCodeAt(i)-64;
+    }
+    return result;
+};
