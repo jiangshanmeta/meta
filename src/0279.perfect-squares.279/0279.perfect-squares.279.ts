ffe3a74522371da02ab1f13c4376d683465cbd28
jiangshanmeta <540118044@qq.com>
2021-03-17 22:05:03 +0800

    279

---
+++ src/0279.perfect-squares.279/0279.perfect-squares.279.ts
@@ -0,0 +1,24 @@
+function numSquares(n: number): number {
+    const dp = new Array(n+1);
+    dp[0] = 0;
+    dp[1] = 1;
+    const candidate = [1];
+    let base = 2;
+    let square = 4;
+    while(square<=n){
+        candidate.push(square);
+        base++;
+        square = base*base;
+    }
+
+    for(let i=2;i<=n;i++){
+        dp[i] = i;
+        for(let j=0;j<candidate.length;j++){
+            if(i-candidate[j]<0){
+                break;
+            }
+            dp[i] = Math.min(dp[i],dp[i-candidate[j]]+1);
+        }
+    }
+    return dp[n];
+};
