a43cf06b54f33d9ce3fddcac09d5b1e4f8155ce4
jiangshanmeta <540118044@qq.com>
2021-03-18 20:07:05 +0800

    442

---
+++ src/0442.find-all-duplicates-in-an-array.442/0442.find-all-duplicates-in-an-array.442.ts
@@ -0,0 +1,23 @@
+function findDuplicates(nums: number[]): number[] {
+    const result:number[] = [];
+    let index = 0;
+    while(index<nums.length){
+        if(nums[index] === 0 || nums[index] === index+1){
+            index++;
+            continue;
+        }
+        if(nums[nums[index]-1] === nums[index]){
+            result.push(nums[index]);
+            nums[index++] = 0;
+        }else{
+            swap(nums,index,nums[index]-1);
+        }
+    }
+    return result;
+};
+
+function swap<T>(arr:T[],i,j){
+    const tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+}
