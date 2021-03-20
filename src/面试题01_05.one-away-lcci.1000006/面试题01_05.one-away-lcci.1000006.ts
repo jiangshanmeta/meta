00ec0f7b5701f5488b4e7639f759017862c87a1a
jiangshanmeta <540118044@qq.com>
2021-03-20 13:51:08 +0800

    1

---
+++ src/面试题01_05.one-away-lcci.1000006/面试题01_05.one-away-lcci.1000006.ts
@@ -0,0 +1,40 @@
+function oneEditAway(first: string, second: string): boolean {
+    if(Math.abs(first.length-second.length)>1){
+        return false;
+    }
+    if(first.length === second.length){
+        let changed = false;
+        for(let i=0;i<first.length;i++){
+            if(first[i] === second[i]){
+                continue;
+            }
+            if(changed){
+                return false
+            }
+            changed = true;
+        }
+        return true;
+    }
+    if(first.length<second.length){
+        const tmp = first;
+        first = second;
+        second = tmp;
+    }
+    let index1 = 0;
+    let index2 = 0;
+    let changed = false;
+    while(index1<first.length && index2<second.length){
+        if(first[index1] === second[index2]){
+            index1++;
+            index2++;
+            continue;
+        }
+        if(changed){
+            return false;
+        }
+        changed = true;
+        index1++;
+    }
+    
+    return true;
+};
