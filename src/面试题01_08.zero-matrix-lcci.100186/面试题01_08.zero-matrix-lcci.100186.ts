fe6c712cf7b604932840d51af92bc4c57cef4f4e
jiangshanmeta <540118044@qq.com>
2021-03-20 13:51:13 +0800

    1

---
+++ src/面试题01_08.zero-matrix-lcci.100186/面试题01_08.zero-matrix-lcci.100186.ts
@@ -0,0 +1,30 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes(matrix: number[][]): void {
+    const M = matrix.length;
+    if(M === 0){
+        return;
+    }
+    const N = matrix[0].length;
+    const vertical:boolean[] = new Array(M).fill(false);
+    const horizontal:boolean[] = new Array(N).fill(false);
+    for(let i=0;i<M;i++){
+        for(let j=0;j<N;j++){
+            if(matrix[i][j] === 0){
+                vertical[i] = true;
+                horizontal[j] = true;
+            }
+        }
+    }
+
+    for(let i=0;i<M;i++){
+        for(let j=0;j<N;j++){
+            if(vertical[i] || horizontal[j]){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+
+};
