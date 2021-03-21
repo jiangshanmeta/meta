459599289000778499d9f9e6853da828e851a751
jiangshanmeta <540118044@qq.com>
2021-03-21 14:25:21 +0800

    122

---
+++ src/0122.best-time-to-buy-and-sell-stock-ii.122/0122.best-time-to-buy-and-sell-stock-ii.122.ts
@@ -0,0 +1,17 @@
+function maxProfit(prices: number[]): number {
+    const stackMin = [prices[0]];
+    const stackMax = [prices[0]];
+    for(let i=1;i<prices.length;i++){
+        if(prices[i]>stackMax[stackMax.length-1]){
+            stackMax[stackMax.length-1] = prices[i];
+        }else if(prices[i]<stackMax[stackMax.length-1]){
+            stackMin.push(prices[i]);
+            stackMax.push(prices[i]);
+        }
+    }
+    let result = 0;
+    for(let i=0;i<stackMax.length;i++){
+        result += stackMax[i]-stackMin[i];
+    }
+    return result
+};
