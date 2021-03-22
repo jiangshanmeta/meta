a2faaa399a04a235861de834d1ad798a43eae2b7
jiangshanmeta <540118044@qq.com>
2021-03-23 01:29:28 +0800

    3

---
+++ src/面试题03_04.implement-queue-using-stacks-lcci.100170/面试题03_04.implement-queue-using-stacks-lcci.100170.ts
@@ -0,0 +1,44 @@
+class MyQueue {
+    stack1:number[] = [];
+    stack2:number[] = [];
+    constructor() {
+        
+    }
+
+    push(x: number): void {
+        this.stack1.push(x);
+    }
+
+    pop(): number {
+        if(this.stack2.length === 0){
+            this.adjust()
+        }
+        return this.stack2.pop()!;
+    }
+
+    peek(): number {
+        if(this.stack2.length === 0){
+            this.adjust()
+        }
+        return this.stack2[this.stack2.length-1]
+    }
+
+    empty(): boolean {
+        return this.stack1.length === 0 && this.stack2.length === 0;
+    }
+
+    adjust(){
+        while(this.stack1.length){
+            this.stack2.push(this.stack1.pop()!);
+        }
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
