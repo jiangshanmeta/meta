cd51fe3d84b5931005ece1bb33023848fe409a94
jiangshanmeta <540118044@qq.com>
2021-03-22 21:30:24 +0800

    1790

---
+++ src/1790.check-if-one-string-swap-can-make-strings-equal.1915/1790.check-if-one-string-swap-can-make-strings-equal.1915.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var areAlmostEqual = function(s1, s2) {
+    let changed = false;
+    let firstIndex = -1;
+    for(let i=0;i<s1.length;i++){
+        if(s1[i] === s2[i]){
+            continue;
+        }
+        if(changed){
+            return false;
+        }
+        if(firstIndex === -1){
+            firstIndex = i;
+        }else{
+            if(s1[firstIndex] === s2[i] && s1[i] === s2[firstIndex]){
+                changed = true;
+                continue;
+            }
+            return false;
+        }
+    }
+    if(firstIndex !== -1 && !changed){
+        return false;
+    }
+    return true
+};
