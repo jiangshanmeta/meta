dc52795fe2af3b24c74d8187c9191a4298aea0a7
jiangshanmeta <540118044@qq.com>
2021-03-24 22:20:17 +0800

    8

---
+++ src/面试题08_01.three-steps-problem-lcci.100197/面试题08_01.three-steps-problem-lcci.100197.ts
@@ -0,0 +1,11 @@
+function waysToStep(n: number): number {
+    const dp:number[] = new Array(n+1).fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+    const mod = 1000000007;
+    for(let i=3;i<dp.length;i++){
+        dp[i] = (dp[i-1]+dp[i-2]+dp[i-3])%mod;
+    }
+    return dp[n];
+};
