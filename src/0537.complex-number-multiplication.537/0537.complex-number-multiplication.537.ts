910e67de730b1c4617ac65e69a14a1c31d38b346
jiangshanmeta <540118044@qq.com>
2021-03-26 20:55:34 +0800

    537

---
+++ src/0537.complex-number-multiplication.537/0537.complex-number-multiplication.537.ts
@@ -0,0 +1,14 @@
+function complexNumberMultiply(a: string, b: string): string {
+    const [n1,n2] = parse(a);
+    const [n3,n4] = parse(b);
+
+    return `${n1*n3-n2*n4}+${n2*n3+n1*n4}i`; 
+};
+
+function parse(str:string):[number,number]{
+    const plusIndex = str.indexOf('+');
+    return [
+        +str.slice(0,plusIndex),
+        +str.slice(plusIndex+1,str.length-1)
+    ];
+}
