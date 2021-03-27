6ba47c7aff6c604a516c62861b59df06dd9ad313
jiangshanmeta <540118044@qq.com>
2021-03-27 16:49:11 +0800

    693

---
+++ src/0693.binary-number-with-alternating-bits.693/0693.binary-number-with-alternating-bits.693.ts
@@ -0,0 +1,13 @@
+function hasAlternatingBits(n: number): boolean {
+    let last = n&1;
+    n >>>= 1;
+    while(n>0){
+        const digit = n&1;
+        if( (digit^last) === 0){
+            return false;
+        }
+        last = digit;
+        n >>>= 1;
+    }
+    return true
+};
