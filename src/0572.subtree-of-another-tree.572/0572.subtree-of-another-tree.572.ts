d6619c12653b1675d6fade37dc76aa1df0d64576
jiangshanmeta <540118044@qq.com>
2021-03-27 16:48:55 +0800

    572

---
+++ src/0572.subtree-of-another-tree.572/0572.subtree-of-another-tree.572.ts
@@ -0,0 +1,28 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+         this.val = (val===undefined ? 0 : val)
+         this.left = (left===undefined ? null : left)
+         this.right = (right===undefined ? null : right)
+    }
+}
+
+function isSubtree(s: TreeNode | null, t: TreeNode | null): boolean {
+    if(s === null || t === null){
+        return false;
+    }
+
+    return match(s,t) || isSubtree(s.left,t) || isSubtree(s.right,t);
+};
+
+function match(s:TreeNode|null,t:TreeNode|null):boolean{
+    if(s && t){
+        return s.val === t.val && match(s.left,t.left) && match(s.right,t.right);
+    }else if(s || t){
+        return false;
+    }else{
+        return true;
+    }
+}
