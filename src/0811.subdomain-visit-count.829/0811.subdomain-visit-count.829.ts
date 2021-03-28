d0033affdd2eaab1e6b92841a9f96d5066046bde
jiangshanmeta <540118044@qq.com>
2021-03-28 15:26:40 +0800

    811

---
+++ src/0811.subdomain-visit-count.829/0811.subdomain-visit-count.829.ts
@@ -0,0 +1,19 @@
+function subdomainVisits(cpdomains: string[]): string[] {
+    const map = new Map<string,number>();
+    for(let i=0;i<cpdomains.length;i++){
+        const infos = cpdomains[i].split(' ');
+        const count = +infos[0];
+        const domainList = infos[1].split('.');
+        while(domainList.length){
+            const domain = domainList.join('.');
+            map.set(domain,map.has(domain)?map.get(domain)+count:count);
+            domainList.shift();
+        }
+    }
+
+    const result:string[] = [];
+    for(let [domain,count] of map){
+        result.push(`${count} ${domain}`)
+    }
+    return result;
+};
