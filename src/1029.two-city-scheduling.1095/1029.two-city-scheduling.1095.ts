c34245738873ebbd8286bbff394fbf2c3c260510
jiangshanmeta <540118044@qq.com>
2021-03-29 20:18:30 +0800

    1029

---
+++ src/1029.two-city-scheduling.1095/1029.two-city-scheduling.1095.ts
@@ -0,0 +1,14 @@
+function twoCitySchedCost(costs: number[][]): number {
+    for(let i=0;i<costs.length;i++){
+        costs[i].push(costs[i][1]-costs[i][0]);
+    }
+    costs.sort((a,b)=>a[2]-b[2]);
+    let result = 0;
+    for(let i=0;i<costs.length/2;i++){
+        result += costs[i][1];
+    }
+    for(let i=costs.length/2;i<costs.length;i++){
+        result += costs[i][0];
+    }
+    return result;
+};
