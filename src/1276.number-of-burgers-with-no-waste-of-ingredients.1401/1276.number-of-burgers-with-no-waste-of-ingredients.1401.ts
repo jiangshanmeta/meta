548c836524fdd6b2c11a59c8fc54341c43f4fa04
jiangshanmeta <540118044@qq.com>
2021-03-31 00:02:38 +0800

    1276

---
+++ src/1276.number-of-burgers-with-no-waste-of-ingredients.1401/1276.number-of-burgers-with-no-waste-of-ingredients.1401.ts
@@ -0,0 +1,8 @@
+function numOfBurgers(tomatoSlices: number, cheeseSlices: number): number[] {
+    const x = (tomatoSlices-2*cheeseSlices)/2;
+    if(x<0 || !Number.isInteger(x) || cheeseSlices-x<0){
+        return [];
+    }
+    
+    return [x,cheeseSlices-x]
+};
