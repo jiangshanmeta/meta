9957e38f125edb42ad43543ce49e251b319f9935
jiangshanmeta <540118044@qq.com>
2021-03-30 22:35:08 +0800

    1033

---
+++ src/1033.moving-stones-until-consecutive.1103/1033.moving-stones-until-consecutive.1103.ts
@@ -0,0 +1,12 @@
+function numMovesStones(a: number, b: number, c: number): number[] {
+    const seq = [a,b,c].sort((a,b)=>a-b);
+    const result = [1,seq[2]-seq[0]-2];
+    const diff1 = seq[1]-seq[0];
+    const diff2 = seq[2]-seq[1];
+    if(diff1>2 && diff2>2){
+        result[0] = 2;
+    }else if(diff1 === 1 && diff2 === 1){
+        result[0] = 0;
+    }
+    return result;
+};
