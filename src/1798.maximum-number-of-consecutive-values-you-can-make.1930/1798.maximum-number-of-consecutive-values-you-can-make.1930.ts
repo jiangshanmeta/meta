fefffe72f683712a5d68463743b7502c55e798c4
jiangshanmeta <540118044@qq.com>
2021-04-03 10:19:03 +0800

    1798

---
+++ src/1798.maximum-number-of-consecutive-values-you-can-make.1930/1798.maximum-number-of-consecutive-values-you-can-make.1930.ts
@@ -0,0 +1,11 @@
+function getMaximumConsecutive(coins: number[]): number {
+    let result = 0;
+    coins.sort((a,b)=>a-b);
+    for(let i=0;i<coins.length;i++){
+        if(coins[i]>result+1){
+            break;
+        }
+        result += coins[i];
+    }
+    return result+1;
+};
