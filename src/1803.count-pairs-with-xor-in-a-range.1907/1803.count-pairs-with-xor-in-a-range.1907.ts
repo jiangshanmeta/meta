f62bdf906cb1c435a75dad3d552a1630724a8b12
jiangshanmeta <540118044@qq.com>
2021-04-04 00:16:31 +0800

    1803

---
+++ src/1803.count-pairs-with-xor-in-a-range.1907/1803.count-pairs-with-xor-in-a-range.1907.ts
@@ -0,0 +1,12 @@
+function countPairs(nums: number[], low: number, high: number): number {
+    let result = 0;
+    for(let i=1;i<nums.length;i++){
+        for(let j=0;j<i;j++){
+            const xor = nums[i]^nums[j];
+            if(xor>=low && xor<=high){
+                result++;
+            }
+        }
+    }
+    return result
+};
