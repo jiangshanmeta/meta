0f14cfe747eb3f52953e47274bc0fbf855c6cfd6
jiangshanmeta <540118044@qq.com>
2021-04-03 10:19:13 +0800

    1800

---
+++ src/1800.maximum-ascending-subarray-sum.1927/1800.maximum-ascending-subarray-sum.1927.ts
@@ -0,0 +1,15 @@
+function maxAscendingSum(nums: number[]): number {
+    let result = nums[0];
+    let sum = nums[0];
+    let max = nums[0];
+    for(let i=1;i<nums.length;i++){
+        if(nums[i]>max){
+            sum += nums[i];
+        }else{
+            sum = nums[i];
+        }
+        max = nums[i];
+        result = Math.max(result,sum);
+    }
+    return result;
+};
