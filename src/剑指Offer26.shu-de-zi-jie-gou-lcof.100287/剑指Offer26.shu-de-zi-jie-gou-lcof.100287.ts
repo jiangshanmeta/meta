61b524c89ca02314120a67d7665f8d27f801ec53
jiangshanmeta <540118044@qq.com>
2021-04-04 00:16:36 +0800

    26

---
+++ src/剑指Offer26.shu-de-zi-jie-gou-lcof.100287/剑指Offer26.shu-de-zi-jie-gou-lcof.100287.ts
@@ -0,0 +1,34 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+         this.val = (val===undefined ? 0 : val)
+         this.left = (left===undefined ? null : left)
+         this.right = (right===undefined ? null : right)
+    }
+}
+
+
+function isSubStructure(A: TreeNode | null, B: TreeNode | null): boolean {
+    if(B === null){
+        return false;
+    }
+    if(A === null){
+        return false;
+    }
+
+    return judge(A,B) || isSubStructure(A.left,B) || isSubStructure(A.right,B);
+};
+
+function judge(A:TreeNode|null,B:TreeNode|null){
+    if(A && B){
+        return A.val === B.val && judge(A.left,B.left) && judge(A.right,B.right);
+    }else if(A){
+        return true;
+    }else if(B){
+        return false;
+    }else{
+        return true;
+    }
+}
