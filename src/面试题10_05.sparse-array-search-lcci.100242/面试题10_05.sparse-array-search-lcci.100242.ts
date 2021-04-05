5e2b30bfdcefaa25e54bc7f2f90d79a72219c787
jiangshanmeta <540118044@qq.com>
2021-04-06 00:12:46 +0800

    10

---
+++ src/面试题10_05.sparse-array-search-lcci.100242/面试题10_05.sparse-array-search-lcci.100242.ts
@@ -0,0 +1,25 @@
+function findString(words: string[], s: string): number {
+    return binarySearch(words,s,0,words.length)
+};
+
+function binarySearch(words:string[],s:string,left:number,right:number):number{
+    if(right<left){
+        return -1;
+    }
+    const mid = left + ((right-left)>>1);
+
+    if(words[mid] === ''){
+        const l = binarySearch(words,s,left,mid-1);
+        if(l === -1){
+            return binarySearch(words,s,mid+1,right);
+        }
+        return l;
+    }else if(words[mid] === s){
+        return mid;
+    }else if(words[mid]>s){
+        return binarySearch(words,s,left,mid-1);
+    }else{
+        return binarySearch(words,s,mid+1,right);
+    }
+
+}
