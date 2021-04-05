6b9fdbd61c606ac76f7d4f5b2de6d8bc68d1dae8
jiangshanmeta <540118044@qq.com>
2021-04-06 00:12:35 +0800

    10

---
+++ src/面试题10_02.group-anagrams-lcci.1000040/面试题10_02.group-anagrams-lcci.1000040.ts
@@ -0,0 +1,18 @@
+function groupAnagrams(strs: string[]): string[][] {
+    const map = new Map<string,string[]>();
+    for(let i=0;i<strs.length;i++){
+        const counts:number[] = new Array(26).fill(0);
+        for(let j=0;j<strs[i].length;j++){
+            counts[strs[i].charCodeAt(j)-97]++;
+        }
+        const key = counts.join(',');
+        if(map.has(key)){
+            map.get(key)!.push(strs[i]);
+        }else{
+            map.set(key,[strs[i]]);
+        }
+    }
+    return [
+        ...map.values()
+    ]
+};
