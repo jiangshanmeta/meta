0c870b0a78d8121678f224f9d5abd8058f9ba902
jiangshanmeta <540118044@qq.com>
2021-04-05 10:41:59 +0800

    61

---
+++ src/剑指Offer61.bu-ke-pai-zhong-de-shun-zi-lcof.100341/剑指Offer61.bu-ke-pai-zhong-de-shun-zi-lcof.100341.ts
@@ -0,0 +1,33 @@
+function isStraight(nums: number[]): boolean {
+    let zeroCount = 0;
+    const counts:boolean[] = new Array(14).fill(false);
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] === 0){
+            zeroCount++;
+        }else{
+            counts[nums[i]] = true;
+        }
+    }
+
+    let hasCount = 0;
+    for(let i=1;i<5;i++){
+        if(counts[i]){
+            hasCount++;
+        }
+    }
+    for(let i=5;i<14;i++){
+        if(counts[i]){
+            hasCount++;
+        }
+
+        if(hasCount+zeroCount === 5){
+            return true;
+        }
+
+        if(counts[i-4]){
+            hasCount--;
+        }
+    }
+
+    return false
+};
