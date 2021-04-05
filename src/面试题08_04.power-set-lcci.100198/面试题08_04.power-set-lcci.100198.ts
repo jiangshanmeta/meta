939e0aa9a963e3d91642b8c1598febda08cc985f
jiangshanmeta <540118044@qq.com>
2021-04-06 00:12:20 +0800

    8

---
+++ src/面试题08_04.power-set-lcci.100198/面试题08_04.power-set-lcci.100198.ts
@@ -0,0 +1,17 @@
+function subsets(nums: number[]): number[][] {
+    const result:number[][] = [];
+    backTracking(nums,0,[] as number[],result);
+    return result;
+};
+
+function backTracking(nums:number[],index:number,sequence:number[],result:number[][]){
+    if(index === nums.length){
+        result.push([...sequence])
+        return
+    }
+    backTracking(nums,index+1,sequence,result);
+
+    sequence.push(nums[index]);
+    backTracking(nums,index+1,sequence,result);
+    sequence.pop()
+}
