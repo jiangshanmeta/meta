fdd022b25ff84575c972edd7839a2597116c2030
jiangshanmeta <540118044@qq.com>
2021-04-17 12:46:40 +0800

    271

---
+++ src/0271.encode-and-decode-strings.271/0271.encode-and-decode-strings.271.js
@@ -0,0 +1,24 @@
+/**
+ * Encodes a list of strings to a single string.
+ *
+ * @param {string[]} strs
+ * @return {string}
+ */
+var encode = function (strs) {
+    return strs;
+};
+
+/**
+ * Decodes a single string to a list of strings.
+ *
+ * @param {string} s
+ * @return {string[]}
+ */
+var decode = function (s) {
+    return s;
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(strs));
+ */
