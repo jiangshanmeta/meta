a6a5d3b7918f66bcbb865b3dcc033f21908d9635
jiangshanmeta <540118044@qq.com>
2021-04-20 19:44:54 +0800

    359

---
+++ src/0359.logger-rate-limiter.359/0359.logger-rate-limiter.359.js
@@ -0,0 +1,32 @@
+/**
+ * Initialize your data structure here.
+ */
+var Logger = function () {
+    this.map = {};
+};
+
+/**
+ * Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity.
+ * @param {number} timestamp
+ * @param {string} message
+ * @return {boolean}
+ */
+Logger.prototype.shouldPrintMessage = function (timestamp, message) {
+    if (this.map[message] === undefined) {
+        this.map[message] = timestamp;
+        return true;
+    }
+    if (this.map[message] > timestamp - 10) {
+        return false;
+    }
+    this.map[message] = timestamp;
+    return true;
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * var obj = new Logger()
+ * var param_1 = obj.shouldPrintMessage(timestamp,message)
+ */
