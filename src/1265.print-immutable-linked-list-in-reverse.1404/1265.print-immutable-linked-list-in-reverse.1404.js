a765bcaed67cd1434c1e93872b81f15fae58d544
jiangshanmeta <540118044@qq.com>
2021-04-29 20:37:37 +0800

    1265

---
+++ src/1265.print-immutable-linked-list-in-reverse.1404/1265.print-immutable-linked-list-in-reverse.1404.js
@@ -0,0 +1,27 @@
+/**
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * function ImmutableListNode() {
+ *    @ return {void}
+ *    this.printValue = function() { // print the value of this node.
+ *       ...
+ *    };
+ *
+ *    @return {ImmutableListNode}
+ *    this.getNext = function() { // return the next node.
+ *       ...
+ *    };
+ * };
+ */
+
+/**
+ * @param {ImmutableListNode} head
+ * @return {void}
+ */
+var printLinkedListInReverse = function (head) {
+    if (!head) {
+        return;
+    }
+    printLinkedListInReverse(head.getNext());
+    head.printValue();
+};
