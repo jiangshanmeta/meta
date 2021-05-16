801db3ea444e9e512324ba719f56b82ac37c3c68
jiangshanmeta <540118044@qq.com>
2021-05-16 13:49:34 +0800

    339

---
+++ src/0339.nested-list-weight-sum.339/0339.nested-list-weight-sum.339.ts
@@ -0,0 +1,52 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *     If value is provided, then it holds a single integer
+ *     Otherwise it holds an empty nested list
+ *     constructor(value?: number) {
+ *         ...
+ *     };
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     isInteger(): boolean {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     getInteger(): number | null {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a single integer equal to value.
+ *     setInteger(value: number) {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+ *     add(elem: NestedInteger) {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds,
+ *     or an empty list if this NestedInteger holds a single integer
+ *     getList(): NestedInteger[] {
+ *         ...
+ *     };
+ * };
+ */
+
+function depthSum (nestedList: NestedInteger[]): number {
+    let result = 0;
+    function dfs (root:NestedInteger, depth:number) {
+        if (root.isInteger()) {
+            result += root.getInteger() * depth;
+        } else {
+            depth++;
+            root.getList().forEach((node) => dfs(node, depth));
+        }
+    }
+    nestedList.forEach((node) => dfs(node, 1));
+    return result;
+}
