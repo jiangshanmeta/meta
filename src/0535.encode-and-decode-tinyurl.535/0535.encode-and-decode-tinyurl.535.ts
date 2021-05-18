cafadf8270c92b06a2906c2ee7dff6fb5adbd69b
jiangshanmeta <540118044@qq.com>
2021-05-18 19:42:39 +0800

    535

---
+++ src/0535.encode-and-decode-tinyurl.535/0535.encode-and-decode-tinyurl.535.ts
@@ -0,0 +1,18 @@
+/**
+ * Encodes a URL to a shortened URL.
+ */
+function encode (longUrl: string): string {
+    return longUrl;
+}
+
+/**
+ * Decodes a shortened URL to its original URL.
+ */
+function decode (shortUrl: string): string {
+    return shortUrl;
+}
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(strs));
+ */
