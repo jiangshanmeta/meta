31fc78f11c581ab01d9fe62fa9374eb9de32dff6
jiangshanmeta <540118044@qq.com>
2021-06-12 20:39:24 +0800

    29

---
+++ src/剑指Offer29.shun-shi-zhen-da-yin-ju-zhen-lcof.100293/剑指Offer29.shun-shi-zhen-da-yin-ju-zhen-lcof.100293.ts
@@ -0,0 +1,33 @@
+function spiralOrder (matrix: number[][]): number[] {
+    if (matrix.length === 0) {
+        return [];
+    }
+    const result:number[] = [];
+    let rowL = 0;
+    let rowH = matrix.length - 1;
+    let colL = 0;
+    let colH = matrix[0].length - 1;
+    while (rowL <= rowH && colL <= colH) {
+        for (let j = colL; j <= colH; j++) {
+            result.push(matrix[rowL][j]);
+        }
+        for (let i = rowL + 1; i <= rowH; i++) {
+            result.push(matrix[i][colH]);
+        }
+        if (rowL !== rowH) {
+            for (let j = colH - 1; j >= colL; j--) {
+                result.push(matrix[rowH][j]);
+            }
+        }
+        if (colL !== colH) {
+            for (let i = rowH - 1; i > rowL; i--) {
+                result.push(matrix[i][colL]);
+            }
+        }
+        rowL++;
+        rowH--;
+        colL++;
+        colH--;
+    }
+    return result;
+}
