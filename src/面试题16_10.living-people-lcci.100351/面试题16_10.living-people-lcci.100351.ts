31218e07082b3d71eea6dff370d3878f253ff53e
jiangshanmeta <540118044@qq.com>
2021-06-14 16:03:36 +0800

    10

---
+++ src/面试题16_10.living-people-lcci.100351/面试题16_10.living-people-lcci.100351.ts
@@ -0,0 +1,26 @@
+function maxAliveYear (birth: number[], death: number[]): number {
+    birth.sort((a, b) => a - b);
+    death.sort((a, b) => a - b);
+    let result = birth[0];
+    let maxCount = 0;
+    let curCount = 0;
+    let index1 = 0;
+    let index2 = 0;
+    while (index1 < birth.length) {
+        const curYear = birth[index1];
+        while (index1 < birth.length && birth[index1] === curYear) {
+            index1++;
+            curCount++;
+        }
+        while (index2 < death.length && death[index2] < curYear) {
+            index2++;
+            curCount--;
+        }
+        if (curCount > maxCount) {
+            maxCount = curCount;
+            result = curYear;
+        }
+    }
+
+    return result;
+}
