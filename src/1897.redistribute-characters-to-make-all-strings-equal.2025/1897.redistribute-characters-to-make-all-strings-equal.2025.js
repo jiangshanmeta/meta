880efda94ac798ed818afa635e541fd229f37076
jiangshanmeta <540118044@qq.com>
2021-06-20 13:38:52 +0800

    1897

---
+++ src/1897.redistribute-characters-to-make-all-strings-equal.2025/1897.redistribute-characters-to-make-all-strings-equal.2025.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} words
+ * @return {boolean}
+ */
+var makeEqual = function (words) {
+    const counts = new Array(26).fill(0);
+    for (const word of words) {
+        for (let i = 0; i < word.length; i++) {
+            counts[word.charCodeAt(i) - 97]++;
+        }
+    }
+    for (let i = 0; i < 26; i++) {
+        if (counts[i] % words.length !== 0) {
+            return false;
+        }
+    }
+    return true;
+};
