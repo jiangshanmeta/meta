72bf5672968934f15f63461cc7990918f9396efc
jiangshanmeta <540118044@qq.com>
2021-06-22 21:27:02 +0800

    1854

---
+++ src/1854.maximum-population-year.1983/1854.maximum-population-year.1983.ts
@@ -0,0 +1,17 @@
+function maximumPopulation (logs: number[][]): number {
+    const counts = new Array(101).fill(0);
+    for (const [birth, death, ] of logs) {
+        counts[birth - 1950]++;
+        counts[death - 1950]--;
+    }
+    let result = 0;
+    let maxCount = counts[0];
+    for (let i = 1; i < counts.length; i++) {
+        counts[i] += counts[i - 1];
+        if (counts[i] > maxCount) {
+            result = i;
+            maxCount = counts[i];
+        }
+    }
+    return result + 1950;
+}
