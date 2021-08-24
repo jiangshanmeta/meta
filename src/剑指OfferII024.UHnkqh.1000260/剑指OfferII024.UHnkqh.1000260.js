a02677e33d76d4e424dbf56a10572d0205e3b14b
jiangshanmeta <540118044@qq.com>
2021-08-24 22:35:03 +0800

    24

---
+++ src/剑指OfferII024.UHnkqh.1000260/剑指OfferII024.UHnkqh.1000260.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    const dummyHead = new ListNode(0);
+    while (head) {
+        const next = head.next;
+        head.next = dummyHead.next;
+        dummyHead.next = head;
+        head = next;
+    }
+    return dummyHead.next;
+};
