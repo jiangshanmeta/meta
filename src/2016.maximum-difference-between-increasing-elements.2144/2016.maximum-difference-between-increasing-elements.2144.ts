57bbb5d019ba8793b556184bace43afe74c21bdc
jiangshanmeta <540118044@qq.com>
2021-10-24 17:03:09 +0800

    2016

---
+++ src/2016.maximum-difference-between-increasing-elements.2144/2016.maximum-difference-between-increasing-elements.2144.ts
@@ -0,0 +1,12 @@
+function maximumDifference (nums: number[]): number {
+    let result = -1;
+    let prevMin = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > prevMin) {
+            result = Math.max(result, nums[i] - prevMin);
+        } else {
+            prevMin = nums[i];
+        }
+    }
+    return result;
+}
