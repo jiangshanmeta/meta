ca4f7059cb29042805f6219aaa7c012835c936b7
jiangshanmeta <540118044@qq.com>
2021-11-26 23:17:14 +0800

    88

---
+++ src/剑指OfferII088.GzCJIP.1000243/剑指OfferII088.GzCJIP.1000243.ts
@@ -0,0 +1,9 @@
+function minCostClimbingStairs (cost: number[]): number {
+    const dp:number[] = new Array(cost.length + 1);
+    dp[0] = 0;
+    dp[1] = 0;
+    for (let i = 2; i < dp.length; i++) {
+        dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    }
+    return dp[dp.length - 1];
+}
