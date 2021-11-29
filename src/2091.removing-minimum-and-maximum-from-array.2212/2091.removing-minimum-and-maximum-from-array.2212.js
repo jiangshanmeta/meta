170a3b5750e45c43c6b216f211e19b4a3e1bcb4b
jiangshanmeta <540118044@qq.com>
2021-11-29 22:35:25 +0800

    2091

---
+++ src/2091.removing-minimum-and-maximum-from-array.2212/2091.removing-minimum-and-maximum-from-array.2212.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumDeletions = function (nums) {
+    let maxVal = nums[0];
+    let maxValIndex = 0;
+    let minVal = nums[0];
+    let minValIndex = 0;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > maxVal) {
+            maxVal = nums[i];
+            maxValIndex = i;
+        } else if (nums[i] < minVal) {
+            minVal = nums[i];
+            minValIndex = i;
+        }
+    }
+
+    const maxIndex = Math.max(maxValIndex, minValIndex);
+    const minIndex = Math.min(maxValIndex, minValIndex);
+
+    return Math.min(
+        maxIndex + 1,
+        nums.length - minIndex,
+        minIndex + 1 + nums.length - maxIndex
+    );
+};
