c3fa788baa43af752ae3f14aa41ec401131be9f9
jiangshanmeta <540118044@qq.com>
2021-12-09 00:07:55 +0800

    5

---
+++ src/剑指OfferII005.aseY1I.1000236/剑指OfferII005.aseY1I.1000236.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var maxProduct = function (words) {
+    const bitwise = words.map((word) => {
+        let bit = 0;
+        for (const letter of word) {
+            bit |= (1 << (letter.charCodeAt(0) - 97));
+        }
+        return bit;
+    });
+    let result = 0;
+    for (let i = 0; i < words.length; i++) {
+        for (let j = i + 1; j < words.length; j++) {
+            if (bitwise[i] & bitwise[j]) {
+                continue;
+            }
+            result = Math.max(result, words[i].length * words[j].length);
+        }
+    }
+    return result;
+};
