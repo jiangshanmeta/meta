de29e4dd67c81a73cf887c6e385660c2466d3f51
jiangshanmeta <540118044@qq.com>
2021-12-14 22:18:55 +0800

    70

---
+++ src/剑指OfferII070.skFtm2.1000334/剑指OfferII070.skFtm2.1000334.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function (nums) {
+    let low = 0;
+    let high = nums.length - 1;
+    while (low <= high) {
+        const mid = (low + high) >> 1;
+        if (mid % 2) {
+            if (nums[mid] === nums[mid - 1]) {
+                low = mid + 1;
+            } else if (mid + 1 < nums.length && nums[mid] === nums[mid + 1]) {
+                high = mid - 1;
+            } else {
+                return nums[mid];
+            }
+        } else {
+            if (nums[mid] === nums[mid + 1]) {
+                low = mid + 2;
+            } else if (mid > 0 && nums[mid] === nums[mid - 1]) {
+                high = mid - 2;
+            } else {
+                return nums[mid];
+            }
+        }
+    }
+    return nums[0];
+};
