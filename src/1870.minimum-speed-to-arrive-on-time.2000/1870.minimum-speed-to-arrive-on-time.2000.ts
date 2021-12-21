3a107ea68793b7fe6a9750da0b95023b33e3d76c
jiangshanmeta <540118044@qq.com>
2021-12-21 21:11:25 +0800

    1870

---
+++ src/1870.minimum-speed-to-arrive-on-time.2000/1870.minimum-speed-to-arrive-on-time.2000.ts
@@ -0,0 +1,34 @@
+function minSpeedOnTime (dist: number[], hour: number): number {
+    if (Number.isInteger(hour)) {
+        if (dist.length > hour) {
+            return -1;
+        }
+    } else if (dist.length - 1 > Math.trunc(hour)) {
+        return -1;
+    }
+    let min = 1;
+    let max = Math.max(...dist);
+    if (!Number.isInteger(hour)) {
+        max = Math.max(max, dist[dist.length - 1] / (hour % 1));
+    }
+    let result = max;
+    while (min <= max) {
+        const mid = (min + max) >> 1;
+        const time = calcTime(dist, mid);
+        if (time <= hour) {
+            result = Math.min(result, mid);
+            max = mid - 1;
+        } else {
+            min = mid + 1;
+        }
+    }
+    return result;
+}
+
+function calcTime (dist:number[], speed:number) {
+    let result = dist[dist.length - 1] / speed;
+    for (let i = 0; i < dist.length - 1; i++) {
+        result += Math.ceil(dist[i] / speed);
+    }
+    return result;
+}
