04eb086eaea9c63e05fbb0830cd8d8d3f4b688a8
jiangshanmeta <540118044@qq.com>
2021-12-27 22:46:19 +0800

    2007

---
+++ src/2007.find-original-array-from-doubled-array.2117/2007.find-original-array-from-doubled-array.2117.ts
@@ -0,0 +1,30 @@
+function findOriginalArray (changed: number[]): number[] {
+    if (changed.length % 2 !== 0) {
+        return [];
+    }
+    changed.sort((a, b) => a - b);
+    const countBy = changed.reduce((acc, item) => {
+        if (acc[item]) {
+            acc[item]++;
+        } else {
+            acc[item] = 1;
+        }
+        return acc;
+    }, {});
+    const removedCountMap = {};
+    const result = [];
+    for (let i = 0; i < changed.length; i++) {
+        if (removedCountMap[changed[i]]) {
+            removedCountMap[changed[i]]--;
+            continue;
+        }
+        countBy[changed[i]]--;
+        result.push(changed[i]);
+        if (!countBy[2 * changed[i]]) {
+            return [];
+        }
+        removedCountMap[2 * changed[i]] = (removedCountMap[2 * changed[i]] || 0) + 1;
+        countBy[2 * changed[i]]--;
+    }
+    return result;
+}
