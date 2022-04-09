8895a9b1bb84f8e41d82a0f354795b93f6278dab
jiangshanmeta <540118044@qq.com>
2022-04-09 11:20:18 +0800

    2221

---
+++ src/2221.find-triangular-sum-of-an-array.2324/2221.find-triangular-sum-of-an-array.2324.ts
@@ -0,0 +1,10 @@
+function triangularSum (nums: number[]): number {
+    let length = nums.length;
+    while (length > 1) {
+        for (let i = 0; i < length - 1; i++) {
+            nums[i] = (nums[i] + nums[i + 1]) % 10;
+        }
+        length--;
+    }
+    return nums[0];
+}
