3dc5b2d20adc86534d5097a1609cfd751bf468d9
jiangshanmeta <540118044@qq.com>
2022-06-14 20:38:56 +0800

    2293

---
+++ src/2293.min-max-game.2386/2293.min-max-game.2386.ts
@@ -0,0 +1,14 @@
+function minMaxGame (nums: number[]): number {
+    if (nums.length === 1) {
+        return nums[0];
+    }
+    const result = new Array<number>(nums.length / 2);
+    for (let i = 0; i < result.length; i++) {
+        if (i & 1) {
+            result[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
+        } else {
+            result[i] = Math.min(nums[2 * i], nums[2 * i + 1]);
+        }
+    }
+    return minMaxGame(result);
+}
