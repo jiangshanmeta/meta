eee9e92f08e139f5e4cc72afda0e8489fb07c4f0
jiangshanmeta <540118044@qq.com>
2022-06-19 21:21:29 +0800

    2299

---
+++ src/2299.strong-password-checker-ii.2391/2299.strong-password-checker-ii.2391.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} password
+ * @return {boolean}
+ */
+var strongPasswordCheckerII = function (password) {
+    if (password.length < 8) {
+        return false;
+    }
+    let hasUpper = false;
+    let hasLower = false;
+    let hasNum = false;
+    const specialSet = new Set('!@#$%^&*()-+');
+    let hasSpecial = false;
+    let last = '';
+    for (const char of password) {
+        if (char === last) {
+            return false;
+        }
+        last = char;
+        const code = char.charCodeAt(0);
+        if (code > 96 && code < 123) {
+            hasLower = true;
+        } else if (code > 64 && code < 91) {
+            hasUpper = true;
+        } else if (code > 47 && code < 58) {
+            hasNum = true;
+        } else if (specialSet.has(char)) {
+            hasSpecial = true;
+        }
+    }
+    return hasUpper && hasLower && hasNum && hasSpecial;
+};
