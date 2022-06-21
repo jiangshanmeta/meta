96eeb4ae9b6a45e7ffb82a0bfd065f2d40b387d9
jiangshanmeta <540118044@qq.com>
2022-06-21 23:05:51 +0800

    2303

---
+++ src/2303.calculate-amount-paid-in-taxes.1382/2303.calculate-amount-paid-in-taxes.1382.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} brackets
+ * @param {number} income
+ * @return {number}
+ */
+var calculateTax = function (brackets, income) {
+    brackets.push([Infinity, brackets[brackets.length - 1][1], ]);
+    let result = 0;
+    let base = 0;
+    for (const [upper, percent, ] of brackets) {
+        if (income <= base) {
+            break;
+        }
+        result += (Math.min(upper, income) - base) * percent / 100;
+        base = upper;
+    }
+
+    return result;
+};
