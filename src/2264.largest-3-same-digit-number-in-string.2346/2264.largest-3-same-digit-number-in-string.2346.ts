e9ca0505f98c2b1b026ba7e87c7c11eea84124e7
jiangshanmeta <540118044@qq.com>
2022-07-03 15:43:34 +0800

    2264

---
+++ src/2264.largest-3-same-digit-number-in-string.2346/2264.largest-3-same-digit-number-in-string.2346.ts
@@ -0,0 +1,19 @@
+function largestGoodInteger (num: string): string {
+    let start = 0;
+    let result = '';
+    const N = 3;
+    while (start + N - 1 < num.length) {
+        let end = start + 1;
+        while (end < num.length && num[end] === num[start]) {
+            end++;
+        }
+        if (end - start >= N) {
+            const sub = num[start].repeat(N);
+            if (sub > result) {
+                result = sub;
+            }
+        }
+        start = end;
+    }
+    return result;
+}
