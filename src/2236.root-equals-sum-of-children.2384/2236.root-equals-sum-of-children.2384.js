efc407a2c5c1a1c22084438501e5f2c0dd40da9e
jiangshanmeta <540118044@qq.com>
2022-07-03 15:43:18 +0800

    2236

---
+++ src/2236.root-equals-sum-of-children.2384/2236.root-equals-sum-of-children.2384.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var checkTree = function (root) {
+    return root.val === root.left.val + root.right.val;
+};
