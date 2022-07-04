5ec4f7a7bd50355486140210f57e9f85d737d46f
jiangshanmeta <540118044@qq.com>
2022-07-04 11:45:17 +0800

    2273

---
+++ src/2273.find-resultant-array-after-removing-anagrams.1353/2273.find-resultant-array-after-removing-anagrams.1353.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var removeAnagrams = function (words) {
+    const map = new Map();
+    for (const w of words) {
+        map.set(w, w.split('').sort().join(''));
+    }
+    const result = [words[0], ];
+    for (let i = 1; i < words.length; i++) {
+        if (map.get(words[i]) !== map.get(result[result.length - 1])) {
+            result.push(words[i]);
+        }
+    }
+
+    return result;
+};
