407c9678fa86fe4b56c8fd7d2544b0bcb8a4dd5a
jiangshanmeta <540118044@qq.com>
2022-07-04 11:45:12 +0800

    2210

---
+++ src/2210.count-hills-and-valleys-in-an-array.2316/2210.count-hills-and-valleys-in-an-array.2316.ts
@@ -0,0 +1,23 @@
+function countHillValley (nums: number[]): number {
+    let result = 0;
+    let index = 1;
+    while (index < nums.length && nums[index] === nums[0]) {
+        index++;
+    }
+    while (index < nums.length - 1) {
+        const smallerL = nums[index] < nums[index - 1];
+        const num = nums[index++];
+        while (index < nums.length && nums[index] === num) {
+            index++;
+        }
+        if (index === nums.length) {
+            break;
+        }
+        const smallerR = num < nums[index];
+
+        if (smallerL === smallerR) {
+            result++;
+        }
+    }
+    return result;
+}
