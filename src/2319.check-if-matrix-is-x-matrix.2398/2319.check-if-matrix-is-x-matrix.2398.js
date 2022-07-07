f87c9c844338f584dcd02a711a334f340c341f34
jiangshanmeta <540118044@qq.com>
2022-07-07 10:47:49 +0800

    2319

---
+++ src/2319.check-if-matrix-is-x-matrix.2398/2319.check-if-matrix-is-x-matrix.2398.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} grid
+ * @return {boolean}
+ */
+var checkXMatrix = function (grid) {
+    const N = grid.length;
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < N; j++) {
+            if (i === j || i + j === N - 1) {
+                if (grid[i][j] === 0) {
+                    return false;
+                }
+            } else if (grid[i][j] !== 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
