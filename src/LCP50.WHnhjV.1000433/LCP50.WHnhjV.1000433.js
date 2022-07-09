2e5414699c15e821d8c4308a65a96b27195a5c58
jiangshanmeta <540118044@qq.com>
2022-07-09 16:19:49 +0800

    50

---
+++ src/LCP50.WHnhjV.1000433/LCP50.WHnhjV.1000433.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} gem
+ * @param {number[][]} operations
+ * @return {number}
+ */
+var giveGem = function (gem, operations) {
+    for (const [from, to, ] of operations) {
+        const half = gem[from] >>> 1;
+        gem[from] -= half;
+        gem[to] += half;
+    }
+    let max = -Infinity;
+    let min = Infinity;
+    for (const n of gem) {
+        max = Math.max(max, n);
+        min = Math.min(min, n);
+    }
+    return max - min;
+};
