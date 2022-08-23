ca94d6942ff670dde85701bc0a870e5d62fb97c8
jiangshanmeta <540118044@qq.com>
2022-08-23 09:05:33 +0800

    2335

---
+++ src/2335.minimum-amount-of-time-to-fill-cups.2412/2335.minimum-amount-of-time-to-fill-cups.2412.ts
@@ -0,0 +1,8 @@
+function fillCups (amount: number[]): number {
+    amount.sort((a, b) => a - b);
+    if (amount[0] + amount[1] <= amount[2]) {
+        return amount[2];
+    }
+
+    return amount[2] + Math.ceil((amount[0] + amount[1] - amount[2]) / 2);
+}
