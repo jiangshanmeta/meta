833455fba2dea4221a1009b458a925c25cce6d35
jiangshanmeta <540118044@qq.com>
2022-09-29 20:26:52 +0800

    2419

---
+++ src/2419.longest-subarray-with-maximum-bitwise-and.2503/solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSubarray = function (nums) {
+    let maxNum = 0;
+    let maxLength = 0;
+    let index = 0;
+    while (index < nums.length) {
+        const num = nums[index++];
+        if (num < maxNum) {
+            continue;
+        }
+        let count = 1;
+        while (index < nums.length && nums[index] === num) {
+            count++;
+            index++;
+        }
+        if (num > maxNum) {
+            maxNum = num;
+            maxLength = count;
+        } else if (num === maxNum) {
+            maxLength = Math.max(maxLength, count);
+        }
+    }
+    return maxLength;
+};
