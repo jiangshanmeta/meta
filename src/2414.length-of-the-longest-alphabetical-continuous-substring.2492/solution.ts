e7e15626af8cb1de271337649d0c3a385af7f6d4
jiangshanmeta <540118044@qq.com>
2022-09-29 20:26:44 +0800

    2414

---
+++ src/2414.length-of-the-longest-alphabetical-continuous-substring.2492/solution.ts
@@ -0,0 +1,16 @@
+function longestContinuousSubstring (s: string): number {
+    let result = 1;
+    let lastCode = s.charCodeAt(0);
+    let count = 1;
+    for (let i = 1; i < s.length; i++) {
+        const code = s.charCodeAt(i);
+        if (code === lastCode + 1) {
+            count++;
+            result = Math.max(result, count);
+        } else {
+            count = 1;
+        }
+        lastCode = code;
+    }
+    return result;
+}
