366c150b01d6a024e4256e68eed52d8d439d8fa1
jiangshanmeta <540118044@qq.com>
2022-10-03 22:49:47 +0800

    2423

---
+++ src/2423.remove-letter-to-equalize-frequency.2532/solution.ts
@@ -0,0 +1,26 @@
+function equalFrequency (word: string): boolean {
+    const frequency = new Array<number>(26).fill(0);
+    for (const c of word) {
+        frequency[c.charCodeAt(0) - 97]++;
+    }
+    const map = new Map<number, number>();
+    for (const f of frequency) {
+        if (f === 0) {
+            continue;
+        }
+        if (map.has(f)) {
+            map.set(f, map.get(f) + 1);
+        } else {
+            map.set(f, 1);
+        }
+    }
+    const entries = [...map.entries(), ].sort((a, b) => b[0] - a[0]);
+    if (entries.length > 2) {
+        return false;
+    }
+    if (entries.length === 2) {
+        return (entries[0][1] === 1 && entries[0][0] - entries[1][0] === 1) || entries[1][1] === 1;
+    }
+
+    return entries[0][0] === 1 || entries[0][1] === 1;
+}
