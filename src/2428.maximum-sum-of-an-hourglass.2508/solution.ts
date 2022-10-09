4ba5f4cd544c0ca3a6b7d24b7fb958cea93af266
jiangshanmeta <540118044@qq.com>
2022-10-09 17:16:28 +0800

    2428

---
+++ src/2428.maximum-sum-of-an-hourglass.2508/solution.ts
@@ -0,0 +1,12 @@
+function maxSum (grid: number[][]): number {
+    const M = grid.length;
+    const N = grid[0].length;
+    let result = 0;
+    for (let i = 1; i < M - 1; i++) {
+        for (let j = 1; j < N - 1; j++) {
+            const sum = grid[i][j] + grid[i - 1][j - 1] + grid[i - 1][j] + grid[i - 1][j + 1] + grid[i + 1][j - 1] + grid[i + 1][j] + grid[i + 1][j + 1];
+            result = Math.max(result, sum);
+        }
+    }
+    return result;
+}
