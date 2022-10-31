50f8747225e0e56e789863c78a22f04e4b2b615b
jiangshanmeta <540118044@qq.com>
2022-10-31 17:48:44 +0800

    2547

---
+++ src/2451.odd-string-difference.2547/solution.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var oddString = function (words) {
+    const F = toDiffStr(words[0]);
+    const L = toDiffStr(words[words.length - 1]);
+    if (F !== L) {
+        const S = toDiffStr(words[1]);
+        if (S === F) {
+            return words[words.length - 1];
+        }
+        return words[0];
+    } else {
+        for (let i = 1; i < words.length - 1; i++) {
+            const S = toDiffStr(words[i]);
+            if (S !== F) {
+                return words[i];
+            }
+        }
+    }
+    return '';
+};
+
+function toDiffStr (word) {
+    const result = [];
+    let last = word.charCodeAt(0);
+    for (let i = 1; i < word.length; i++) {
+        const code = word.charCodeAt(i);
+        result.push(code - last);
+        last = code;
+    }
+
+    return result.join(' ');
+}
