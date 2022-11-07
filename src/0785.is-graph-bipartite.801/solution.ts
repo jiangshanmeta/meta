6da47abdcccc1b9a3cadb4de4a144cebc925e068
jiangshanmeta <540118044@qq.com>
2022-11-07 21:39:58 +0800

    785

---
+++ src/0785.is-graph-bipartite.801/solution.ts
@@ -0,0 +1,28 @@
+function isBipartite (graph: number[][]): boolean {
+    const N = graph.length;
+    const visited = new Array<boolean>(N);
+    const colors = new Array<boolean>(N).fill(false);
+    let result = true;
+    const dfs = (v:number) => {
+        if (!result) {
+            return;
+        }
+        visited[v] = true;
+
+        for (const nextV of graph[v]) {
+            if (!visited[nextV]) {
+                colors[nextV] = !colors[v];
+                dfs(nextV);
+            } else if (colors[nextV] === colors[v]) {
+                result = false;
+            }
+        }
+    };
+    for (let i = 0; i < N; i++) {
+        if (!visited[i]) {
+            dfs(i);
+        }
+    }
+
+    return result;
+}
