768aa4fe29c661ffe6a4318665bcd37eeba9e6cc
jiangshanmeta <540118044@qq.com>
2022-12-28 16:32:40 +0800

    2501

---
+++ src/2501.longest-square-streak-in-an-array.2586/solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSquareStreak = function (nums) {
+    nums.sort((a, b) => a - b);
+    let result = -1;
+    const all = new Set(nums);
+    const visited = new Set();
+    for (let i = 0; i < nums.length; i++) {
+        if (visited.has(nums[i])) {
+            continue;
+        }
+        let base = nums[i] ** 2;
+        let count = 1;
+        while (all.has(base)) {
+            count++;
+            visited.add(base);
+            base = base ** 2;
+        }
+        if (count > 1) {
+            result = Math.max(count, result);
+        }
+    }
+    return result;
+};
