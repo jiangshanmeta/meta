31eb46a7ba3c7804f593e08521d5a29ebb4457cb
jiangshanmeta <540118044@qq.com>
2023-02-05 11:50:54 +0800

    2520

---
+++ src/2520.count-the-digits-that-divide-a-number.2608/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var countDigits = function (num) {
+    let copy = num;
+    let result = 0;
+    while (copy) {
+        const digit = copy % 10;
+        if (num % digit === 0) {
+            result++;
+        }
+        copy = (copy - digit) / 10;
+    }
+    return result;
+};
