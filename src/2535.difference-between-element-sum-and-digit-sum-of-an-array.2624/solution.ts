2e459f518f561f9377dcb1f7191fdc86490debb7
jiangshanmeta <540118044@qq.com>
2023-02-06 17:57:43 +0800

    2535

---
+++ src/2535.difference-between-element-sum-and-digit-sum-of-an-array.2624/solution.ts
@@ -0,0 +1,14 @@
+function differenceOfSum (nums: number[]): number {
+    const elementSum = nums.reduce((sum, item) => sum + item, 0);
+    const digitSum = nums.reduce((sum, item) => {
+        while (item) {
+            const digit = item % 10;
+            sum += digit;
+            item = (item - digit) / 10;
+        }
+
+        return sum;
+    }, 0);
+
+    return Math.abs(elementSum - digitSum);
+}
