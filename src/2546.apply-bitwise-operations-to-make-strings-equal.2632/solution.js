dd307492e00c8334c5e6173dcb0a71b92012f3f2
jiangshanmeta <540118044@qq.com>
2023-02-08 13:04:53 +0800

    2546

---
+++ src/2546.apply-bitwise-operations-to-make-strings-equal.2632/solution.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} s
+ * @param {string} target
+ * @return {boolean}
+ */
+var makeStringsEqual = function (s, target) {
+    return s.includes('1') === target.includes('1');
+};
