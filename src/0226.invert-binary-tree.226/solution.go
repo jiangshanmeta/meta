a48d8951ad1150375ce466a1d9c51bcc6abb73bc
jiangshanmeta <540118044@qq.com>
2023-04-17 23:22:11 +0800

    226

---
+++ src/0226.invert-binary-tree.226/solution.go
@@ -0,0 +1,19 @@
+package invertbinarytree226
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	tmp := root.Left
+	root.Left = root.Right
+	root.Right = tmp
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
