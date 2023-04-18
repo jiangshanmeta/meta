e6aed2ac2cab2518157ab6b2009d6c55a3eaa6c3
jiangshanmeta <540118044@qq.com>
2023-04-18 19:46:52 +0800

    700

---
+++ src/0700.search-in-a-binary-search-tree.783/solution.go
@@ -0,0 +1,20 @@
+package searchinabinarysearchtree783
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return root
+	}
+	if root.Val == val {
+		return root
+	}
+	if root.Val > val {
+		return searchBST(root.Left, val)
+	}
+	return searchBST(root.Right, val)
+}
