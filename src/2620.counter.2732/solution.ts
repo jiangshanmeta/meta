02599555ed1100bbec36e92cb7746958b071046d
jiangshanmeta <540118044@qq.com>
2023-04-21 22:28:38 +0800

    2620

---
+++ src/2620.counter.2732/solution.ts
@@ -0,0 +1,12 @@
+function createCounter (n: number): () => number {
+    return function () {
+        return n++;
+    };
+}
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
