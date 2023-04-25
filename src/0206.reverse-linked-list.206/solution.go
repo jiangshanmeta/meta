4853f99fffde0f788a138c7a9197a45bbb6f9a4b
jiangshanmeta <540118044@qq.com>
2023-04-25 20:42:19 +0800

    206

---
+++ src/0206.reverse-linked-list.206/solution.go
@@ -0,0 +1,17 @@
+package reverselinkedlist206
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	dummyHead := ListNode{}
+	for head != nil {
+		next := head.Next
+		head.Next = dummyHead.Next
+		dummyHead.Next = head
+		head = next
+	}
+	return dummyHead.Next
+}
