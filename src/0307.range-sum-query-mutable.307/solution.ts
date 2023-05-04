31153b1a75b22bdc0c1a177d0416a81946b39ec4
jiangshanmeta <540118044@qq.com>
2023-05-04 21:51:00 +0800

    307

--- src/0307.range-sum-query-mutable.307/solution.ts
+++ src/0307.range-sum-query-mutable.307/solution.ts
@@ -56,3 +56,51 @@ class NumArray {
  * obj.update(index,val)
  * var param_2 = obj.sumRange(left,right)
  */
+// zkw 线段树 非递归
+class NumArray2 {
+    N: number;
+    segmentTree:number[]
+    constructor (nums: number[]) {
+        const N = Math.pow(2, Math.ceil(Math.log2(nums.length + 2)));
+        this.N = N;
+        this.segmentTree = new Array<number>(this.N * 2).fill(0);
+        for (let i = 0; i < nums.length; i++) {
+            this.segmentTree[i + N + 1] = nums[i];
+        }
+        for (let i = N - 1; i > 0; i--) {
+            this.segmentTree[i] = this.segmentTree[i << 1] + this.segmentTree[(i << 1) | 1];
+        }
+    }
+
+    update (index: number, val: number): void {
+        // 对应叶节点的坐标
+        let i = index + this.N + 1;
+        this.segmentTree[i] = val;
+        i >>= 1;
+        // 更新父节点
+        while (i > 0) {
+            this.segmentTree[i] = this.segmentTree[i << 1] + this.segmentTree[i << 1 | 1];
+            i >>= 1;
+        }
+    }
+
+    sumRange (left: number, right: number): number {
+        let result = 0;
+        // 处理一下坐标 转化为叶节点 开区间 （原始输入为闭区间）
+        left += this.N;
+        right += 2 + this.N;
+        while (left ^ right ^ 1) {
+            // 如果left是左节点 那么对应的兄弟节点要被计算在内
+            if ((~left) & 1) {
+                result += this.segmentTree[left ^ 1];
+            }
+            // 如果right是右节点 那么对应的兄弟节点要被计算在内
+            if (right & 1) {
+                result += this.segmentTree[right ^ 1];
+            }
+            left >>= 1;
+            right >>= 1;
+        }
+        return result;
+    }
+}
