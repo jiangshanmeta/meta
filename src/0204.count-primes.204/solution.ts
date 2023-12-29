e8879aa5bc8d6dffa110f5cc1242bd9744174066
jiangshanmeta <540118044@qq.com>
2023-12-29 21:23:43 +0800

    204

--- src/0204.count-primes.204/solution.ts
+++ src/0204.count-primes.204/solution.ts
@@ -15,3 +15,23 @@ function countPrimes (n: number): number {
 
     return result;
 }
+// 欧拉筛 O(N)
+function countPrimes2 (n: number): number {
+    const primes:number[] = [];
+    const isPrimes = new Array<boolean>(n).fill(true);
+    for (let i = 2; i < isPrimes.length; i++) {
+        if (isPrimes[i]) {
+            primes.push(i);
+        }
+        for (let j = 0; j < primes.length; j++) {
+            if (primes[j] * i >= n) {
+                break;
+            }
+            isPrimes[i * primes[j]] = false;
+            if (i % primes[j] === 0) {
+                break;
+            }
+        }
+    }
+    return primes.length;
+}
