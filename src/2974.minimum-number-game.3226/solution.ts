45615f89e5767ea8170918d67916d27a582acfe6
jiangshanmeta <540118044@qq.com>
2023-12-29 21:23:50 +0800

    2974

---
+++ src/2974.minimum-number-game.3226/solution.ts
@@ -0,0 +1,9 @@
+function numberGame (nums: number[]): number[] {
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length; i += 2) {
+        nums[i] ^= nums[i + 1];
+        nums[i + 1] ^= nums[i];
+        nums[i] ^= nums[i + 1];
+    }
+    return nums;
+}
