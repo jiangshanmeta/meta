a3fbc0de38b954dd22e83c071534ca2ffa43a0d0
jiangshanmeta <540118044@qq.com>
2024-02-04 22:18:05 +0800

    39

---
+++ src/LCR039.0ynMMM.1000283/solution.ts
@@ -0,0 +1,17 @@
+function largestRectangleArea(heights: number[]): number {
+    let result = 0;
+    const stack:number[] = [];
+    for(let i=0;i<heights.length;i++){
+        while(stack.length && heights[stack[stack.length-1]]>=heights[i]){
+            const index = stack.pop()!;
+            const left = stack.length>0?stack[stack.length-1]:-1;
+            result = Math.max(result,heights[index]*(i-left-1))
+        }
+        stack.push(i);
+    }
+    for(let i=stack.length-1;i>-1;i--){
+        const left = i>0? stack[i-1]:-1;
+        result = Math.max(result,heights[stack[i]]*(heights.length-left-1))
+    }
+    return result;
+};
