8938a381676c62d7c15ae83d604799298588299b
jiangshanmeta <540118044@qq.com>
2024-02-07 20:29:51 +0800

    962

--- src/0962.maximum-width-ramp.1002/solution.ts
+++ src/0962.maximum-width-ramp.1002/solution.ts
@@ -11,8 +11,26 @@ function maxWidthRamp (A: number[]): number {
             stack.pop();
         }
         while (stack.length && A[stack[stack.length - 1]] <= A[i]) {
-            result = Math.max(result, i - stack.pop());
+            result = Math.max(result, i - stack.pop()!);
         }
     }
     return result;
 }
+
+
+function maxWidthRamp2(nums: number[]): number {
+    const stack:number[] = [];
+    let result = 0;
+    for(let i=0;i<nums.length;i++){
+        if(stack.length === 0 || nums[i]<nums[stack[stack.length-1]] ){
+            stack.push(i);
+        }
+    }
+    for(let i=nums.length-1;i>-1 && stack.length>0;i--){
+        while(stack.length && nums[stack[stack.length-1]]<=nums[i] ){
+            const index = stack.pop()!;
+            result = Math.max(result,i-index);
+        }
+    }
+    return result
+};
