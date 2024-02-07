d44726ddf73987e3291a81e1639dea57175a83b0
jiangshanmeta <540118044@qq.com>
2024-02-07 20:29:39 +0800

    316

--- src/0316.remove-duplicate-letters.316/solution.ts
+++ src/0316.remove-duplicate-letters.316/solution.ts
@@ -20,3 +20,34 @@ function removeDuplicateLetters (s: string): string {
     }
     return stack.join('');
 }
+
+
+function removeDuplicateLetters2(s: string): string {
+    const counts = new Array<number>(26).fill(0);
+    for(let i=0;i<s.length;i++){
+        counts[s.charCodeAt(i)-97]++;
+    }
+
+    const stack:string[] = [];
+    const inStack = new Array<boolean>(26);
+    for(let i=0;i<s.length;i++){
+        const code = s.charCodeAt(i)-97;
+        counts[code]--;
+        if(inStack[code]){
+            continue;
+        }
+        while(stack.length){
+            const last = stack[stack.length-1];
+            const lastCode = last.charCodeAt(0)-97;
+            if(lastCode>code && counts[lastCode]>0){
+                stack.pop();
+                inStack[lastCode] = false;
+            }else{
+                break;
+            }
+        }
+        stack.push(s[i]);
+        inStack[code] = true;
+    }
+    return stack.join('');
+};
