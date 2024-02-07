4b8236870c0a59beaf30df1acc7147c85df42c37
jiangshanmeta <540118044@qq.com>
2024-02-07 20:29:56 +0800

    1504

--- src/1504.count-submatrices-with-all-ones.1628/solution.ts
+++ src/1504.count-submatrices-with-all-ones.1628/solution.ts
@@ -22,3 +22,32 @@ function numSubmat (mat: number[][]): number {
     }
     return result;
 }
+
+function numSubmat2(mat: number[][]): number {
+    const M = mat.length;
+    const N = mat[0].length;
+    const cols = new Array<number>(N).fill(0);
+    let result = 0;
+    for(let i =0;i<M;i++){
+        const stack:number[] = [];
+        const sums:number[] = [];
+        for(let j=0;j<N;j++){
+            if(mat[i][j] === 0){
+                cols[j] = 0;
+            }else{
+                cols[j]++;
+            }
+            while(stack.length && cols[stack[stack.length-1]] >= cols[j]){
+                stack.pop();
+                sums.pop();
+            }
+            const last = sums.length>0? sums[sums.length-1]:0;
+            const index = stack.length>0? stack[stack.length-1]:-1;
+            const sum = last+ (j-index)*cols[j] 
+            sums.push(sum);
+            stack.push(j);
+            result += sum;
+        }
+    }
+    return result
+};
