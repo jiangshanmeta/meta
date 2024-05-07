e373c1b3ad29c5c57a6c41c5eb8095bf277d567b
jiangshanmeta <540118044@qq.com>
2024-05-07 21:56:01 +0800

    20

---
+++ src/0020.valid-parentheses.20/solution.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (stack.size() == 0) {
+                stack.push(c);
+                continue;
+            }
+            if (c == ')' && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == ']' && stack.peek() == '[') {
+                stack.pop();
+            } else if (c == '}' && stack.peek() == '{') {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+
+        }
+        return stack.size() == 0;
+    }
+}
