1c7125fec939bffc940d4bac781026c920715c87
jiangshanmeta <540118044@qq.com>
2024-05-11 14:16:00 +0800

    217

---
+++ src/0217.contains-duplicate.217/solution.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (set.contains(n)) {
+                return true;
+            }
+            set.add(n);
+        }
+        return false;
+    }
+}
