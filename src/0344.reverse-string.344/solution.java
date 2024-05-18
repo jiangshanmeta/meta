de50a7ad7969cacf392873624a5e791cb0a96fb6
jiangshanmeta <540118044@qq.com>
2024-05-18 22:51:51 +0800

    344

---
+++ src/0344.reverse-string.344/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int l = 0;
+        int r = s.length - 1;
+        while (l < r) {
+            char tmp = s[l];
+            s[l++] = s[r];
+            s[r--] = tmp;
+        }
+    }
+}
