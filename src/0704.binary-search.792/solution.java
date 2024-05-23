1f43d3925a19cba33a2e74b637d2c980e665147a
jiangshanmeta <540118044@qq.com>
2024-05-23 19:48:31 +0800

    792

---
+++ src/0704.binary-search.792/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) >> 1;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
