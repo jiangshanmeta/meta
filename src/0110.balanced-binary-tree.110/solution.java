0e3db6e7e467da9d5cf9abac3d1e43413824cadd
jiangshanmeta <540118044@qq.com>
2024-10-01 10:51:02 +0800

    110

---
+++ src/0110.balanced-binary-tree.110/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = getHeight(root.left);
+        if (left == -1) {
+            return -1;
+        }
+        int right = getHeight(root.right);
+        if (right == -1) {
+            return -1;
+        }
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+
+}
