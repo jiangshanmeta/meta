d04412435475b2ae64d8f268bf3fa4de150cdf3e
jiangshanmeta <540118044@qq.com>
2024-10-01 10:51:18 +0800

    236

---
+++ src/0236.lowest-common-ancestor-of-a-binary-tree.236/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+
+        if (left == null) {
+            return right;
+        }
+        return left;
+    }
+
+}
