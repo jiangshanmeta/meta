155ffaf700c763269d343d133b5581d19617eff0
jiangshanmeta <540118044@qq.com>
2024-10-02 17:19:24 +0800

    108

---
+++ src/0108.convert-sorted-array-to-binary-search-tree.108/solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        return build(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode build(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = (left + right) >> 1;
+        return new TreeNode(
+                nums[mid],
+                build(nums, left, mid - 1),
+                build(nums, mid + 1, right));
+    }
+
+}
