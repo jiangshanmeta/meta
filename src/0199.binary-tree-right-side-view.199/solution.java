5ee15eb4dd0e38909e399c5d593e906a03242c61
jiangshanmeta <540118044@qq.com>
2024-10-03 10:44:22 +0800

    199

---
+++ src/0199.binary-tree-right-side-view.199/solution.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) {
+            return List.of();
+        }
+        List<Integer> result = new ArrayList<>();
+        travel(List.of(root), result);
+
+        return result;
+    }
+
+    private void travel(List<TreeNode> level, List<Integer> result) {
+        int last = -1;
+        List<TreeNode> next = new ArrayList<>();
+        for (TreeNode node : level) {
+            last = node.val;
+            if (node.left != null) {
+                next.add(node.left);
+            }
+            if (node.right != null) {
+                next.add(node.right);
+            }
+        }
+        result.add(last);
+        if (!next.isEmpty()) {
+            travel(next, result);
+        }
+    }
+
+}
