d1dc7e4c9b27f75a6cc22d5170a675e4ec6351b7
jiangshanmeta <540118044@qq.com>
2024-10-03 17:17:27 +0800

    338

---
+++ src/0338.counting-bits.338/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            result[i] = result[i >> 1] + (i & 1);
+        }
+
+        return result;
+    }
+}
