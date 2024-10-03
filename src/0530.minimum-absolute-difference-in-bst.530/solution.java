007afba3e792cef0498570b4a576dce73fbff788
jiangshanmeta <540118044@qq.com>
2024-10-03 17:17:37 +0800

    530

---
+++ src/0530.minimum-absolute-difference-in-bst.530/solution.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    private int last = Integer.MAX_VALUE;
+    private int result = Integer.MAX_VALUE;
+
+    public int getMinimumDifference(TreeNode root) {
+        last = root.val;
+        inOrder(root);
+        return result;
+    }
+
+    private void inOrder(TreeNode node) {
+        if (node.left != null) {
+            inOrder(node.left);
+        }
+
+        if (last < node.val) {
+            result = Math.min(result, node.val - last);
+        }
+
+        last = node.val;
+
+        if (node.right != null) {
+            inOrder(node.right);
+        }
+
+    }
+}
