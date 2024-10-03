54c00a40300954c52ec1bb0770108f2e44350475
jiangshanmeta <540118044@qq.com>
2024-10-03 10:44:17 +0800

    160

---
+++ src/0160.intersection-of-two-linked-lists.160/solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lengthA = getLength(headA);
+        int lengthB = getLength(headB);
+        while (lengthA > lengthB) {
+            headA = headA.next;
+            lengthA--;
+        }
+        while (lengthB > lengthA) {
+            headB = headB.next;
+            lengthB--;
+        }
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+    }
+
+    private int getLength(ListNode node) {
+        int count = 0;
+        while (node != null) {
+            node = node.next;
+            count++;
+        }
+        return count;
+    }
+}
