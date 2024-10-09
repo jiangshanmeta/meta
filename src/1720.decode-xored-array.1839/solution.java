32c330157552808f36596a1204bfffa37ccd5ef4
jiangshanmeta <540118044@qq.com>
2024-10-09 19:22:18 +0800

    1720

---
+++ src/1720.decode-xored-array.1839/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        int[] result = new int[encoded.length + 1];
+        result[0] = first;
+        for (int i = 0; i < encoded.length; i++) {
+            result[i + 1] = result[i] ^ encoded[i];
+        }
+        return result;
+    }
+}
