83d4dcab5ff6e32ac7cac5d4b035a254307eaa40
jiangshanmeta <540118044@qq.com>
2024-11-04 17:50:35 +0800

    175

---
+++ src/LCR175.er-cha-shu-de-shen-du-lcof.100319/solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int calculateDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        return Math.max(calculateDepth(root.left),calculateDepth(root.right) )+1;
+    }
+}
