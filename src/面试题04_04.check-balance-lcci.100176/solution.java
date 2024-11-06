7925a31ab6585844cfd379166789584611d2006b
jiangshanmeta <540118044@qq.com>
2024-11-06 17:53:00 +0800

    4

---
+++ src/面试题04_04.check-balance-lcci.100176/solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return check(root) != -1;
+    }
+    
+    private int check(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int left = check(root.left);
+        if(left == -1){
+            return -1;
+        }
+        int right = check(root.right);
+        if(right == -1){
+            return -1;
+        }
+        if(Math.abs(left-right)>1){
+            return -1;
+        }
+        return Math.max(left,right)+1;
+    }
+}
