7678042f4d7de210115cd4c3b0d8c5ec0b5372e6
jiangshanmeta <540118044@qq.com>
2024-11-20 16:33:04 +0800

    412

---
+++ src/0412.fizz-buzz.412/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%15 == 0){
+                result.add("FizzBuzz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            }else if(i%3 == 0){
+                result.add("Fizz");
+            }else{
+                result.add(String.valueOf(i));
+            }
+        }
+        return result;
+    }
+}
