d1b7e0986ae6c7baf506fd1581cca59e63c3f847
jiangshanmeta <540118044@qq.com>
2024-12-13 11:03:12 +0800

    470

---
+++ src/0470.implement-rand10-using-rand7.903/solution.java
@@ -0,0 +1,16 @@
+/**
+ * The rand7() API is already defined in the parent class SolBase.
+ * public int rand7();
+ * @return a random integer in the range 1 to 7
+ */
+class Solution extends SolBase {
+    public int rand10() {
+        int row, col, idx;
+        do {
+            row = rand7();
+            col = rand7();
+            idx = col + (row - 1) * 7;
+        } while (idx > 40);
+        return 1 + (idx - 1) % 10;
+    }
+}
