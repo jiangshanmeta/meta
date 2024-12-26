b684531816ae6d5b61741c1f55fe9707cb0fab8d
jiangshanmeta <540118044@qq.com>
2024-12-26 10:57:29 +0800

    590

---
+++ src/0590.n-ary-tree-postorder-traversal.776/solution.java
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> result = new ArrayList<>();
+        dfs(root,result);
+        return result;
+    }
+
+    private void dfs(Node root,List<Integer> result){
+        if(root == null){
+            return;
+        }
+        
+        for(Node node : root.children){
+            dfs(node,result);
+        }
+        result.add(root.val);
+    }
+}
