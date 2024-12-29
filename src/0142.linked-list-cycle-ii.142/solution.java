bb402813ffa7b58f079626e4e720644110f22721
jiangshanmeta <540118044@qq.com>
2024-12-29 18:46:21 +0800

    142

---
+++ src/0142.linked-list-cycle-ii.142/solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast){
+                while (head != slow){
+                    head = head.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+            
+        }
+        return null;
+    }
+}
