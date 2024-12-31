3097c6c5b47410334350b0b338cf97e5baea90a1
jiangshanmeta <540118044@qq.com>
2024-12-31 22:13:20 +0800

    701

---
+++ src/0701.insert-into-a-binary-search-tree.784/solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null){
+            return new TreeNode(val);
+        }
+        if(root.val>val){
+            root.left = insertIntoBST(root.left,val);
+        }else{
+            root.right = insertIntoBST(root.right,val);
+        }
+        return root;
+    }
+}
