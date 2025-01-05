5473fcb3cb663cd656ba739a92a0dc7ed8aa3903
jiangshanmeta <540118044@qq.com>
2025-01-05 09:17:04 +0800

    1290

---
+++ src/1290.convert-binary-number-in-a-linked-list-to-integer.1411/solution.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int result = 0;
+        while(head != null){
+            result = result*2+head.val;
+            head = head.next;
+        }
+        return result;
+    }
+}
