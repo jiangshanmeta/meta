a1ea451196d3f14e9d6d9779a770bafdf48a6443
jiangshanmeta <540118044@qq.com>
2025-01-06 21:55:45 +0800

    1325

---
+++ src/1325.delete-leaves-with-a-given-value.1450/solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if(root == null){
+            return null;
+        }
+        
+        root.left = removeLeafNodes(root.left,target);
+        root.right = removeLeafNodes(root.right,target);
+        
+        if(root.left == null && root.right == null){
+            if(root.val == target){
+                return null;
+            }
+            
+        }
+        return root;
+    }
+}
