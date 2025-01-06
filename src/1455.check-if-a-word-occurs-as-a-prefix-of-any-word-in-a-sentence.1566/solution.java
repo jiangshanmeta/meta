be3218465560db12e555f38c2790bc231f0115c8
jiangshanmeta <540118044@qq.com>
2025-01-06 21:56:06 +0800

    1455

---
+++ src/1455.check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.1566/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] words = sentence.split(" ");
+        for(int i=0;i<words.length;i++){
+            if(words[i].startsWith(searchWord)){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+}
