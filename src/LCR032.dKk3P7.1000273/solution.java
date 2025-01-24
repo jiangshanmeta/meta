65e1894043d2884bd897f3d298780203adf116e5
jiangshanmeta <540118044@qq.com>
2025-01-24 22:22:11 +0800

    32

---
+++ src/LCR032.dKk3P7.1000273/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] counts = new int[26];
+        for(char c : s.toCharArray() ){
+            counts[c-'a']++;
+        }
+        for(char c : t.toCharArray() ){
+            int index = c-'a';
+            if(counts[index] == 0){
+                return false;
+            }
+            counts[index]--;
+        }
+        return !s.equals(t);
+    }
+}
