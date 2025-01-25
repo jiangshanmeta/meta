46993abe6ee2bee38a2f7e86da5937c2e685eac3
jiangshanmeta <540118044@qq.com>
2025-01-25 19:25:13 +0800

    47

---
+++ src/LCR047.pOCWxh.1000301/solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+
+        if(root.left == null && root.right == null && root.val == 0){
+            return null;
+        }
+        return root;
+    }
+}
