1181c8e325740a5ec66fcbc7d02150fb1f1deb52
jiangshanmeta <540118044@qq.com>
2025-01-31 22:14:19 +0800

    158

---
+++ src/LCR158.shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof.100310/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int inventoryManagement(int[] stock) {
+        int element = stock[0];
+        int count = 1;
+        for(int i=1;i<stock.length;i++){
+            if(stock[i] == element){
+                count++;
+            }else if(count>0){
+                count--;
+            }else{
+                count = 1;
+                element = stock[i];
+            }
+        }
+        return element;
+    }
+}
